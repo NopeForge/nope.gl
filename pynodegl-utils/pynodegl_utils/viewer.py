NopeForge/nope.gl

commit 180df017005a109e45fb38eb1feaf3ff68de8974

utils/viewer: port to QOpenGLWidget
Also enables depth, stencil and alpha buffers.

pynodegl-utils/pynodegl_utils/viewer.py
@@ -36,10 +36,9 @@ import traceback
 import pynodegl as ngl
 
 from PyQt5 import QtGui, QtCore, QtWidgets
-from PyQt5.QtOpenGL import QGLWidget, QGLFormat
 
 
-class _GLWidget(QGLWidget):
+class _GLWidget(QtWidgets.QOpenGLWidget):
 
     def set_time(self, t):
         self._time = t
@@ -60,10 +59,16 @@ class _GLWidget(QGLWidget):
         self.update()
 
     def __init__(self, parent, aspect_ratio):
-        gl_format = QGLFormat()
-        gl_format.setProfile(QGLFormat.CoreProfile)
-        gl_format.setVersion(3,3)
-        QGLWidget.__init__(self, gl_format, parent=parent)
+        QtWidgets.QOpenGLWidget.__init__(self, parent=parent)
+
+        gl_format = QtGui.QSurfaceFormat()
+        gl_format.setVersion(3, 3)
+        gl_format.setProfile(QtGui.QSurfaceFormat.CoreProfile)
+        gl_format.setDepthBufferSize(24);
+        gl_format.setStencilBufferSize(8);
+        gl_format.setAlphaBufferSize(8);
+        self.setFormat(gl_format)
+
         self.setMinimumSize(640, 360)
         self._viewer = ngl.Viewer(None)
         self._time = 0

commit 4f0809d1f1e8f7cf582a7c1f0293d3fb817481b3

utils/viewer: fix viewport coordinates
Fixes a regression introduced by
180df017005a109e45fb38eb1feaf3ff68de8974 as the new QOpenGLWidget
overrides the viewport before each call to paintGL with the following
coordinates: 0, 0, self.width(), self.height()

pynodegl-utils/pynodegl_utils/viewer.py
@@ -74,23 +74,23 @@ class _GLWidget(QtWidgets.QOpenGLWidget):
         self._viewer = ngl.Viewer()
         self._time = 0
         self._aspect_ratio = aspect_ratio
+        self.resizeGL(self.width(), self.height())
 
     def paintGL(self):
+        self._viewer.set_viewport(self.view_x, self.view_y, self.view_width, self.view_height)
         self._viewer.draw(self._time)
 
     def resizeGL(self, screen_width, screen_height):
         aspect = self._aspect_ratio[0] / float(self._aspect_ratio[1])
-        view_width = screen_width
-        view_height = screen_width / aspect
+        self.view_width = screen_width
+        self.view_height = screen_width / aspect
 
-        if view_height > screen_height:
-            view_height = screen_height
-            view_width = screen_height * aspect
+        if self.view_height > screen_height:
+            self.view_height = screen_height
+            self.view_width = screen_height * aspect
 
-        view_x = (screen_width - view_width) / 2.0
-        view_y = (screen_height - view_height) / 2.0
-
-        self._viewer.set_viewport(view_x, view_y, view_width, view_height)
+        self.view_x = (screen_width - self.view_width) / 2.0
+        self.view_y = (screen_height - self.view_height) / 2.0
 
     def initializeGL(self):
         if platform.system() == 'Linux':