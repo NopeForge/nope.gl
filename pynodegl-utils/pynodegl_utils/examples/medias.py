dc1cd5a533e93e6146b6abb79ee0f2069ccf7de8
Clément Bœsch <cboesch@gopro.com>
2017-04-14 13:59:30 +0200

    utils/examples: add a progress bar toggle in centered_media

--- pynodegl-utils/pynodegl_utils/examples/medias.py
+++ pynodegl-utils/pynodegl_utils/examples/medias.py
@@ -1,24 +1,82 @@
 import math
 
-from pynodegl import TexturedShape, Quad, Triangle, Shape, ShapePrimitive, Texture, Media, Shader, AnimKeyFrameScalar, Group, GLStencilState, GLState, Scale, AnimKeyFrameScalar, AnimKeyFrameVec3, Rotate, GLColorState
+from pynodegl import TexturedShape, Quad, Triangle, Shape, ShapePrimitive, Texture, Media, Shader, AnimKeyFrameScalar, Group, GLStencilState, GLState, Scale, AnimKeyFrameScalar, AnimKeyFrameVec3, Rotate, GLColorState, UniformScalar
 
 from pynodegl_utils.misc import scene
 
 from OpenGL import GL
 
+pgbar_shader = '''
+#version 100
+precision mediump float;
+
+uniform float time;
+uniform float ar;
+uniform sampler2D tex0_sampler;
+varying vec2 var_tex0_coords;
+
+void main()
+{
+    float pad    = 2.0 / 100.;
+    float padw   = pad;
+    float padh   = pad * ar;
+    float height = 3.0 / 100. * ar;
+    float thick  = 0.3 / 100.;
+    float thickw = thick;
+    float thickh = thick * ar;
+
+    float x = var_tex0_coords.x;
+    float y = var_tex0_coords.y;
+    vec4 video_pix = texture2D(tex0_sampler, var_tex0_coords);
+
+    float bar_width = 1. - padw*2. - thickw*2.;
+
+    float t = time*bar_width + padw+thickw;
+
+    if (x < t && x > padw+thickw &&
+        y < 1.-padh-thickh && y > 1.-padh-height) {
+        vec4 color = vec4(1.,0,0,0);
+        gl_FragColor = mix(video_pix, color, 0.7);
+    } else {
+        gl_FragColor = video_pix;
+    }
+
+    if (y < 1.-padh-thickh && y > 1.-padh-height &&
+        ((x > padw && x < padw+thickw) ||
+         (x > 1.-padw-thickw && x < 1.-padw))) {
+        gl_FragColor = vec4(1);
+    }
+
+    if (x < 1.-padw-thickw && x > padw+thickw &&
+        ((y < 1.-padh-height && y > 1.-padh-height-thickh) ||
+         (y < 1.-padh && y > 1. - padh-thickh))) {
+        gl_FragColor = vec4(1);
+    }
+}
+'''
+
 @scene({'name': 'uv_corner_x', 'type': 'range', 'range': [0,1], 'unit_base': 100},
        {'name': 'uv_corner_y', 'type': 'range', 'range': [0,1], 'unit_base': 100},
        {'name': 'uv_width', 'type': 'range', 'range': [0,1], 'unit_base': 100},
-       {'name': 'uv_height', 'type': 'range', 'range': [0,1], 'unit_base': 100})
-def centered_media(cfg, uv_corner_x=0, uv_corner_y=0, uv_width=1, uv_height=1):
+       {'name': 'uv_height', 'type': 'range', 'range': [0,1], 'unit_base': 100},
+       {'name': 'progress_bar', 'type': 'bool'})
+def centered_media(cfg, uv_corner_x=0, uv_corner_y=0, uv_width=1, uv_height=1, progress_bar=False):
+
     cfg.duration = cfg.medias[0].duration
 
-    q = Quad((-0.5, -0.5, 0), (1, 0, 0), (0, 1, 0),
+    q = Quad((-1, -1, 0), (2, 0, 0), (0, 2, 0),
              (uv_corner_x, uv_corner_y), (uv_width, 0), (0, uv_height))
     m = Media(cfg.medias[0].filename)
     t = Texture(data_src=m)
     s = Shader()
     tshape = TexturedShape(q, s, t)
+    if progress_bar:
+        s.set_fragment_data(pgbar_shader)
+        time = UniformScalar("time")
+        time.add_animkf(AnimKeyFrameScalar(0, 0),
+                        AnimKeyFrameScalar(cfg.duration, 1))
+        ar = UniformScalar("ar", cfg.aspect_ratio)
+        tshape.add_uniforms(time, ar)
     return tshape
 
 frag_data='''
