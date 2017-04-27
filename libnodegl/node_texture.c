81130ce97bcb7e8ea09fc6fa625fcc1b7842e7c5
Matthieu Bouron <matthieu.bouron@gmail.com>
2017-04-27 16:59:37 +0200

    texture: split upload codepaths into separate functions
    Also improves handling of different data source pixel formats.

--- libnodegl/node_texture.c
+++ libnodegl/node_texture.c
@@ -166,7 +166,8 @@ static void handle_fps_frame(struct ngl_node *node)
     gl->BindTexture(GL_TEXTURE_2D, 0);
 }
 
-static void handle_media_frame(struct ngl_node *node)
+#if defined(TARGET_ANDROID)
+static void handle_mc_frame(struct ngl_node *node, struct sxplayer_frame *frame)
 {
     struct ngl_ctx *ctx = node->ctx;
     struct glcontext *glcontext = ctx->glcontext;
@@ -174,139 +175,233 @@ static void handle_media_frame(struct ngl_node *node)
 
     struct texture *s = node->priv_data;
     struct media *media = s->data_src->priv_data;
+
+    AVMediaCodecBuffer *buffer = (AVMediaCodecBuffer *)frame->data;
+    float matrix[4*4] = {
+        1.0f,  0.0f, 0.0f, 0.0f,
+        0.0f, -1.0f, 0.0f, 0.0f,
+        0.0f,  0.0f, 1.0f, 0.0f,
+        0.0f,  1.0f, 0.0f, 1.0f,
+    };
+
+    av_android_surface_render_buffer(media->android_surface, buffer, s->coordinates_matrix);
+    ngli_mat4_mul(s->coordinates_matrix, matrix, s->coordinates_matrix);
+
+    s->id = s->media_id = media->android_texture_id;
+    s->target = s->media_target = media->android_texture_target;
+    s->width = frame->width;
+    s->height = frame->height;
+}
+#endif
+
+#if defined(TARGET_DARWIN)
+static void handle_vt_frame(struct ngl_node *node, struct sxplayer_frame *frame)
+{
+    struct ngl_ctx *ctx = node->ctx;
+    struct glcontext *glcontext = ctx->glcontext;
+    const struct glfunctions *gl = &glcontext->funcs;
+
+    struct texture *s = node->priv_data;
+
+    CVPixelBufferRef cvpixbuf = (CVPixelBufferRef)frame->data;
+    CVPixelBufferLockBaseAddress(cvpixbuf, kCVPixelBufferLock_ReadOnly);
+
+    int width = CVPixelBufferGetBytesPerRow(cvpixbuf) >> 2;
+    int height = CVPixelBufferGetHeight(cvpixbuf);
+    float padding = CVPixelBufferGetWidth(cvpixbuf) / (float)width;
+    uint8_t *data = CVPixelBufferGetBaseAddress(cvpixbuf);
+    OSType format = CVPixelBufferGetPixelFormatType(cvpixbuf);
+
+    switch (format) {
+    case kCVPixelFormatType_32BGRA:
+        s->format = GL_BGRA;
+        break;
+    case kCVPixelFormatType_32RGBA:
+        s->format = GL_RGBA;
+        break;
+    default:
+        ngli_assert(0);
+    }
+    s->internal_format = GL_RGBA;
+    s->type = GL_UNSIGNED_BYTE;
+    s->coordinates_matrix[0] = padding;
+
+    gl->BindTexture(GL_TEXTURE_2D, s->id);
+    if (s->width == width && s->height == height)
+        gl->TexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, s->format, s->type, data);
+    else
+        gl->TexImage2D(GL_TEXTURE_2D, 0, s->internal_format, width, height, 0, s->format, s->type, data);
+
+    CVPixelBufferUnlockBaseAddress(cvpixbuf, kCVPixelBufferLock_ReadOnly);
+
+    switch(s->min_filter) {
+    case GL_NEAREST_MIPMAP_NEAREST:
+    case GL_NEAREST_MIPMAP_LINEAR:
+    case GL_LINEAR_MIPMAP_NEAREST:
+    case GL_LINEAR_MIPMAP_LINEAR:
+        gl->GenerateMipmap(GL_TEXTURE_2D);
+        break;
+    }
+    gl->BindTexture(GL_TEXTURE_2D, 0);
+
+    s->width = width;
+    s->height = height;
+}
+#endif
+
+#if defined(TARGET_IPHONE)
+static void handle_vt_frame(struct ngl_node *node, struct sxplayer_frame *frame)
+{
+    struct ngl_ctx *ctx = node->ctx;
+    struct glcontext *glcontext = ctx->glcontext;
+    const struct glfunctions *gl = &glcontext->funcs;
+
+    struct texture *s = node->priv_data;
+
+    CVOpenGLESTextureRef texture = NULL;
+    CVPixelBufferRef cvpixbuf = (CVPixelBufferRef)frame->data;
+
+    int width = CVPixelBufferGetBytesPerRow(cvpixbuf) >> 2;
+    int height = CVPixelBufferGetHeight(cvpixbuf);
+    float padding = CVPixelBufferGetWidth(cvpixbuf) / (float)width;
+    OSType format = CVPixelBufferGetPixelFormatType(cvpixbuf);
+
+    switch (format) {
+    case kCVPixelFormatType_32BGRA:
+        s->format = GL_BGRA;
+        break;
+    case kCVPixelFormatType_32RGBA:
+        s->format = GL_RGBA;
+        break;
+    default:
+        ngli_assert(0);
+    }
+    s->internal_format = GL_RGBA;
+    s->type = GL_UNSIGNED_BYTE;
+    s->coordinates_matrix[0] = padding;
+
+    CVReturn err = CVOpenGLESTextureCacheCreateTextureFromImage(kCFAllocatorDefault,
+                                                                s->cache,
+                                                                cvpixbuf,
+                                                                NULL,
+                                                                GL_TEXTURE_2D,
+                                                                s->internal_format,
+                                                                width,
+                                                                height,
+                                                                s->format,
+                                                                s->type,
+                                                                0,
+                                                                &texture);
+
+    if (err != noErr) {
+        LOG(ERROR, "Could not create CoreVideo texture from image: %d", err);
+        s->id = s->local_id;
+        return;
+    }
+
+    if (s->texture)
+        CFRelease(s->texture);
+
+    s->texture = texture;
+    s->id = s->media_id = CVOpenGLESTextureGetName(texture);
+
+    gl->BindTexture(GL_TEXTURE_2D, s->id);
+    gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, s->min_filter);
+    gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, s->mag_filter);
+    gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, s->wrap_s);
+    gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, s->wrap_t);
+    switch(s->min_filter) {
+    case GL_NEAREST_MIPMAP_NEAREST:
+    case GL_NEAREST_MIPMAP_LINEAR:
+    case GL_LINEAR_MIPMAP_NEAREST:
+    case GL_LINEAR_MIPMAP_LINEAR:
+        gl->GenerateMipmap(GL_TEXTURE_2D);
+        break;
+    }
+    gl->BindTexture(GL_TEXTURE_2D, 0);
+
+    s->width = width;
+    s->height = height;
+}
+#endif
+
+static void handle_frame(struct ngl_node *node, struct sxplayer_frame *frame)
+{
+    struct ngl_ctx *ctx = node->ctx;
+    struct glcontext *glcontext = ctx->glcontext;
+    const struct glfunctions *gl = &glcontext->funcs;
+
+    struct texture *s = node->priv_data;
+
+    int width = frame->linesize >> 2;
+    int height = frame->height;
+    float padding = frame->width / (float)width;
+
+    s->id = s->local_id;
+    s->target = s->local_target;
+    s->coordinates_matrix[0] = padding;
+
+    switch (frame->pix_fmt) {
+    case SXPLAYER_PIXFMT_RGBA:
+        s->format = GL_RGBA;
+        s->internal_format = GL_RGBA;
+        s->type = GL_UNSIGNED_BYTE;
+        break;
+    case SXPLAYER_PIXFMT_BGRA:
+        s->format = GL_BGRA;
+        s->internal_format = GL_RGBA;
+        s->type = GL_UNSIGNED_BYTE;
+        break;
+    case SXPLAYER_SMPFMT_FLT:
+        s->format = GL_RED;
+        s->internal_format = GL_R32F;
+        s->type = GL_FLOAT;
+        break;
+    default:
+        ngli_assert(0);
+    }
+
+    gl->BindTexture(GL_TEXTURE_2D, s->id);
+    if (s->width == width && s->height == height) {
+        gl->TexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, s->format, s->type, frame->data);
+    } else {
+        gl->TexImage2D(GL_TEXTURE_2D, 0, s->internal_format, width, height, 0, s->format, s->type, frame->data);
+    }
+
+    switch(s->min_filter) {
+    case GL_NEAREST_MIPMAP_NEAREST:
+    case GL_NEAREST_MIPMAP_LINEAR:
+    case GL_LINEAR_MIPMAP_NEAREST:
+    case GL_LINEAR_MIPMAP_LINEAR:
+        gl->GenerateMipmap(GL_TEXTURE_2D);
+        break;
+    }
+    gl->BindTexture(GL_TEXTURE_2D, 0);
+
+    s->width = width;
+    s->height = height;
+}
+
+static void handle_media_frame(struct ngl_node *node)
+{
+    struct texture *s = node->priv_data;
+    struct media *media = s->data_src->priv_data;
     struct sxplayer_frame *frame = media->frame;
 
     if (frame) {
-        if (frame->pix_fmt == SXPLAYER_PIXFMT_MEDIACODEC) {
-#ifdef __ANDROID__
-            AVMediaCodecBuffer *buffer = (AVMediaCodecBuffer *)frame->data;
-            float matrix[4*4] = {
-                1.0f,  0.0f, 0.0f, 0.0f,
-                0.0f, -1.0f, 0.0f, 0.0f,
-                0.0f,  0.0f, 1.0f, 0.0f,
-                0.0f,  1.0f, 0.0f, 1.0f,
-            };
-
-            av_android_surface_render_buffer(media->android_surface, buffer, s->coordinates_matrix);
-            ngli_mat4_mul(s->coordinates_matrix, matrix, s->coordinates_matrix);
-
-            s->id = s->media_id = media->android_texture_id;
-            s->target = s->media_target = media->android_texture_target;
-            s->width = frame->width;
-            s->height = frame->height;
+        switch(frame->pix_fmt) {
+#if defined(TARGET_ANDROID)
+        case SXPLAYER_PIXFMT_MEDIACODEC:
+            handle_mc_frame(node, frame);
+            break;
+#elif defined(TARGET_DARWIN) || defined(TARGET_IPHONE)
+        case SXPLAYER_PIXFMT_VT:
+            handle_vt_frame(node, frame);
+            break;
 #endif
-        } else if (frame->pix_fmt == SXPLAYER_PIXFMT_VT) {
-#if defined(TARGET_DARWIN)
-            CVPixelBufferRef cvpixbuf = (CVPixelBufferRef)frame->data;
-            CVPixelBufferLockBaseAddress(cvpixbuf, kCVPixelBufferLock_ReadOnly);
-
-            int width = CVPixelBufferGetBytesPerRow(cvpixbuf) >> 2;
-            int height = CVPixelBufferGetHeight(cvpixbuf);
-            float padding = CVPixelBufferGetWidth(cvpixbuf) / (float)width;
-            uint8_t *data = CVPixelBufferGetBaseAddress(cvpixbuf);
-
-            s->id = s->local_id;
-            s->target = s->local_target;
-            s->format = GL_BGRA;
-            s->internal_format = GL_RGBA;
-            s->coordinates_matrix[0] = padding;
-
-            gl->BindTexture(GL_TEXTURE_2D, s->id);
-            if (s->width == width && s->height == height) {
-                gl->TexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, s->format, s->type, data);
-            } else {
-                gl->TexImage2D(GL_TEXTURE_2D, 0, s->internal_format, width, height, 0, s->format, s->type, data);
-            }
-
-            CVPixelBufferUnlockBaseAddress(cvpixbuf, kCVPixelBufferLock_ReadOnly);
-
-            switch(s->min_filter) {
-            case GL_NEAREST_MIPMAP_NEAREST:
-            case GL_NEAREST_MIPMAP_LINEAR:
-            case GL_LINEAR_MIPMAP_NEAREST:
-            case GL_LINEAR_MIPMAP_LINEAR:
-                gl->GenerateMipmap(GL_TEXTURE_2D);
-                break;
-            }
-            gl->BindTexture(GL_TEXTURE_2D, 0);
-
-            s->width = width;
-            s->height = height;
-#elif defined(TARGET_IPHONE)
-            CVOpenGLESTextureRef texture = NULL;
-            CVPixelBufferRef cvpixbuf = (CVPixelBufferRef)frame->data;
-
-            int width = CVPixelBufferGetBytesPerRow(cvpixbuf) >> 2;
-            int height = CVPixelBufferGetHeight(cvpixbuf);
-            float padding = CVPixelBufferGetWidth(cvpixbuf) / (float)width;
-
-            CVReturn err = CVOpenGLESTextureCacheCreateTextureFromImage(kCFAllocatorDefault,
-                                                                        s->cache,
-                                                                        cvpixbuf,
-                                                                        NULL,
-                                                                        GL_TEXTURE_2D,
-                                                                        GL_RGBA,
-                                                                        width,
-                                                                        height,
-                                                                        GL_BGRA,
-                                                                        GL_UNSIGNED_BYTE,
-                                                                        0,
-                                                                        &texture);
-
-            if (err != noErr) {
-                LOG(ERROR, "Could not create CoreVideo texture from image: %d", err);
-                s->id = s->local_id;
-                return;
-            }
-
-            if (s->texture)
-                CFRelease(s->texture);
-
-            s->texture = texture;
-            s->id = s->media_id = CVOpenGLESTextureGetName(texture);
-            s->target = GL_TEXTURE_2D;
-            s->format = GL_BGRA;
-            s->internal_format = GL_RGBA;
-            s->coordinates_matrix[0] = padding;
-
-            gl->BindTexture(GL_TEXTURE_2D, s->id);
-            gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, s->min_filter);
-            gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, s->mag_filter);
-            gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, s->wrap_s);
-            gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, s->wrap_t);
-            gl->BindTexture(GL_TEXTURE_2D, 0);
-
-            s->width = width;
-            s->height = height;
-#endif
-        } else {
-            int width = frame->linesize >> 2;
-            int height = frame->height;
-            float padding = frame->width / (float)width;
-
-            s->id = s->local_id;
-            s->target = s->local_target;
-            s->coordinates_matrix[0] = padding;
-
-            gl->BindTexture(GL_TEXTURE_2D, s->id);
-            if (s->width == width && s->height == height) {
-                gl->TexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, s->format, s->type, frame->data);
-            } else {
-                gl->TexImage2D(GL_TEXTURE_2D, 0, s->internal_format, width, height, 0, s->format, s->type, frame->data);
-            }
-
-            switch(s->min_filter) {
-            case GL_NEAREST_MIPMAP_NEAREST:
-            case GL_NEAREST_MIPMAP_LINEAR:
-            case GL_LINEAR_MIPMAP_NEAREST:
-            case GL_LINEAR_MIPMAP_LINEAR:
-                gl->GenerateMipmap(GL_TEXTURE_2D);
-                break;
-            }
-            gl->BindTexture(GL_TEXTURE_2D, 0);
-
-            s->width = width;
-            s->height = height;
+        default:
+            handle_frame(node, frame);
+            break;
         }
 
         sxplayer_release_frame(media->frame);
