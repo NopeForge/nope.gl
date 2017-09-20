0b496a4aa959355b9a2a6862edcc5108d5855a91
Clément Bœsch <u@pkh.me>
2017-09-20 13:54:27 +0200

    nodes_register: sort nodes alphabetically

--- libnodegl/nodes_register.h
+++ libnodegl/nodes_register.h
@@ -23,42 +23,42 @@
 #define NODES_REGISTER_H
 
 #define NODE_MAP_TYPE2CLASS(action)                                             \
+    action(NGL_NODE_ANIMATIONSCALAR,        ngli_animationscalar_class)         \
+    action(NGL_NODE_ANIMATIONVEC2,          ngli_animationvec2_class)           \
+    action(NGL_NODE_ANIMATIONVEC3,          ngli_animationvec3_class)           \
+    action(NGL_NODE_ANIMATIONVEC4,          ngli_animationvec4_class)           \
+    action(NGL_NODE_ANIMKEYFRAMESCALAR,     ngli_animkeyframescalar_class)      \
+    action(NGL_NODE_ANIMKEYFRAMEVEC2,       ngli_animkeyframevec2_class)        \
+    action(NGL_NODE_ANIMKEYFRAMEVEC3,       ngli_animkeyframevec3_class)        \
+    action(NGL_NODE_ANIMKEYFRAMEVEC4,       ngli_animkeyframevec4_class)        \
     action(NGL_NODE_CAMERA,                 ngli_camera_class)                  \
-    action(NGL_NODE_TEXTURE,                ngli_texture_class)                 \
-    action(NGL_NODE_MEDIA,                  ngli_media_class)                   \
-    action(NGL_NODE_GLSTATE,                ngli_glstate_class)                 \
+    action(NGL_NODE_FPS,                    ngli_fps_class)                     \
     action(NGL_NODE_GLBLENDSTATE,           ngli_glblendstate_class)            \
     action(NGL_NODE_GLCOLORSTATE,           ngli_glcolorstate_class)            \
+    action(NGL_NODE_GLSTATE,                ngli_glstate_class)                 \
     action(NGL_NODE_GLSTENCILSTATE,         ngli_glstencilstate_class)          \
     action(NGL_NODE_GROUP,                  ngli_group_class)                   \
-    action(NGL_NODE_TEXTUREDSHAPE,          ngli_texturedshape_class)           \
+    action(NGL_NODE_IDENTITY,               ngli_identity_class)                \
+    action(NGL_NODE_MEDIA,                  ngli_media_class)                   \
     action(NGL_NODE_QUAD,                   ngli_quad_class)                    \
-    action(NGL_NODE_TRIANGLE,               ngli_triangle_class)                \
-    action(NGL_NODE_SHAPEPRIMITIVE,         ngli_shapeprimitive_class)          \
-    action(NGL_NODE_SHAPE,                  ngli_shape_class)                   \
-    action(NGL_NODE_SHADER,                 ngli_shader_class)                  \
     action(NGL_NODE_RENDERRANGECONTINUOUS,  ngli_renderrangecontinuous_class)   \
     action(NGL_NODE_RENDERRANGENORENDER,    ngli_renderrangenorender_class)     \
     action(NGL_NODE_RENDERRANGEONCE,        ngli_renderrangeonce_class)         \
     action(NGL_NODE_ROTATE,                 ngli_rotate_class)                  \
     action(NGL_NODE_RTT,                    ngli_rtt_class)                     \
-    action(NGL_NODE_TRANSLATE,              ngli_translate_class)               \
     action(NGL_NODE_SCALE,                  ngli_scale_class)                   \
-    action(NGL_NODE_ANIMKEYFRAMESCALAR,     ngli_animkeyframescalar_class)      \
-    action(NGL_NODE_ANIMKEYFRAMEVEC2,       ngli_animkeyframevec2_class)        \
-    action(NGL_NODE_ANIMKEYFRAMEVEC3,       ngli_animkeyframevec3_class)        \
-    action(NGL_NODE_ANIMKEYFRAMEVEC4,       ngli_animkeyframevec4_class)        \
+    action(NGL_NODE_SHADER,                 ngli_shader_class)                  \
+    action(NGL_NODE_SHAPE,                  ngli_shape_class)                   \
+    action(NGL_NODE_SHAPEPRIMITIVE,         ngli_shapeprimitive_class)          \
+    action(NGL_NODE_TEXTURE,                ngli_texture_class)                 \
+    action(NGL_NODE_TEXTUREDSHAPE,          ngli_texturedshape_class)           \
+    action(NGL_NODE_TRANSLATE,              ngli_translate_class)               \
+    action(NGL_NODE_TRIANGLE,               ngli_triangle_class)                \
+    action(NGL_NODE_UNIFORMINT,             ngli_uniformint_class)              \
+    action(NGL_NODE_UNIFORMMAT4,            ngli_uniformmat4_class)             \
     action(NGL_NODE_UNIFORMSCALAR,          ngli_uniformscalar_class)           \
     action(NGL_NODE_UNIFORMVEC2,            ngli_uniformvec2_class)             \
     action(NGL_NODE_UNIFORMVEC3,            ngli_uniformvec3_class)             \
     action(NGL_NODE_UNIFORMVEC4,            ngli_uniformvec4_class)             \
-    action(NGL_NODE_UNIFORMMAT4,            ngli_uniformmat4_class)             \
-    action(NGL_NODE_UNIFORMINT,             ngli_uniformint_class)              \
-    action(NGL_NODE_FPS,                    ngli_fps_class)                     \
-    action(NGL_NODE_IDENTITY,               ngli_identity_class)                \
-    action(NGL_NODE_ANIMATIONSCALAR,        ngli_animationscalar_class)         \
-    action(NGL_NODE_ANIMATIONVEC2,          ngli_animationvec2_class)           \
-    action(NGL_NODE_ANIMATIONVEC3,          ngli_animationvec3_class)           \
-    action(NGL_NODE_ANIMATIONVEC4,          ngli_animationvec4_class)           \
 
 #endif
