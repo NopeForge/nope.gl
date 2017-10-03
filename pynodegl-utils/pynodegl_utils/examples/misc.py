29b8dfaad5ccad7309b12bf236024c3eda370236
Clément Bœsch <u@pkh.me>
2017-10-04 00:15:42 +0200

    utils/examples: add square2circle morphing demo

--- pynodegl-utils/pynodegl_utils/examples/misc.py
+++ pynodegl-utils/pynodegl_utils/examples/misc.py
@@ -6,9 +6,11 @@ from OpenGL import GL
 
 from pynodegl import (
         AnimKeyFrameVec3,
+        AnimKeyFrameVec4,
         AnimatedBufferVec3,
         AnimKeyFrameBuffer,
         AnimationVec3,
+        AnimationVec4,
         Camera,
         Circle,
         Geometry,
@@ -21,10 +23,76 @@ from pynodegl import (
         Texture,
         Translate,
         Triangle,
+        UniformScalar,
         UniformVec4,
 )
 
 from pynodegl_utils.misc import scene
+
+
+@scene({'name': 'square_color', 'type': 'color'},
+       {'name': 'circle_color', 'type': 'color'})
+def square2circle(cfg, square_color=(0.9, 0.1, 0.3, 1.0), circle_color=(1.0, 1.0, 1.0, 1.0)):
+    cfg.duration = 5
+
+    frag = '''#version 100
+precision mediump float;
+uniform vec4 color;
+void main(void)
+{
+    gl_FragColor = color;
+}'''
+
+    def sqxf(t): # square x coordinates clockwise starting top-left
+        if t < 1/4.: return t*4
+        if t < 1/2.: return 1
+        if t < 3/4.: return 1.-(t-.5)*4
+        return 0
+
+    def sqyf(t): # square y coordinates clockwise starting top-left
+        if t < 1/4.: return 1
+        if t < 1/2.: return 1.-(t-.25)*4
+        if t < 3/4.: return 0
+        return (t-.75)*4
+
+    n = 1024 # number of vertices
+    s = 1.25 # shapes scale
+    interp = 'exp_in_out'
+
+    square_vertices = array.array('f')
+    for i in range(n):
+        x = (sqxf(i / float(n)) - .5) * s
+        y = (sqyf(i / float(n)) - .5) * s
+        square_vertices.extend([x, y, 0])
+
+    circle_vertices = array.array('f')
+    step = 2 * math.pi / float(n)
+    for i in range(n):
+        angle = i * step - math.pi/4.
+        x = math.sin(angle) * .5 * s
+        y = math.cos(angle) * .5 * s
+        circle_vertices.extend([x, y, 0])
+
+    vertices_animkf = [
+            AnimKeyFrameBuffer(0,               square_vertices),
+            AnimKeyFrameBuffer(cfg.duration/2., circle_vertices, interp),
+            AnimKeyFrameBuffer(cfg.duration,    square_vertices, interp),
+    ]
+    vertices = AnimatedBufferVec3(vertices_animkf)
+
+    color_animkf = [
+            AnimKeyFrameVec4(0,               square_color),
+            AnimKeyFrameVec4(cfg.duration/2., circle_color, interp),
+            AnimKeyFrameVec4(cfg.duration,    square_color, interp),
+    ]
+    ucolor = UniformVec4(anim=AnimationVec4(color_animkf))
+
+    geom = Geometry(vertices)
+    geom.set_draw_mode(GL.GL_TRIANGLE_FAN)
+    p = Program(fragment=frag)
+    render = Render(geom, p)
+    render.update_uniforms(color=ucolor)
+    return render
 
 def _get_vertices(n, radius_func, offset=0):
     vertices = []
