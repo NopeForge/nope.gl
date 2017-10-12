710b1cbc7dfe5dc9b32ae925cdd2707fd93250e8
Clément Bœsch <u@pkh.me>
2017-10-12 09:00:04 +0200

    math: support 0-length vector in ngli_vec3_norm()

--- libnodegl/math_utils.c
+++ libnodegl/math_utils.c
@@ -39,6 +39,11 @@ void ngli_vec3_sub(float *dst, float *v1, float *v2)
 
 void ngli_vec3_norm(float *dst, float *v)
 {
+    if (!v[0] && !v[1] && !v[2]) {
+        dst[0] = dst[1] = dst[2] = 0;
+        return;
+    }
+
     const float l = 1.0f / ngli_vec3_length(v);
 
     dst[0] = v[0] * l;
