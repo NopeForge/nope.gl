9c527e920d66ceab013990f907d83f0adebbb3dd
Clément Bœsch <cboesch@gopro.com>
2017-10-30 18:01:20 +0100

    utils/examples: simplify final color computation in tex-mix shader

--- pynodegl-utils/pynodegl_utils/examples/shaders/tex-mix.glsl
+++ pynodegl-utils/pynodegl_utils/examples/shaders/tex-mix.glsl
@@ -9,12 +9,6 @@ void main(void)
 {
     vec4 c1 = texture2D(tex0_sampler, var_tex0_coord);
     vec4 c2 = texture2D(tex1_sampler, var_tex1_coord);
-    vec4 c3 = vec4(
-        c1.r * delta + c2.r * (1.0 - delta),
-        c1.g * delta + c2.g * (1.0 - delta),
-        c1.b * delta + c2.b * (1.0 - delta),
-        1.0
-    );
-
+    vec4 c3 = vec4(vec3(c1.rgb * delta + c2.rgb * (1.0 - delta)), 1.0);
     gl_FragColor = c3;
 }
