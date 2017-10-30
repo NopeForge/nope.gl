a464d80129092b30615602eee038f6c826e03cd5
Clément Bœsch <cboesch@gopro.com>
2017-10-30 17:55:43 +0100

    utils/examples/particles: move compute shader to a dedicated file

--- pynodegl-utils/pynodegl_utils/examples/misc.py
+++ pynodegl-utils/pynodegl_utils/examples/misc.py
@@ -174,81 +174,8 @@ def particules(cfg, particules=32):
     random.seed(0)
 
     compute_data_version = "310 es" if cfg.glbackend == "gles" else "430"
-    compute_data = '''
-#version %s
-
-layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
-
-layout (std430, binding = 0) buffer ipositions_buffer {
-    vec3 ipositions[];
-};
-
-layout (std430, binding = 1) buffer ivelocities_buffer {
-    vec2 ivelocities[];
-};
-
-layout (std430, binding = 2) buffer opositions_buffer {
-    vec3 opositions[];
-};
-
-uniform float time;
-uniform float duration;
-
-float bounceOut(float t)
-{
-    float c = 1.0;
-    float a = 1.70158;
-
-    if (t >= 1.0) {
-        return c;
-    } else if (t < 4.0 / 11.0) {
-        return c * (7.5625 * t * t);
-    } else if (t < 8.0 / 11.0) {
-        t -= 6.0 / 11.0;
-        return -a * (1.0 - (7.5625 * t * t + 0.75)) + c;
-    } else if (t < 10.0 / 11.0) {
-        t -= 9.0 / 11.0;
-        return -a * (1.0 - (7.5625 * t * t + 0.9375)) + c;
-    } else {
-        t -= 21.0 / 22.0;
-        return -a * (1.0 - (7.5625 * t * t + 0.984375)) + c;
-    }
-}
-
-float bounce(float t)
-{
-    return 1.0 - bounceOut(t);
-}
-
-void main(void)
-{
-    uint i = gl_GlobalInvocationID.x +
-             gl_GlobalInvocationID.y * 1024U;
-
-    vec3 iposition = ipositions[i];
-    vec2 ivelocity = ivelocities[i];
-    float step = time * duration * 30.0;
-    vec2 velocity = ivelocity;
-    vec3 position = iposition;
-    float yoffset = 1.0 - iposition.y;
-    float speed = 1.0 + ivelocity.y;
-
-    position.x = iposition.x + step * velocity.x;
-    position.y = ((2.0 - yoffset) * bounce(time * speed * (1.0  + yoffset))) - 0.99;
-
-    opositions[i] = position;
-}
-''' % (compute_data_version)
-
-    fragment_data = '''
-#version 100
-
-precision highp float;
-
-void main(void)
-{
-    gl_FragColor = vec4(0.0, 0.6, 0.8, 1.0);
-}'''
+    compute_data = '#version %s\n' % compute_data_version
+    compute_data += get_shader('particules')
 
     cfg.duration = 6
 
