ada207df64a525d40699f8fd03277b620ddd1d6a
Matthieu Bouron <matthieu.bouron@gmail.com>
2017-11-12 13:02:49 +0100

    ngl-tools: set default OpenGL version to 4.1 on all platforms
    Target by default the same OpenGL version that the one given by recent
    OSX releases. Good drivers will anyway give us an higher OpenGL version
    which is a superset of the one we request on other platforms.
    
    Fixes rendering of scenes using compute shaders as we previously were
    requesting an OpenGL version of 3.0 preventing the driver to give us a
    context potentially compatible with compute shaders.

--- ngl-tools/common.c
+++ ngl-tools/common.c
@@ -48,14 +48,11 @@ int init_glfw(void)
         return -1;
     }
 
-#ifdef __APPLE__
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+#ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#else
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 #endif
 
     return 0;
