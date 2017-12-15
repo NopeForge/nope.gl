NopeForge/nope.gl

commit 6ccee9ea299249aadc6ef2a87b137a6d3dd1011e

anim: add doc string for the ref doc

libnodegl/node_animkeyframe.c
@@ -34,45 +34,27 @@
 #include "utils.h"
 
 #define OFFSET(x) offsetof(struct animkeyframe, x)
-static const struct node_param animkeyframefloat_params[] = {
-    {"time",   PARAM_TYPE_DBL,  OFFSET(time),   .flags=PARAM_FLAG_CONSTRUCTOR},
-    {"value",  PARAM_TYPE_DBL,  OFFSET(scalar), .flags=PARAM_FLAG_CONSTRUCTOR},
-    {"easing", PARAM_TYPE_STR,  OFFSET(easing), {.str="linear"}},
-    {"easing_args", PARAM_TYPE_DBLLIST, OFFSET(args)},
-    {NULL}
-};
 
-static const struct node_param animkeyframevec2_params[] = {
-    {"time",   PARAM_TYPE_DBL,  OFFSET(time),   .flags=PARAM_FLAG_CONSTRUCTOR},
-    {"value",  PARAM_TYPE_VEC2, OFFSET(value),  .flags=PARAM_FLAG_CONSTRUCTOR},
-    {"easing", PARAM_TYPE_STR,  OFFSET(easing), {.str="linear"}},
-    {"easing_args", PARAM_TYPE_DBLLIST, OFFSET(args)},
-    {NULL}
-};
+#define ANIMKEYFRAME_PARAMS(id, value_data_key, value_data_type, value_data_field)              \
+static const struct node_param animkeyframe##id##_params[] = {                                  \
+    {"time",          PARAM_TYPE_DBL, OFFSET(time),                                             \
+                      .flags=PARAM_FLAG_CONSTRUCTOR,                                            \
+                      .desc=NGLI_DOCSTRING("the time key point in seconds")},                   \
+    {#value_data_key, value_data_type, OFFSET(value_data_field),                                \
+                      .flags=PARAM_FLAG_CONSTRUCTOR,                                            \
+                      .desc=NGLI_DOCSTRING("the " #value_data_key " at time `time`")},          \
+    {"easing",        PARAM_TYPE_STR,  OFFSET(easing), {.str="linear"},                         \
+                      .desc=NGLI_DOCSTRING("a string identifying the interpolation")},          \
+    {"easing_args",   PARAM_TYPE_DBLLIST, OFFSET(args),                                         \
+                      .desc=NGLI_DOCSTRING("a list of arguments some easings may use")},        \
+    {NULL}                                                                                      \
+}
 
-static const struct node_param animkeyframevec3_params[] = {
-    {"time",   PARAM_TYPE_DBL,  OFFSET(time),   .flags=PARAM_FLAG_CONSTRUCTOR},
-    {"value",  PARAM_TYPE_VEC3, OFFSET(value),  .flags=PARAM_FLAG_CONSTRUCTOR},
-    {"easing", PARAM_TYPE_STR,  OFFSET(easing), {.str="linear"}},
-    {"easing_args", PARAM_TYPE_DBLLIST, OFFSET(args)},
-    {NULL}
-};
-
-static const struct node_param animkeyframevec4_params[] = {
-    {"time",   PARAM_TYPE_DBL,  OFFSET(time),   .flags=PARAM_FLAG_CONSTRUCTOR},
-    {"value",  PARAM_TYPE_VEC4, OFFSET(value),  .flags=PARAM_FLAG_CONSTRUCTOR},
-    {"easing", PARAM_TYPE_STR,  OFFSET(easing), {.str="linear"}},
-    {"easing_args", PARAM_TYPE_DBLLIST, OFFSET(args)},
-    {NULL}
-};
-
-static const struct node_param animkeyframebuffer_params[] = {
-    {"time",        PARAM_TYPE_DBL,     OFFSET(time), .flags=PARAM_FLAG_CONSTRUCTOR},
-    {"data",        PARAM_TYPE_DATA,    OFFSET(data)},
-    {"easing",      PARAM_TYPE_STR,     OFFSET(easing), {.str="linear"}},
-    {"easing_args", PARAM_TYPE_DBLLIST, OFFSET(args)},
-    {NULL}
-};
+ANIMKEYFRAME_PARAMS(float, value, PARAM_TYPE_DBL, scalar);
+ANIMKEYFRAME_PARAMS(vec2,  value, PARAM_TYPE_DBL, value);
+ANIMKEYFRAME_PARAMS(vec3,  value, PARAM_TYPE_DBL, value);
+ANIMKEYFRAME_PARAMS(vec4,  value, PARAM_TYPE_DBL, value);
+ANIMKEYFRAME_PARAMS(buffer, data, PARAM_TYPE_DATA, data);
 
 #ifdef __ANDROID__
 #define log2(x)  (log(x) / log(2))

libnodegl/node_animatedbuffer.c
@@ -29,7 +29,8 @@
 static const struct node_param animatedbuffer_params[] = {
     {"keyframes", PARAM_TYPE_NODELIST, OFFSET(animkf),
                   .node_types=(const int[]){NGL_NODE_ANIMKEYFRAMEBUFFER, -1},
-                  .flags=PARAM_FLAG_DOT_DISPLAY_PACKED},
+                  .flags=PARAM_FLAG_DOT_DISPLAY_PACKED,
+                  .desc=NGLI_DOCSTRING("key frame buffers to interpolate from")},
     {NULL}
 };
 

libnodegl/node_animation.c
@@ -28,25 +28,29 @@
 #define OFFSET(x) offsetof(struct animation, x)
 static const struct node_param animatedfloat_params[] = {
     {"keyframes", PARAM_TYPE_NODELIST, OFFSET(animkf), .flags=PARAM_FLAG_DOT_DISPLAY_PACKED,
-                  .node_types=(const int[]){NGL_NODE_ANIMKEYFRAMEFLOAT, -1}},
+                  .node_types=(const int[]){NGL_NODE_ANIMKEYFRAMEFLOAT, -1},
+                  .desc=NGLI_DOCSTRING("float key frames to interpolate from")},
     {NULL}
 };
 
 static const struct node_param animatedvec2_params[] = {
     {"keyframes", PARAM_TYPE_NODELIST, OFFSET(animkf), .flags=PARAM_FLAG_DOT_DISPLAY_PACKED,
-                  .node_types=(const int[]){NGL_NODE_ANIMKEYFRAMEVEC2, -1}},
+                  .node_types=(const int[]){NGL_NODE_ANIMKEYFRAMEVEC2, -1},
+                  .desc=NGLI_DOCSTRING("vec2 key frames to interpolate from")},
     {NULL}
 };
 
 static const struct node_param animatedvec3_params[] = {
     {"keyframes", PARAM_TYPE_NODELIST, OFFSET(animkf), .flags=PARAM_FLAG_DOT_DISPLAY_PACKED,
-                  .node_types=(const int[]){NGL_NODE_ANIMKEYFRAMEVEC3, -1}},
+                  .node_types=(const int[]){NGL_NODE_ANIMKEYFRAMEVEC3, -1},
+                  .desc=NGLI_DOCSTRING("vec3 key frames to interpolate from")},
     {NULL}
 };
 
 static const struct node_param animatedvec4_params[] = {
     {"keyframes", PARAM_TYPE_NODELIST, OFFSET(animkf), .flags=PARAM_FLAG_DOT_DISPLAY_PACKED,
-                  .node_types=(const int[]){NGL_NODE_ANIMKEYFRAMEVEC4, -1}},
+                  .node_types=(const int[]){NGL_NODE_ANIMKEYFRAMEVEC4, -1},
+                  .desc=NGLI_DOCSTRING("vec4 key frames to interpolate from")},
     {NULL}
 };
 

commit aa1b72312871afbd37a41aa31ce99d186031567c

anim: fix data/value types
Regression since 6ccee9ea299249aadc6ef2a87b137a6d3dd1011e.

libnodegl/node_animkeyframe.c
@@ -35,13 +35,13 @@
 
 #define OFFSET(x) offsetof(struct animkeyframe, x)
 
-#define ANIMKEYFRAME_PARAMS(id, value_data_key, value_data_type, value_data_field)              \
+#define ANIMKEYFRAME_PARAMS(id, value_data_key, value_data_type, value_data_field, value_data_flags) \
 static const struct node_param animkeyframe##id##_params[] = {                                  \
     {"time",          PARAM_TYPE_DBL, OFFSET(time),                                             \
                       .flags=PARAM_FLAG_CONSTRUCTOR,                                            \
                       .desc=NGLI_DOCSTRING("the time key point in seconds")},                   \
     {#value_data_key, value_data_type, OFFSET(value_data_field),                                \
-                      .flags=PARAM_FLAG_CONSTRUCTOR,                                            \
+                      .flags=value_data_flags,                                                  \
                       .desc=NGLI_DOCSTRING("the " #value_data_key " at time `time`")},          \
     {"easing",        PARAM_TYPE_STR,  OFFSET(easing), {.str="linear"},                         \
                       .desc=NGLI_DOCSTRING("a string identifying the interpolation")},          \
@@ -50,11 +50,11 @@ static const struct node_param animkeyframe##id##_params[] = {                  
     {NULL}                                                                                      \
 }
 
-ANIMKEYFRAME_PARAMS(float, value, PARAM_TYPE_DBL, scalar);
-ANIMKEYFRAME_PARAMS(vec2,  value, PARAM_TYPE_DBL, value);
-ANIMKEYFRAME_PARAMS(vec3,  value, PARAM_TYPE_DBL, value);
-ANIMKEYFRAME_PARAMS(vec4,  value, PARAM_TYPE_DBL, value);
-ANIMKEYFRAME_PARAMS(buffer, data, PARAM_TYPE_DATA, data);
+ANIMKEYFRAME_PARAMS(float, value, PARAM_TYPE_DBL, scalar, PARAM_FLAG_CONSTRUCTOR);
+ANIMKEYFRAME_PARAMS(vec2,  value, PARAM_TYPE_VEC2, value, PARAM_FLAG_CONSTRUCTOR);
+ANIMKEYFRAME_PARAMS(vec3,  value, PARAM_TYPE_VEC3, value, PARAM_FLAG_CONSTRUCTOR);
+ANIMKEYFRAME_PARAMS(vec4,  value, PARAM_TYPE_VEC4, value, PARAM_FLAG_CONSTRUCTOR);
+ANIMKEYFRAME_PARAMS(buffer, data, PARAM_TYPE_DATA, data, 0);
 
 #ifdef __ANDROID__
 #define log2(x)  (log(x) / log(2))