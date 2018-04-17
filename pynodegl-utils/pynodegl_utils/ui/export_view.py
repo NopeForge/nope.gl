198ae196eb1e96ff7f138fdf277ef305af760db3
Clément Bœsch <cboesch@gopro.com>
2018-04-17 15:55:36 +0200

    utils/viewer: remove encoder list from export view
    This is redundant with the encoder arguments.

--- pynodegl-utils/pynodegl_utils/ui/export_view.py
+++ pynodegl-utils/pynodegl_utils/ui/export_view.py
@@ -47,9 +47,6 @@ class ExportView(QtWidgets.QWidget):
         self._spinbox_height.setRange(1, 0xffff)
         self._spinbox_height.setValue(600)
 
-        self._encoders_cbox = QtWidgets.QComboBox()
-        self._encoders_cbox.addItems(self._get_encoders_list())
-
         self._encopts_text = QtWidgets.QLineEdit()
 
         self._export_btn = QtWidgets.QPushButton('Export')
@@ -61,7 +58,6 @@ class ExportView(QtWidgets.QWidget):
         form.addRow('Filename:', file_box)
         form.addRow('Width:',    self._spinbox_width)
         form.addRow('Height:',   self._spinbox_height)
-        form.addRow('Encoder:',  self._encoders_cbox)
         form.addRow('Extra encoder arguments:', self._encopts_text)
         form.addRow(self._export_btn)
         form.addRow(self._pgbar)
@@ -72,33 +68,13 @@ class ExportView(QtWidgets.QWidget):
         ofile_btn.clicked.connect(self._select_ofile)
         self._export_btn.clicked.connect(self._export)
 
-    def _get_encoders_list(self):
-        encoders = ['']
-        stdout = subprocess.check_output(['ffmpeg', '-v', '0', '-encoders'])
-        list_started = False
-        for line in stdout.splitlines():
-            line = line.lstrip()
-            if not list_started:
-                if line.startswith('------'):
-                    list_started = True
-                continue
-            caps, encoder, desc = line.split(None, 2)
-            if caps[0] != 'V':
-                continue
-            encoders.append(encoder)
-        return encoders
-
     @QtCore.pyqtSlot()
     def _export(self):
         ofile = self._ofile_text.text()
         width = self._spinbox_width.value()
         height = self._spinbox_height.value()
 
-        extra_enc_args = []
-        encoder_id = self._encoders_cbox.currentIndex()
-        if encoder_id:
-            extra_enc_args += ['-c:v', self._encoders_cbox.itemText(encoder_id)]
-        extra_enc_args += self._encopts_text.text().split()
+        extra_enc_args = self._encopts_text.text().split()
 
         self._pgbar.setValue(0)
         self._pgbar.show()
