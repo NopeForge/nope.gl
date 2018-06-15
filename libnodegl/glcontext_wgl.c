77fafc34948e7ae8f14bd7962d7e8f0b2795059e
Matthieu Bouron <matthieu.bouron@gmail.com>
2018-06-15 16:24:26 +0200

    glcontext_wgl: shorten structure and variable names

--- libnodegl/glcontext_wgl.c
+++ libnodegl/glcontext_wgl.c
@@ -28,61 +28,61 @@
 #include "glcontext.h"
 #include "nodegl.h"
 
-struct glcontext_wgl {
+struct wgl_priv {
     HGLRC handle;
     HDC window;
     HMODULE module;
 };
 
-static int glcontext_wgl_init(struct glcontext *glcontext, void *display, void *window, void *handle)
+static int wgl_init(struct glcontext *ctx, void *display, void *window, void *handle)
 {
-    struct glcontext_wgl *glcontext_wgl = glcontext->priv_data;
+    struct wgl_priv *wgl = ctx->priv_data;
 
-    glcontext_wgl->handle = handle ? *(HGLRC *)handle : wglGetCurrentContext();
-    glcontext_wgl->window = window ? *(HDC *)window   : wglGetCurrentDC();
+    wgl->handle = handle ? *(HGLRC *)handle : wglGetCurrentContext();
+    wgl->window = window ? *(HDC *)window   : wglGetCurrentDC();
 
-    if (!glcontext_wgl->window || !glcontext_wgl->handle)
+    if (!wgl->window || !wgl->handle)
         return -1;
 
-    glcontext_wgl->module = LoadLibrary("opengl32.dll");
-    if (!glcontext_wgl->module)
+    wgl->module = LoadLibrary("opengl32.dll");
+    if (!wgl->module)
         return -1;
 
     return 0;
 }
 
-static void glcontext_wgl_uninit(struct glcontext *glcontext)
+static void wgl_uninit(struct glcontext *ctx)
 {
-    struct glcontext_wgl *glcontext_wgl = glcontext->priv_data;
+    struct wgl_priv *wgl = ctx->priv_data;
 
-    if (glcontext_wgl->module)
-        FreeLibrary(glcontext_wgl->module);
+    if (wgl->module)
+        FreeLibrary(wgl->module);
 }
 
-static int glcontext_wgl_create(struct glcontext *glcontext, struct glcontext *other)
+static int wgl_create(struct glcontext *ctx, struct glcontext *other)
 {
     return -1;
 }
 
-static int glcontext_wgl_make_current(struct glcontext *glcontext, int current)
+static int wgl_make_current(struct glcontext *ctx, int current)
 {
     int ret;
-    struct glcontext_wgl *glcontext_wgl = glcontext->priv_data;
+    struct wgl_priv *wgl = ctx->priv_data;
 
     if (current) {
-        ret = wglMakeCurrent(glcontext_wgl->window, glcontext_wgl->handle);
+        ret = wglMakeCurrent(wgl->window, wgl->handle);
     } else {
-        ret = wglMakeCurrent(glcontext_wgl->window, NULL);
+        ret = wglMakeCurrent(wgl->window, NULL);
     }
 
     return ret - 1;
 }
 
-static void *glcontext_wgl_get_proc_address(struct glcontext *glcontext, const char *name)
+static void *wgl_get_proc_address(struct glcontext *ctx, const char *name)
 {
-    struct glcontext_wgl *glcontext_wgl = glcontext->priv_data;
+    struct wgl_priv *wgl = ctx->priv_data;
 
-    void *addr = GetProcAddress(glcontext_wgl->module, name);
+    void *addr = GetProcAddress(wgl->module, name);
     if (!addr)
         addr = wglGetProcAddress((LPCSTR)name);
 
@@ -90,10 +90,10 @@ static void *glcontext_wgl_get_proc_address(struct glcontext *glcontext, const c
 }
 
 const struct glcontext_class ngli_glcontext_wgl_class = {
-    .init = glcontext_wgl_init,
-    .uninit = glcontext_wgl_uninit,
-    .create = glcontext_wgl_create,
-    .make_current = glcontext_wgl_make_current,
-    .get_proc_address = glcontext_wgl_get_proc_address,
-    .priv_size = sizeof(struct glcontext_wgl),
+    .init = wgl_init,
+    .uninit = wgl_uninit,
+    .create = wgl_create,
+    .make_current = wgl_make_current,
+    .get_proc_address = wgl_get_proc_address,
+    .priv_size = sizeof(struct wgl_priv),
 };
