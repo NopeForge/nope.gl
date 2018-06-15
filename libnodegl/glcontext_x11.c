2fa0413bfdbd79bb7bfe0215705edca27945cd6d
Matthieu Bouron <matthieu.bouron@gmail.com>
2018-06-15 16:16:25 +0200

    glcontext_x11: shorten structure and variable names

--- libnodegl/glcontext_x11.c
+++ libnodegl/glcontext_x11.c
@@ -28,7 +28,7 @@
 #include "nodegl.h"
 #include "utils.h"
 
-struct glcontext_x11 {
+struct x11_priv {
     Display *display;
     int own_display;
     Window window;
@@ -47,38 +47,38 @@ struct glcontext_x11 {
 
 typedef GLXContext (*glXCreateContextAttribsFunc)(Display*, GLXFBConfig, GLXContext, Bool, const int*);
 
-static int glcontext_x11_init(struct glcontext *glcontext, uintptr_t display, uintptr_t window, uintptr_t handle)
+static int x11_init(struct glcontext *ctx, uintptr_t display, uintptr_t window, uintptr_t handle)
 {
-    struct glcontext_x11 *glcontext_x11 = glcontext->priv_data;
+    struct x11_priv *x11 = ctx->priv_data;
 
-    if (glcontext->wrapped) {
-        glcontext_x11->display = display ? (Display *)display : glXGetCurrentDisplay();
-        glcontext_x11->window  = window  ? (Window)window     : glXGetCurrentDrawable();
-        glcontext_x11->handle  = handle  ? (GLXContext)handle : glXGetCurrentContext();
-        if (!glcontext_x11->display || !glcontext_x11->window || !glcontext_x11->handle) {
+    if (ctx->wrapped) {
+        x11->display = display ? (Display *)display : glXGetCurrentDisplay();
+        x11->window  = window  ? (Window)window     : glXGetCurrentDrawable();
+        x11->handle  = handle  ? (GLXContext)handle : glXGetCurrentContext();
+        if (!x11->display || !x11->window || !x11->handle) {
             LOG(ERROR,
                 "could not retrieve GLX display (%p), window (0x%lx) and context (%p)",
-                glcontext_x11->display,
-                glcontext_x11->window,
-                glcontext_x11->handle);
+                x11->display,
+                x11->window,
+                x11->handle);
             return -1;
         }
     } else {
         if (display)
-            glcontext_x11->display = (Display *)display;
-        if (!glcontext_x11->display) {
-            glcontext_x11->own_display = 1;
-            glcontext_x11->display = XOpenDisplay(NULL);
-            if (!glcontext_x11->display) {
+            x11->display = (Display *)display;
+        if (!x11->display) {
+            x11->own_display = 1;
+            x11->display = XOpenDisplay(NULL);
+            if (!x11->display) {
                 LOG(ERROR, "could not retrieve GLX display");
                 return -1;
             }
         }
 
-        if (!glcontext->offscreen) {
+        if (!ctx->offscreen) {
             if (window) {
-                glcontext_x11->window  = (Window)window;
-                if (!glcontext_x11->window) {
+                x11->window  = (Window)window;
+                if (!x11->window) {
                     LOG(ERROR, "could not retrieve GLX window");
                     return -1;
                 }
@@ -99,16 +99,16 @@ static int glcontext_x11_init(struct glcontext *glcontext, uintptr_t display, ui
         None
     };
 
-    if (glcontext->samples > 0) {
+    if (ctx->samples > 0) {
         attribs[NGLI_ARRAY_NB(attribs) - 4] = 1;
-        attribs[NGLI_ARRAY_NB(attribs) - 2] = glcontext->samples;
+        attribs[NGLI_ARRAY_NB(attribs) - 2] = ctx->samples;
     }
 
-    glcontext_x11->fbconfigs = glXChooseFBConfig(glcontext_x11->display,
-                                                 DefaultScreen(glcontext_x11->display),
-                                                 attribs,
-                                                 &glcontext_x11->nb_fbconfigs);
-    if (!glcontext_x11->fbconfigs) {
+    x11->fbconfigs = glXChooseFBConfig(x11->display,
+                                       DefaultScreen(x11->display),
+                                       attribs,
+                                       &x11->nb_fbconfigs);
+    if (!x11->fbconfigs) {
         LOG(ERROR, "could not choose a valid framebuffer configuration");
         return -1;
     }
@@ -116,26 +116,26 @@ static int glcontext_x11_init(struct glcontext *glcontext, uintptr_t display, ui
     return 0;
 }
 
-static void glcontext_x11_uninit(struct glcontext *glcontext)
+static void x11_uninit(struct glcontext *ctx)
 {
-    struct glcontext_x11 *glcontext_x11 = glcontext->priv_data;
+    struct x11_priv *x11 = ctx->priv_data;
 
-    if (glcontext_x11->fbconfigs)
-        XFree(glcontext_x11->fbconfigs);
+    if (x11->fbconfigs)
+        XFree(x11->fbconfigs);
 
-    if (glcontext_x11->own_handle)
-        glXDestroyContext(glcontext_x11->display, glcontext_x11->handle);
+    if (x11->own_handle)
+        glXDestroyContext(x11->display, x11->handle);
 
-    if (glcontext_x11->own_window)
-        glXDestroyPbuffer(glcontext_x11->display, glcontext_x11->window);
+    if (x11->own_window)
+        glXDestroyPbuffer(x11->display, x11->window);
 
-    if (glcontext_x11->own_display)
-        XCloseDisplay(glcontext_x11->display);
+    if (x11->own_display)
+        XCloseDisplay(x11->display);
 }
 
-static int glcontext_x11_create(struct glcontext *glcontext, uintptr_t other)
+static int x11_create(struct glcontext *ctx, uintptr_t other)
 {
-    struct glcontext_x11 *glcontext_x11 = glcontext->priv_data;
+    struct x11_priv *x11 = ctx->priv_data;
 
     glXCreateContextAttribsFunc glXCreateContextAttribs =
         (glXCreateContextAttribsFunc)glXGetProcAddress((const GLubyte *)"glXCreateContextAttribsARB");
@@ -144,9 +144,9 @@ static int glcontext_x11_create(struct glcontext *glcontext, uintptr_t other)
         return -1;
     }
 
-    Display *display = glcontext_x11->display;
+    Display *display = x11->display;
     int screen = DefaultScreen(display);
-    GLXFBConfig *fbconfigs = glcontext_x11->fbconfigs;
+    GLXFBConfig *fbconfigs = x11->fbconfigs;
 
     const char *glx_extensions = glXQueryExtensionsString(display, screen);
     if (!ngli_glcontext_check_extension("GLX_ARB_create_context", glx_extensions)) {
@@ -156,8 +156,8 @@ static int glcontext_x11_create(struct glcontext *glcontext, uintptr_t other)
 
     GLXContext shared_context = other ? (GLXContext)other : NULL;
 
-    glcontext_x11->own_handle = 1;
-    if (glcontext->api == NGL_GLAPI_OPENGLES) {
+    x11->own_handle = 1;
+    if (ctx->api == NGL_GLAPI_OPENGLES) {
         if (!ngli_glcontext_check_extension("GLX_EXT_create_context_es2_profile", glx_extensions)) {
             LOG(ERROR, "context does not support GLX_EXT_create_context_es2_profile extension");
             return -1;
@@ -171,12 +171,12 @@ static int glcontext_x11_create(struct glcontext *glcontext, uintptr_t other)
             None
         };
 
-        glcontext_x11->handle = glXCreateContextAttribs(display,
-                                                        fbconfigs[0],
-                                                        shared_context,
-                                                        1,
-                                                        attribs);
-    } else if (glcontext->api == NGL_GLAPI_OPENGL) {
+        x11->handle = glXCreateContextAttribs(display,
+                                              fbconfigs[0],
+                                              shared_context,
+                                              1,
+                                              attribs);
+    } else if (ctx->api == NGL_GLAPI_OPENGL) {
         int attribs[] = {
             GLX_CONTEXT_MAJOR_VERSION_ARB, 4,
             GLX_CONTEXT_MINOR_VERSION_ARB, 1,
@@ -184,45 +184,45 @@ static int glcontext_x11_create(struct glcontext *glcontext, uintptr_t other)
             None
         };
 
-        glcontext_x11->handle = glXCreateContextAttribs(display,
-                                                        fbconfigs[0],
-                                                        shared_context,
-                                                        1,
-                                                        attribs);
+        x11->handle = glXCreateContextAttribs(display,
+                                              fbconfigs[0],
+                                              shared_context,
+                                              1,
+                                              attribs);
     }
 
-    if (!glcontext_x11->handle) {
+    if (!x11->handle) {
         LOG(ERROR, "could not create GLX context");
         return -1;
     }
 
-    if (glcontext->offscreen) {
+    if (ctx->offscreen) {
         int attribs[] = {
-            GLX_PBUFFER_WIDTH, glcontext->width,
-            GLX_PBUFFER_HEIGHT, glcontext->height,
+            GLX_PBUFFER_WIDTH, ctx->width,
+            GLX_PBUFFER_HEIGHT, ctx->height,
             None
         };
 
-        glcontext_x11->own_window = 1;
-        glcontext_x11->window = glXCreatePbuffer(display, fbconfigs[0], attribs);
-        if (!glcontext_x11->window) {
+        x11->own_window = 1;
+        x11->window = glXCreatePbuffer(display, fbconfigs[0], attribs);
+        if (!x11->window) {
             LOG(ERROR, "could not create offscreen pixel buffer");
             return -1;
         }
     }
 
     if (ngli_glcontext_check_extension("GLX_EXT_swap_control", glx_extensions)) {
-        glcontext_x11->glXSwapIntervalEXT = (void *)glXGetProcAddress((const GLubyte *)"glXSwapIntervalEXT");
-        if (glcontext_x11->glXSwapIntervalEXT)
-            glcontext_x11->swap_interval_ext = 1;
+        x11->glXSwapIntervalEXT = (void *)glXGetProcAddress((const GLubyte *)"glXSwapIntervalEXT");
+        if (x11->glXSwapIntervalEXT)
+            x11->swap_interval_ext = 1;
     } else if (ngli_glcontext_check_extension("GLX_MESA_swap_control", glx_extensions)) {
-        glcontext_x11->glXSwapIntervalMESA = (void *)glXGetProcAddress((const GLubyte *)"glXSwapIntervalMESA");
-        if (glcontext_x11->glXSwapIntervalMESA)
-            glcontext_x11->swap_interval_mesa = 1;
+        x11->glXSwapIntervalMESA = (void *)glXGetProcAddress((const GLubyte *)"glXSwapIntervalMESA");
+        if (x11->glXSwapIntervalMESA)
+            x11->swap_interval_mesa = 1;
     } else if (ngli_glcontext_check_extension("GLX_SGI_swap_control", glx_extensions)) {
-        glcontext_x11->glXSwapIntervalSGI = (void *)glXGetProcAddress((const GLubyte *)"glXSwapIntervalSGI");
-        if (glcontext_x11->glXSwapIntervalSGI)
-            glcontext_x11->swap_interval_sgi = 1;
+        x11->glXSwapIntervalSGI = (void *)glXGetProcAddress((const GLubyte *)"glXSwapIntervalSGI");
+        if (x11->glXSwapIntervalSGI)
+            x11->swap_interval_sgi = 1;
     } else {
         LOG(WARNING, "context does not support any swap interval extension");
     }
@@ -230,29 +230,29 @@ static int glcontext_x11_create(struct glcontext *glcontext, uintptr_t other)
     return 0;
 }
 
-static int glcontext_x11_make_current(struct glcontext *glcontext, int current)
+static int x11_make_current(struct glcontext *ctx, int current)
 {
     int ret;
-    struct glcontext_x11 *glcontext_x11 = glcontext->priv_data;
+    struct x11_priv *x11 = ctx->priv_data;
 
     if (current) {
-        ret = glXMakeCurrent(glcontext_x11->display, glcontext_x11->window, glcontext_x11->handle);
+        ret = glXMakeCurrent(x11->display, x11->window, x11->handle);
     } else {
-        ret = glXMakeCurrent(glcontext_x11->display, None, NULL);
+        ret = glXMakeCurrent(x11->display, None, NULL);
     }
 
     return ret - 1;
 }
 
-static void glcontext_x11_swap_buffers(struct glcontext *glcontext)
+static void x11_swap_buffers(struct glcontext *ctx)
 {
-    struct glcontext_x11 *glcontext_x11 = glcontext->priv_data;
-    glXSwapBuffers(glcontext_x11->display, glcontext_x11->window);
+    struct x11_priv *x11 = ctx->priv_data;
+    glXSwapBuffers(x11->display, x11->window);
 }
 
-static int glcontext_x11_set_swap_interval(struct glcontext *glcontext, int interval)
+static int x11_set_swap_interval(struct glcontext *ctx, int interval)
 {
-    struct glcontext_x11 *x11 = glcontext->priv_data;
+    struct x11_priv *x11 = ctx->priv_data;
 
     if (x11->swap_interval_ext) {
         x11->glXSwapIntervalEXT(x11->display, x11->window, interval);
@@ -267,18 +267,18 @@ static int glcontext_x11_set_swap_interval(struct glcontext *glcontext, int inte
     return 0;
 }
 
-static void *glcontext_x11_get_proc_address(struct glcontext *glcontext, const char *name)
+static void *x11_get_proc_address(struct glcontext *ctx, const char *name)
 {
     return glXGetProcAddress((const GLubyte *)name);
 }
 
 const struct glcontext_class ngli_glcontext_x11_class = {
-    .init = glcontext_x11_init,
-    .uninit = glcontext_x11_uninit,
-    .create = glcontext_x11_create,
-    .make_current = glcontext_x11_make_current,
-    .swap_buffers = glcontext_x11_swap_buffers,
-    .set_swap_interval = glcontext_x11_set_swap_interval,
-    .get_proc_address = glcontext_x11_get_proc_address,
-    .priv_size = sizeof(struct glcontext_x11),
+    .init = x11_init,
+    .uninit = x11_uninit,
+    .create = x11_create,
+    .make_current = x11_make_current,
+    .swap_buffers = x11_swap_buffers,
+    .set_swap_interval = x11_set_swap_interval,
+    .get_proc_address = x11_get_proc_address,
+    .priv_size = sizeof(struct x11_priv),
 };
