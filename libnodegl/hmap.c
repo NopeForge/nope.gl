NopeForge/nope.gl

commit 1b2e10751bc0b106e2bff494522c718d0acb2c8e

hmap: check for size overflow

libnodegl/hmap.c
@@ -124,6 +124,9 @@ int ngli_hmap_set(struct hmap *hm, const char *key, void *data)
     if (hm->count * 3 / 4 >= hm->size) {
         struct hmap old_hm = *hm;
 
+        if (hm->size >= sizeof(int)*8 - 2)
+            return -1;
+
         struct bucket *new_buckets = calloc(hm->size << 1, sizeof(*new_buckets));
         if (new_buckets) {
             hm->buckets = new_buckets;

commit 0f20653a586e4d793e223eaac75ce478d31ce271

hmap: fix overflow check
Regression since 1b2e10751bc0b106e2bff494522c718d0acb2c8e.

libnodegl/hmap.c
@@ -124,7 +124,7 @@ int ngli_hmap_set(struct hmap *hm, const char *key, void *data)
     if (hm->count * 3 / 4 >= hm->size) {
         struct hmap old_hm = *hm;
 
-        if (hm->size >= sizeof(int)*8 - 2)
+        if (hm->size >= 1 << (sizeof(hm->size)*8 - 2))
             return -1;
 
         struct bucket *new_buckets = calloc(hm->size << 1, sizeof(*new_buckets));