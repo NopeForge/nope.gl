094ea14775f09f15855b7959404c83513388a85b
Yoann Laala <ylaala@gopro.com>
2018-11-29 13:20:14 +0100

    glcontext: add wgl implementation

--- libnodegl/glcontext_wgl.c
+++ libnodegl/glcontext_wgl.c
@@ -19,6 +19,7 @@
  * under the License.
  */
 
+#include <stddef.h>
 #include <stdio.h>
 #include <windows.h>
 
@@ -27,33 +28,186 @@
 
 #include "glcontext.h"
 #include "nodegl.h"
+#include "log.h"
 
 struct wgl_priv {
-    HGLRC handle;
-    HDC window;
+    HWND window;
+    HPBUFFERARB pixel_buffer;
+    HDC device_context;
+    HGLRC rendering_context;
     HMODULE module;
+    PFNWGLCHOOSEPIXELFORMATARBPROC ChoosePixelFormatARB;
+    PFNWGLCREATECONTEXTATTRIBSARBPROC CreateContextAttribsARB;
+    PFNWGLCREATEPBUFFERARBPROC CreatePbufferARB;
+    PFNWGLRELEASEPBUFFERDCARBPROC ReleasePbufferDCARB;
+    PFNWGLDESTROYPBUFFERARBPROC DestroyPbufferARB;
+    PFNWGLGETPBUFFERDCARBPROC GetPbufferDCARB;
+    PFNWGLSWAPINTERVALEXTPROC SwapIntervalEXT;
 };
 
-static int wgl_init(struct glcontext *ctx, uintptr_t display, uintptr_t window, uintptr_t handle)
+static int wgl_init(struct glcontext *ctx, uintptr_t display, uintptr_t window, uintptr_t other)
 {
     struct wgl_priv *wgl = ctx->priv_data;
 
-    wgl->handle = wglGetCurrentContext();
-    wgl->window = wglGetCurrentDC();
-
-    if (!wgl->window || !wgl->handle)
-        return -1;
-
     wgl->module = LoadLibrary("opengl32.dll");
-    if (!wgl->module)
+    if (!wgl->module) {
+        LOG(ERROR, "could not load opengl32.dll (%lu)", GetLastError());
         return -1;
+    }
 
-    return -1;
+    wgl->window = (HWND)window;
+    if (!ctx->offscreen && !wgl->window) {
+        LOG(ERROR, "could not retrieve window");
+        return -1;
+    }
+
+    wgl->device_context = GetDC((HWND)wgl->window);
+    if (!wgl->device_context) {
+        LOG(ERROR, "could not retrieve device context");
+        return -1;
+    }
+
+    // windows needs a dummy context to probe extensions
+    PIXELFORMATDESCRIPTOR pixel_format_descriptor = {
+        .nSize = sizeof(pixel_format_descriptor),
+        .nVersion = 1,
+        .dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
+        .iPixelType = PFD_TYPE_RGBA,
+        .cColorBits = 32,
+        .cRedBits = 8,
+        .cGreenBits = 8,
+        .cBlueBits = 8,
+        .cAlphaBits = 8,
+        .cDepthBits = 24,
+        .cStencilBits = 8,
+        .iLayerType = PFD_MAIN_PLANE,
+    };
+    int pixel_format = ChoosePixelFormat(wgl->device_context, &pixel_format_descriptor);
+    if (SetPixelFormat(wgl->device_context, pixel_format, &pixel_format_descriptor) == FALSE) {
+        LOG(ERROR, "could not apply default pixel format (%lu)", GetLastError());
+        return -1;
+    }
+
+    wgl->rendering_context = wglCreateContext(wgl->device_context);
+    if (!wgl->rendering_context) {
+        LOG(ERROR, "could not create rendering context (%lu)", GetLastError());
+        return -1;
+    }
+
+    if (wglMakeCurrent(wgl->device_context, wgl->rendering_context) == FALSE) {
+        LOG(ERROR, "could not apply current rendering context (%lu)", GetLastError());
+        return -1;
+    }
+
+    // probe all extensions potentially needed
+    static const struct {
+        const char *name;
+        int offset;
+    } extensions[] = {
+        {"wglChoosePixelFormatARB", offsetof(struct wgl_priv, ChoosePixelFormatARB)},
+        {"wglCreateContextAttribsARB", offsetof(struct wgl_priv, CreateContextAttribsARB)},
+        {"wglCreatePbufferARB", offsetof(struct wgl_priv, CreatePbufferARB)},
+        {"wglReleasePbufferDCARB", offsetof(struct wgl_priv, ReleasePbufferDCARB)},
+        {"wglDestroyPbufferARB", offsetof(struct wgl_priv, DestroyPbufferARB)},
+        {"wglGetPbufferDCARB", offsetof(struct wgl_priv, GetPbufferDCARB)},
+    };
+
+    for (int i = 0; i < NGLI_ARRAY_NB(extensions); i++) {
+        const int offset = extensions[i].offset;
+        const char *name = extensions[i].name;
+        void *function_ptr = wglGetProcAddress(name);
+        if (!function_ptr) {
+            LOG(ERROR, "could not retrieve %s()", name);
+            return -1;
+        }
+
+        memcpy(((uint8_t *)wgl) + offset, &function_ptr, sizeof(function_ptr));
+    }
+
+    wgl->SwapIntervalEXT = (PFNWGLSWAPINTERVALEXTPROC)wglGetProcAddress("wglSwapIntervalEXT");
+    if (!wgl->SwapIntervalEXT)
+        LOG(WARNING, "context does not support any swap interval extension (%lu)", GetLastError());
+
+    const int pixel_format_attributes[] = {
+        WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
+        WGL_DRAW_TO_PBUFFER_ARB, GL_TRUE,
+        WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
+        WGL_PIXEL_TYPE_ARB, WGL_TYPE_RGBA_ARB,
+        WGL_ACCELERATION_ARB, WGL_FULL_ACCELERATION_ARB,
+        WGL_COLOR_BITS_ARB, 32,
+        WGL_RED_BITS_ARB, 8,
+        WGL_GREEN_BITS_ARB, 8,
+        WGL_BLUE_BITS_ARB, 8,
+        WGL_ALPHA_BITS_ARB, 8,
+        WGL_DEPTH_BITS_ARB, 32,
+        WGL_STENCIL_BITS_ARB, 8,
+        WGL_DOUBLE_BUFFER_ARB, GL_TRUE,
+        WGL_SAMPLE_BUFFERS_ARB, ctx->samples > 0 ? GL_TRUE : GL_FALSE,
+        WGL_SAMPLES_ARB, ctx->samples,
+        0
+    };
+
+    UINT pixel_format_count;
+    if (wgl->ChoosePixelFormatARB(wgl->device_context, pixel_format_attributes, NULL, 1, &pixel_format, &pixel_format_count) == FALSE) {
+        LOG(ERROR, "could not choose proper pixel format (%lu)", GetLastError());
+        return -1;
+    }
+    DescribePixelFormat(wgl->device_context, pixel_format, sizeof(PIXELFORMATDESCRIPTOR), &pixel_format_descriptor);
+
+    if (SetPixelFormat(wgl->device_context, pixel_format, &pixel_format_descriptor) == FALSE) {
+        LOG(ERROR, "could not apply pixel format (%lu)", GetLastError());
+        return -1;
+    }
+
+    if (ctx->offscreen) {
+        wgl->pixel_buffer = wgl->CreatePbufferARB(NULL, pixel_format, ctx->width, ctx->height, NULL);
+        if (!wgl->pixel_buffer) {
+            LOG(ERROR, "could not create pixel buffer");
+            return -1;
+        }
+        wgl->device_context = wgl->GetPbufferDCARB(wgl->pixel_buffer);
+    }
+
+    if (wglDeleteContext(wgl->rendering_context) == FALSE)
+        LOG(WARNING, "failed to delete dummy rendering context (%lu)", GetLastError());
+
+    const int context_attributes[] = {
+        WGL_CONTEXT_MAJOR_VERSION_ARB, 1,
+        WGL_CONTEXT_MINOR_VERSION_ARB, 0,
+        WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
+        0
+    };
+
+    HGLRC shared_context = (HGLRC)other;
+    wgl->rendering_context = wgl->CreateContextAttribsARB(wgl->device_context, shared_context, context_attributes);
+
+    if (!wgl->rendering_context) {
+        LOG(ERROR, "failed to create rendering context (%lu)", GetLastError());
+        return -1;
+    }
+
+    if (wglMakeCurrent(wgl->device_context, wgl->rendering_context) == FALSE) {
+        LOG(ERROR, "could not apply current rendering context (%lu)", GetLastError());
+        return -1;
+    }
+
+    return 0;
 }
 
 static void wgl_uninit(struct glcontext *ctx)
 {
     struct wgl_priv *wgl = ctx->priv_data;
+
+    if (wgl->pixel_buffer && wgl->device_context) {
+        if (wgl->ReleasePbufferDCARB)
+            wgl->ReleasePbufferDCARB(wgl->pixel_buffer, wgl->device_context);
+
+        if (wgl->DestroyPbufferARB)
+            wgl->DestroyPbufferARB(wgl->pixel_buffer);
+    }
+
+    if (wgl->rendering_context)
+        wglDeleteContext(wgl->rendering_context);
 
     if (wgl->module)
         FreeLibrary(wgl->module);
@@ -61,16 +215,35 @@ static void wgl_uninit(struct glcontext *ctx)
 
 static int wgl_make_current(struct glcontext *ctx, int current)
 {
-    int ret;
     struct wgl_priv *wgl = ctx->priv_data;
 
-    if (current) {
-        ret = wglMakeCurrent(wgl->window, wgl->handle);
-    } else {
-        ret = wglMakeCurrent(wgl->window, NULL);
+    if (wglMakeCurrent(wgl->device_context, current ? wgl->rendering_context : NULL) == FALSE)
+        return -1;
+
+    return 0;
+}
+
+static void wgl_swap_buffers(struct glcontext *ctx)
+{
+    struct wgl_priv *wgl = ctx->priv_data;
+    SwapBuffers(wgl->device_context);
+}
+
+static int wgl_set_swap_interval(struct glcontext *ctx, int interval)
+{
+    struct wgl_priv *wgl = ctx->priv_data;
+
+    if (!wgl->SwapIntervalEXT) {
+        LOG(WARNING, "context does not support swap interval operation");
+        return -1;
     }
 
-    return ret - 1;
+    if (wgl->SwapIntervalEXT(interval) == FALSE) {
+        LOG(ERROR, "context failed to apply swap interval (%lu)", GetLastError());
+        return -1;
+    }
+
+    return 0;
 }
 
 static void *wgl_get_proc_address(struct glcontext *ctx, const char *name)
@@ -88,6 +261,8 @@ const struct glcontext_class ngli_glcontext_wgl_class = {
     .init = wgl_init,
     .uninit = wgl_uninit,
     .make_current = wgl_make_current,
+    .swap_buffers = wgl_swap_buffers,
+    .set_swap_interval = wgl_set_swap_interval,
     .get_proc_address = wgl_get_proc_address,
     .priv_size = sizeof(struct wgl_priv),
 };
