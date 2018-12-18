NopeForge/nope.gl

commit 5c736d345a2a06713841bce49ed866fb5a365a52

buffer: add graphic buffer internal API

libnodegl/node_buffer.c
@@ -25,6 +25,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+
+#include "buffer.h"
 #include "log.h"
 #include "nodegl.h"
 #include "nodes.h"
@@ -69,6 +71,53 @@ static const struct node_param buffer_params[] = {
                .choices=&usage_choices},
     {NULL}
 };
+
+int ngli_buffer_ref(struct ngl_node *node)
+{
+    struct ngl_ctx *ctx = node->ctx;
+    struct glcontext *gl = ctx->glcontext;
+
+    struct buffer *s = node->priv_data;
+
+    if (s->graphic_buffer_refcount++ == 0) {
+        int ret = ngli_graphic_buffer_allocate(gl, &s->graphic_buffer, s->data_size, s->usage);
+        if (ret < 0)
+            return ret;
+
+        ret = ngli_graphic_buffer_upload(gl, &s->graphic_buffer, s->data, s->data_size);
+        if (ret < 0)
+            return ret;
+    }
+
+    return 0;
+}
+
+void ngli_buffer_unref(struct ngl_node *node)
+{
+    struct ngl_ctx *ctx = node->ctx;
+    struct glcontext *gl = ctx->glcontext;
+
+    struct buffer *s = node->priv_data;
+
+    ngli_assert(s->graphic_buffer_refcount);
+    if (s->graphic_buffer_refcount-- == 1)
+        ngli_graphic_buffer_free(gl, &s->graphic_buffer);
+}
+
+int ngli_buffer_upload(struct ngl_node *node)
+{
+    struct ngl_ctx *ctx = node->ctx;
+    struct glcontext *gl = ctx->glcontext;
+
+    struct buffer *s = node->priv_data;
+
+    if (s->dynamic && s->graphic_buffer_last_upload_time != node->last_update_time) {
+        ngli_graphic_buffer_upload(gl, &s->graphic_buffer, s->data, s->data_size);
+        s->graphic_buffer_last_upload_time = node->last_update_time;
+    }
+
+    return 0;
+}
 
 static int buffer_init_from_data(struct ngl_node *node)
 {

libnodegl/buffer.c
@@ -19,52 +19,38 @@
  * under the License.
  */
 
+#include <string.h>
+
 #include "buffer.h"
+#include "glcontext.h"
 #include "glincludes.h"
-#include "nodes.h"
 
-int ngli_buffer_allocate(struct ngl_node *node)
+int ngli_graphic_buffer_allocate(struct glcontext *gl,
+                                 struct graphic_buffer *buffer,
+                                 int size,
+                                 int usage)
 {
-    struct ngl_ctx *ctx = node->ctx;
-    struct glcontext *gl = ctx->glcontext;
-    struct buffer *s = node->priv_data;
-
-    if (!s->buffer_id) {
-        ngli_glGenBuffers(gl, 1, &s->buffer_id);
-        ngli_glBindBuffer(gl, GL_ARRAY_BUFFER, s->buffer_id);
-        ngli_glBufferData(gl, GL_ARRAY_BUFFER, s->data_size, s->data, s->usage);
-        s->buffer_last_upload_time = -1;
-    }
-    s->buffer_refcount++;
+    ngli_glGenBuffers(gl, 1, &buffer->id);
+    ngli_glBindBuffer(gl, GL_ARRAY_BUFFER, buffer->id);
+    ngli_glBufferData(gl, GL_ARRAY_BUFFER, size, NULL, usage);
+    buffer->size = size;
+    buffer->usage = usage;
     return 0;
 }
 
-void ngli_buffer_upload(struct ngl_node *node)
+int ngli_graphic_buffer_upload(struct glcontext *gl,
+                               struct graphic_buffer *buffer,
+                               void *data,
+                               int size)
 {
-    struct ngl_ctx *ctx = node->ctx;
-    struct glcontext *gl = ctx->glcontext;
-    struct buffer *s = node->priv_data;
-
-    if (s->dynamic && s->buffer_last_upload_time != node->last_update_time) {
-        ngli_glBindBuffer(gl, GL_ARRAY_BUFFER, s->buffer_id);
-        ngli_glBufferSubData(gl, GL_ARRAY_BUFFER, 0, s->data_size, s->data);
-        s->buffer_last_upload_time = node->last_update_time;
-    }
+    ngli_glBindBuffer(gl, GL_ARRAY_BUFFER, buffer->id);
+    ngli_glBufferSubData(gl, GL_ARRAY_BUFFER, 0, size, data);
+    return 0;
 }
 
-void ngli_buffer_free(struct ngl_node *node)
+void ngli_graphic_buffer_free(struct glcontext *gl,
+                              struct graphic_buffer *buffer)
 {
-    if (!node)
-        return;
-
-    struct ngl_ctx *ctx = node->ctx;
-    struct glcontext *gl = ctx->glcontext;
-    struct buffer *s = node->priv_data;
-
-    if (s->buffer_refcount-- == 1) {
-        ngli_glDeleteBuffers(gl, 1, &s->buffer_id);
-        s->buffer_id = 0;
-        s->buffer_last_upload_time = -1;
-    }
-    ngli_assert(s->buffer_refcount >= 0);
+    ngli_glDeleteBuffers(gl, 1, &buffer->id);
+    memset(buffer, 0, sizeof(*buffer));
 }

libnodegl/buffer.h
@@ -22,10 +22,25 @@
 #ifndef BUFFER_H
 #define BUFFER_H
 
-#include "nodes.h"
+#include "glcontext.h"
 
-int ngli_buffer_allocate(struct ngl_node *node);
-void ngli_buffer_upload(struct ngl_node *node);
-void ngli_buffer_free(struct ngl_node *node);
+struct graphic_buffer {
+    GLuint id;
+    int size;
+    int usage;
+};
+
+int ngli_graphic_buffer_allocate(struct glcontext *gl,
+                                 struct graphic_buffer *buffer,
+                                 int size,
+                                 int usage);
+
+int ngli_graphic_buffer_upload(struct glcontext *gl,
+                               struct graphic_buffer *buffer,
+                               void *data,
+                               int size);
+
+void ngli_graphic_buffer_free(struct glcontext *gl,
+                              struct graphic_buffer *buffer);
 
 #endif

libnodegl/node_render.c
@@ -158,7 +158,7 @@ static int update_vertex_attribs(struct ngl_node *node)
         struct buffer *buffer = pair->node->priv_data;
 
         ngli_glEnableVertexAttribArray(gl, aid);
-        ngli_glBindBuffer(gl, GL_ARRAY_BUFFER, buffer->buffer_id);
+        ngli_glBindBuffer(gl, GL_ARRAY_BUFFER, buffer->graphic_buffer.id);
         ngli_glVertexAttribPointer(gl, aid, buffer->data_comp, GL_FLOAT, GL_FALSE, buffer->data_stride, NULL);
 
         if (i >= s->first_instance_attribute_index)
@@ -202,6 +202,10 @@ static int pair_node_to_attribinfo(struct render *s, const char *name,
 
     if (active_attribute->location < 0)
         return 0;
+
+    int ret = ngli_buffer_ref(anode);
+    if (ret < 0)
+        return ret;
 
     struct nodeprograminfopair pair = {
         .node = anode,
@@ -333,16 +337,10 @@ static int render_init(struct ngl_node *node)
     if (ret < 0)
         return ret;
 
-    /* Allocate buffers */
-    ret = ngli_buffer_allocate(geometry->indices_buffer);
+    ret = ngli_buffer_ref(geometry->indices_buffer);
     if (ret < 0)
         return ret;
-    for (int i = 0; i < s->nb_attribute_pairs; i++) {
-        struct nodeprograminfopair *pair = &s->attribute_pairs[i];
-        ret = ngli_buffer_allocate((struct ngl_node *)pair->node);
-        if (ret < 0)
-            return ret;
-    }
+    s->has_indices_buffer_ref = 1;
 
     if (gl->features & NGLI_FEATURE_VERTEX_ARRAY_OBJECT) {
         ngli_glGenVertexArrays(gl, 1, &s->vao_id);
@@ -366,12 +364,14 @@ static void render_uninit(struct ngl_node *node)
 
     ngli_pipeline_uninit(node);
 
-    struct geometry *geometry = s->geometry->priv_data;
-    ngli_buffer_free(geometry->indices_buffer);
+    if (s->has_indices_buffer_ref) {
+        struct geometry *geometry = s->geometry->priv_data;
+        ngli_buffer_unref(geometry->indices_buffer);
+    }
 
     for (int i = 0; i < s->nb_attribute_pairs; i++) {
         struct nodeprograminfopair *pair = &s->attribute_pairs[i];
-        ngli_buffer_free((struct ngl_node *)pair->node);
+        ngli_buffer_unref((struct ngl_node *)pair->node);
     }
 
     free(s->attribute_pairs);
@@ -391,7 +391,9 @@ static int render_update(struct ngl_node *node, double t)
         int ret = ngli_node_update(bnode, t);
         if (ret < 0)
             return ret;
-        ngli_buffer_upload(bnode);
+        ret = ngli_buffer_upload(bnode);
+        if (ret < 0)
+            return ret;
     }
 
     return ngli_pipeline_update(node, t);
@@ -426,7 +428,7 @@ static void render_draw(struct ngl_node *node)
     GLenum indices_type;
     ngli_format_get_gl_format_type(gl, indices_buffer->data_format, NULL, NULL, &indices_type);
 
-    ngli_glBindBuffer(gl, GL_ELEMENT_ARRAY_BUFFER, indices_buffer->buffer_id);
+    ngli_glBindBuffer(gl, GL_ELEMENT_ARRAY_BUFFER, indices_buffer->graphic_buffer.id);
     if (s->nb_instances > 0) {
         ngli_glDrawElementsInstanced(gl, geometry->topology, indices_buffer->count, indices_type, 0, s->nb_instances);
     } else {

libnodegl/nodes.h
@@ -43,6 +43,7 @@
 #include "params.h"
 #include "formats_register.h"
 #include "darray.h"
+#include "buffer.h"
 
 struct node_class;
 
@@ -222,10 +223,14 @@ struct buffer {
     int fd;
     int dynamic;
 
-    GLuint buffer_id;
-    int buffer_refcount;
-    double buffer_last_upload_time;
+    struct graphic_buffer graphic_buffer;
+    int graphic_buffer_refcount;
+    double graphic_buffer_last_upload_time;
 };
+
+int ngli_buffer_ref(struct ngl_node *node);
+void ngli_buffer_unref(struct ngl_node *node);
+int ngli_buffer_upload(struct ngl_node *node);
 
 struct uniform {
     double scalar;
@@ -404,6 +409,8 @@ struct render {
     struct nodeprograminfopair *attribute_pairs; // (attribute, attributeprograminfo)
     int nb_attribute_pairs;
     int first_instance_attribute_index;
+
+    int has_indices_buffer_ref;
 
     GLint modelview_matrix_location;
     GLint projection_matrix_location;

libnodegl/pipeline.c
@@ -325,7 +325,8 @@ static int update_buffers(struct ngl_node *node)
         const struct ngl_node *bnode = pair->node;
         const struct buffer *buffer = bnode->priv_data;
         const struct bufferprograminfo *info = pair->program_info;
-        ngli_glBindBufferBase(gl, info->type, info->binding, buffer->buffer_id);
+
+        ngli_glBindBufferBase(gl, info->type, info->binding, buffer->graphic_buffer.id);
     }
 
     return 0;
@@ -517,16 +518,16 @@ int ngli_pipeline_init(struct ngl_node *node)
             struct ngl_node *bnode = entry->data;
             struct buffer *buffer = bnode->priv_data;
 
-            int ret = ngli_buffer_allocate(bnode);
-            if (ret < 0)
-                return ret;
-
             if (info->type == GL_UNIFORM_BUFFER &&
                 buffer->data_size > gl->max_uniform_block_size) {
                 LOG(ERROR, "buffer %s size (%d) exceeds max uniform block size (%d)",
                     bnode->name, buffer->data_size, gl->max_uniform_block_size);
                 return -1;
             }
+
+            int ret = ngli_buffer_ref(bnode);
+            if (ret < 0)
+                return ret;
 
             struct nodeprograminfopair pair = {
                 .node = bnode,
@@ -549,7 +550,7 @@ void ngli_pipeline_uninit(struct ngl_node *node)
     free(s->uniform_pairs);
     for (int i = 0; i < s->nb_buffer_pairs; i++) {
         struct nodeprograminfopair *pair = &s->buffer_pairs[i];
-        ngli_buffer_free((struct ngl_node *)pair->node);
+        ngli_buffer_unref((struct ngl_node *)pair->node);
     }
     free(s->buffer_pairs);
 }
@@ -582,10 +583,13 @@ int ngli_pipeline_update(struct ngl_node *node, double t)
         gl->features & NGLI_FEATURE_SHADER_STORAGE_BUFFER_OBJECT) {
         const struct hmap_entry *entry = NULL;
         while ((entry = ngli_hmap_next(s->buffers, entry))) {
-            int ret = ngli_node_update(entry->data, t);
+            struct ngl_node *bnode = (struct ngl_node *)entry->data;
+            int ret = ngli_node_update(bnode, t);
             if (ret < 0)
                 return ret;
-            ngli_buffer_upload(entry->data);
+            ret = ngli_buffer_upload(bnode);
+            if (ret < 0)
+                return ret;
         }
     }
 

commit e528b67fd5d130807759cab8826d7b53fac135e0

buffer: initialize buffer_last_upload_time in ngl_node_buffer_ref()
Fixes a regression introduced by
5c736d345a2a06713841bce49ed866fb5a365a52.

libnodegl/node_buffer.c
@@ -87,6 +87,8 @@ int ngli_node_buffer_ref(struct ngl_node *node)
         ret = ngli_buffer_upload(&s->buffer, s->data, s->data_size);
         if (ret < 0)
             return ret;
+
+        s->buffer_last_upload_time = -1.;
     }
 
     return 0;