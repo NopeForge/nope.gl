6f57dd78d13432874a638ef7d0e20d4d12748b7b
Matthieu Bouron <matthieu.bouron@gmail.com>
2019-01-15 16:59:48 +0100

    glincludes: add GL_TEXTURE_RECTANGLE to GLES2 compatibility defines

--- libnodegl/glincludes.h
+++ libnodegl/glincludes.h
@@ -72,6 +72,7 @@ typedef void* GLeglImageOES;
 #endif
 
 #if NGL_GLES2_COMPAT_INCLUDES
+# define GL_TEXTURE_RECTANGLE                  0x84F5
 # define GL_STENCIL_INDEX                      0x1901
 # define GL_STENCIL_INDEX8                     0x8D48
 # define GL_SAMPLER_3D                         0x8B5F
