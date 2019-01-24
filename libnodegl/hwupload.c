NopeForge/nope.gl

commit 9e87c751c5948bb3ddb4c8ee418871b5d62c597d

hwupload: use a class system

libnodegl/hwupload.c
@@ -31,156 +31,81 @@
 #include "nodegl.h"
 #include "nodes.h"
 
-#include "hwupload_common.h"
-#if defined(TARGET_ANDROID)
-#include "hwupload_mediacodec.h"
-#elif defined(TARGET_DARWIN) || defined(TARGET_IPHONE)
-#include "hwupload_videotoolbox.h"
-#endif
+extern const struct hwupload_class ngli_hwupload_common_class;
+extern const struct hwupload_class ngli_hwupload_mc_class;
+extern const struct hwupload_class ngli_hwupload_vt_class;
 
-static int get_config_from_frame(struct ngl_node *node, struct sxplayer_frame *frame, struct hwupload_config *config)
+static const struct hwupload_class *hwupload_class_map[] = {
+    [SXPLAYER_PIXFMT_RGBA]        = &ngli_hwupload_common_class,
+    [SXPLAYER_PIXFMT_BGRA]        = &ngli_hwupload_common_class,
+    [SXPLAYER_SMPFMT_FLT]         = &ngli_hwupload_common_class,
+#if defined(TARGET_ANDROID)
+    [SXPLAYER_PIXFMT_MEDIACODEC]  = &ngli_hwupload_mc_class,
+#elif defined(TARGET_DARWIN) || defined(TARGET_IPHONE)
+    [SXPLAYER_PIXFMT_VT]          = &ngli_hwupload_vt_class,
+#endif
+};
+
+static const struct hwmap_class *get_hwmap_class(struct ngl_node *node, struct sxplayer_frame *frame)
 {
-    switch (frame->pix_fmt) {
-    case SXPLAYER_PIXFMT_RGBA:
-    case SXPLAYER_PIXFMT_BGRA:
-    case SXPLAYER_SMPFMT_FLT:
-        ngli_hwupload_common_get_config_from_frame(node, frame, config);
-        break;
-#if defined(TARGET_ANDROID)
-    case SXPLAYER_PIXFMT_MEDIACODEC:
-        ngli_hwupload_mc_get_config_from_frame(node, frame, config);
-        break;
-#elif defined(TARGET_DARWIN) || defined(TARGET_IPHONE)
-    case SXPLAYER_PIXFMT_VT:
-        ngli_hwupload_vt_get_config_from_frame(node, frame, config);
-        break;
-#endif
-    default:
-        ngli_assert(0);
-    }
+    if (frame->pix_fmt < 0 || frame->pix_fmt >= NGLI_ARRAY_NB(hwupload_class_map))
+        return NULL;
 
-    return 0;
+    const struct hwupload_class *cls = hwupload_class_map[frame->pix_fmt];
+    if (!cls)
+        return NULL;
+    return cls->get_hwmap(node, frame);
 }
 
-static int hwupload_init(struct ngl_node *node, struct hwupload_config *config)
+int ngli_hwupload_upload_frame(struct ngl_node *node)
 {
-    int ret = 0;
-
-    switch (config->format) {
-    case NGLI_HWUPLOAD_FMT_COMMON:
-        ret = ngli_hwupload_common_init(node, config);
-        break;
-#if defined(TARGET_ANDROID)
-    case NGLI_HWUPLOAD_FMT_MEDIACODEC:
-        ret = ngli_hwupload_mc_init(node, config);
-        break;
-    case NGLI_HWUPLOAD_FMT_MEDIACODEC_DR:
-        ret = ngli_hwupload_mc_dr_init(node, config);
-        break;
-#elif defined(TARGET_DARWIN) || defined(TARGET_IPHONE)
-    case NGLI_HWUPLOAD_FMT_VIDEOTOOLBOX_BGRA:
-    case NGLI_HWUPLOAD_FMT_VIDEOTOOLBOX_RGBA:
-    case NGLI_HWUPLOAD_FMT_VIDEOTOOLBOX_NV12:
-        ret = ngli_hwupload_vt_init(node, config);
-        break;
-# if defined(TARGET_IPHONE)
-    case NGLI_HWUPLOAD_FMT_VIDEOTOOLBOX_NV12_DR:
-        ret = ngli_hwupload_vt_dr_init(node, config);
-        break;
-# endif
-#endif
-    default:
-        ngli_assert(0);
-    }
-
-    return ret;
-}
-
-static int hwupload_upload_frame(struct ngl_node *node,
-                                 struct hwupload_config *config,
-                                 struct sxplayer_frame *frame)
-{
-    int ret = 0;
-
-    switch (config->format) {
-    case NGLI_HWUPLOAD_FMT_COMMON:
-        ret = ngli_hwupload_common_upload(node, config, frame);
-        break;
-#if defined(TARGET_ANDROID)
-    case NGLI_HWUPLOAD_FMT_MEDIACODEC:
-        ret = ngli_hwupload_mc_upload(node, config, frame);
-        break;
-    case NGLI_HWUPLOAD_FMT_MEDIACODEC_DR:
-        ret = ngli_hwupload_mc_dr_upload(node, config, frame);
-        break;
-#elif defined(TARGET_DARWIN) || defined(TARGET_IPHONE)
-    case NGLI_HWUPLOAD_FMT_VIDEOTOOLBOX_BGRA:
-    case NGLI_HWUPLOAD_FMT_VIDEOTOOLBOX_RGBA:
-    case NGLI_HWUPLOAD_FMT_VIDEOTOOLBOX_NV12:
-        ret = ngli_hwupload_vt_upload(node, config, frame);
-        break;
-# if defined(TARGET_IPHONE)
-    case NGLI_HWUPLOAD_FMT_VIDEOTOOLBOX_NV12_DR:
-        ret = ngli_hwupload_vt_dr_upload(node, config, frame);
-        break;
-# endif
-#endif
-    default:
-        ngli_assert(0);
-    }
-
-    return ret;
-}
-
-int ngli_hwupload_upload_frame(struct ngl_node *node, struct sxplayer_frame *frame)
-{
+    struct texture *s = node->priv_data;
+    struct media *media = s->data_src->priv_data;
+    struct sxplayer_frame *frame = media->frame;
     if (!frame)
         return 0;
+    media->frame = NULL;
 
-    struct hwupload_config config = {0};
-    int ret = get_config_from_frame(node, frame, &config);
-    if (ret < 0)
-        return ret;
+    const struct hwmap_class *hwmap_class = get_hwmap_class(node, frame);
+    if (!hwmap_class) {
+        sxplayer_release_frame(frame);
+        return -1;
+    }
 
-    ret = hwupload_init(node, &config);
-    if (ret < 0)
-        return ret;
+    if (s->hwupload_map_class != hwmap_class) {
+        ngli_hwupload_uninit(node);
 
-    return hwupload_upload_frame(node, &config, frame);
+        if (hwmap_class->priv_size) {
+            s->hwupload_priv_data = calloc(1, hwmap_class->priv_size);
+            if (!s->hwupload_priv_data) {
+                sxplayer_release_frame(frame);
+                return -1;
+            }
+        }
+
+        int ret = hwmap_class->init(node, frame);
+        if (ret < 0) {
+            sxplayer_release_frame(frame);
+            return ret;
+        }
+        s->hwupload_map_class = hwmap_class;
+
+        LOG(DEBUG, "mapping texture '%s' with method: %s", node->name, hwmap_class->name);
+    }
+
+    int ret = hwmap_class->map_frame(node, frame);
+    if (!(hwmap_class->flags &  HWMAP_FLAG_FRAME_OWNER))
+        sxplayer_release_frame(frame);
+    return ret;
 }
 
 void ngli_hwupload_uninit(struct ngl_node *node)
 {
     struct texture *s = node->priv_data;
-
-    if (s->hwupload_fmt == NGLI_HWUPLOAD_FMT_NONE)
-        return;
-
-    switch (s->hwupload_fmt) {
-    case NGLI_HWUPLOAD_FMT_COMMON:
-        ngli_hwupload_common_uninit(node);
-        break;
-#if defined(TARGET_ANDROID)
-    case NGLI_HWUPLOAD_FMT_MEDIACODEC:
-        ngli_hwupload_mc_uninit(node);
-        break;
-    case NGLI_HWUPLOAD_FMT_MEDIACODEC_DR:
-        ngli_hwupload_mc_dr_uninit(node);
-        break;
-#elif defined(TARGET_DARWIN) || defined(TARGET_IPHONE)
-    case NGLI_HWUPLOAD_FMT_VIDEOTOOLBOX_BGRA:
-    case NGLI_HWUPLOAD_FMT_VIDEOTOOLBOX_RGBA:
-    case NGLI_HWUPLOAD_FMT_VIDEOTOOLBOX_NV12:
-        ngli_hwupload_vt_uninit(node);
-        break;
-# if defined(TARGET_IPHONE)
-    case NGLI_HWUPLOAD_FMT_VIDEOTOOLBOX_NV12_DR:
-        ngli_hwupload_vt_dr_uninit(node);
-        break;
-# endif
-#endif
-    default:
-        ngli_assert(0);
+    if (s->hwupload_map_class && s->hwupload_map_class->uninit) {
+        s->hwupload_map_class->uninit(node);
     }
-    ngli_assert(s->hwupload_fmt == NGLI_HWUPLOAD_FMT_NONE);
+    free(s->hwupload_priv_data);
+    s->hwupload_priv_data = NULL;
+    s->hwupload_map_class = NULL;
 }

libnodegl/hwupload.h
@@ -22,19 +22,27 @@
 #ifndef HWUPLOAD_H
 #define HWUPLOAD_H
 
+#include <stdlib.h>
 #include <sxplayer.h>
 
 #include "nodegl.h"
 
-struct hwupload_config {
-    int format;
-    int width;
-    int height;
-    int linesize;
-    int data_format;
+#define HWMAP_FLAG_FRAME_OWNER (1 << 0)
+
+struct hwmap_class {
+    const char *name;
+    int flags;
+    size_t priv_size;
+    int (*init)(struct ngl_node *node, struct sxplayer_frame *frame);
+    int (*map_frame)(struct ngl_node *node, struct sxplayer_frame *frame);
+    void (*uninit)(struct ngl_node *node);
 };
 
-int ngli_hwupload_upload_frame(struct ngl_node *node, struct sxplayer_frame *frame);
+struct hwupload_class {
+    const struct hwmap_class *(*get_hwmap)(struct ngl_node *node, struct sxplayer_frame *frame);
+};
+
+int ngli_hwupload_upload_frame(struct ngl_node *node);
 void ngli_hwupload_uninit(struct ngl_node *node);
 
 #endif /* HWUPLOAD_H */

libnodegl/hwupload_common.c
@@ -27,46 +27,34 @@
 #include "android_surface.h"
 #include "glincludes.h"
 #include "hwupload.h"
-#include "hwupload_common.h"
 #include "log.h"
 #include "math_utils.h"
 #include "nodegl.h"
 #include "nodes.h"
 
-int ngli_hwupload_common_get_config_from_frame(struct ngl_node *node,
-                                               struct sxplayer_frame *frame,
-                                               struct hwupload_config *config)
+static int common_get_data_format(int pix_fmt)
 {
-    config->format = NGLI_HWUPLOAD_FMT_COMMON;
-    config->width = frame->width;
-    config->height = frame->height;
-    config->linesize = frame->linesize;
-
-    switch (frame->pix_fmt) {
+    switch (pix_fmt) {
     case SXPLAYER_PIXFMT_RGBA:
+        return NGLI_FORMAT_R8G8B8A8_UNORM;
     case SXPLAYER_PIXFMT_BGRA:
+        return NGLI_FORMAT_B8G8R8A8_UNORM;
     case SXPLAYER_SMPFMT_FLT:
-        config->data_format = NGLI_FORMAT_R8G8B8A8_UNORM;
-        break;
+        return NGLI_FORMAT_R32_SFLOAT;
     default:
-        ngli_assert(0);
+        return -1;
     }
-
-    return 0;
 }
 
-int ngli_hwupload_common_init(struct ngl_node *node,
-                              struct hwupload_config *config)
+static int common_init(struct ngl_node *node, struct sxplayer_frame *frame)
 {
     struct ngl_ctx *ctx = node->ctx;
     struct glcontext *gl = ctx->glcontext;
     struct texture *s = node->priv_data;
 
-    if (s->hwupload_fmt == config->format)
-        return 0;
-
-    s->hwupload_fmt = config->format;
-    s->data_format = config->data_format;
+    s->data_format = common_get_data_format(frame->pix_fmt);
+    if (s->data_format < 0)
+        return -1;
 
     int ret = ngli_format_get_gl_format_type(gl,
                                              s->data_format,
@@ -81,22 +69,29 @@ int ngli_hwupload_common_init(struct ngl_node *node,
     return 0;
 }
 
-int ngli_hwupload_common_upload(struct ngl_node *node,
-                                struct hwupload_config *config,
-                                struct sxplayer_frame *frame)
+static int common_map_frame(struct ngl_node *node, struct sxplayer_frame *frame)
 {
     struct texture *s = node->priv_data;
 
-    const int linesize       = config->linesize >> 2;
-    s->coordinates_matrix[0] = linesize ? config->width / (float)linesize : 1.0;
+    const int linesize       = frame->linesize >> 2;
+    s->coordinates_matrix[0] = linesize ? frame->width / (float)linesize : 1.0;
 
-    ngli_texture_update_local_texture(node, linesize, config->height, 0, frame->data);
+    ngli_texture_update_local_texture(node, linesize, frame->height, 0, frame->data);
 
     return 0;
 }
 
-void ngli_hwupload_common_uninit(struct ngl_node *node)
+static const struct hwmap_class hwmap_common_class = {
+    .name      = "default",
+    .init      = common_init,
+    .map_frame = common_map_frame,
+};
+
+static const struct hwmap_class *common_get_hwmap(struct ngl_node *node, struct sxplayer_frame *frame)
 {
-    struct texture *s = node->priv_data;
-    s->hwupload_fmt = NGLI_HWUPLOAD_FMT_NONE;
+    return &hwmap_common_class;
 }
+
+const struct hwupload_class ngli_hwupload_common_class = {
+    .get_hwmap = common_get_hwmap,
+};

libnodegl/hwupload_common.h
@@ -1,42 +1,0 @@
-/*
- * Copyright 2018 GoPro Inc.
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-#ifndef HWUPLOAD_COMMON
-#define HWUPLOAD_COMMON
-
-#include <sxplayer.h>
-
-#include "hwupload.h"
-#include "nodes.h"
-
-int ngli_hwupload_common_get_config_from_frame(struct ngl_node *node,
-                                               struct sxplayer_frame *frame,
-                                               struct hwupload_config *config);
-
-int ngli_hwupload_common_init(struct ngl_node *node,
-                              struct hwupload_config *config);
-
-int ngli_hwupload_common_upload(struct ngl_node *node,
-                                struct hwupload_config *config,
-                                struct sxplayer_frame *frame);
-
-void ngli_hwupload_common_uninit(struct ngl_node *node);
-#endif

libnodegl/hwupload_mediacodec.c
@@ -29,7 +29,6 @@
 #include "android_surface.h"
 #include "glincludes.h"
 #include "hwupload.h"
-#include "hwupload_mediacodec.h"
 #include "log.h"
 #include "math_utils.h"
 #include "nodegl.h"
@@ -44,39 +43,6 @@ struct hwupload_mc {
     struct ngl_node *rtt;
 };
 
-int ngli_hwupload_mc_get_config_from_frame(struct ngl_node *node,
-                                           struct sxplayer_frame *frame,
-                                           struct hwupload_config *config)
-{
-    struct texture *s = node->priv_data;
-
-    config->width = frame->width;
-    config->height = frame->height;
-    config->linesize = frame->linesize;
-
-    if (s->direct_rendering) {
-        if (s->min_filter != GL_NEAREST && s->min_filter != GL_LINEAR) {
-            LOG(WARNING,
-                "External textures only support nearest and linear filtering: disabling direct rendering");
-            s->direct_rendering = 0;
-        } else if (s->wrap_s != GL_CLAMP_TO_EDGE || s->wrap_t != GL_CLAMP_TO_EDGE) {
-            LOG(WARNING,
-                "External textures only support clamp to edge wrapping: disabling direct rendering");
-            s->direct_rendering = 0;
-        }
-    }
-
-    if (s->direct_rendering) {
-        config->format = NGLI_HWUPLOAD_FMT_MEDIACODEC_DR;
-        config->data_format = NGLI_FORMAT_UNDEFINED;
-    } else {
-        config->format = NGLI_HWUPLOAD_FMT_MEDIACODEC;
-        config->data_format = NGLI_FORMAT_R8G8B8A8_UNORM;
-    }
-
-    return 0;
-}
-
 static const char fragment_shader_hwupload_oes_data[] = ""
     "#version 100"                                                                      "\n"
     "#extension GL_OES_EGL_image_external : require"                                    "\n"
@@ -90,30 +56,20 @@ static const char fragment_shader_hwupload_oes_data[] = ""
     "    gl_FragColor = vec4(t.rgb, 1.0);"                                              "\n"
     "}";
 
-int ngli_hwupload_mc_init(struct ngl_node *node,
-                          struct hwupload_config *config)
+static int mc_init(struct ngl_node *node, struct sxplayer_frame *frame)
 {
     struct ngl_ctx *ctx = node->ctx;
     struct glcontext *gl = ctx->glcontext;
 
     struct texture *s = node->priv_data;
     struct media *media = s->data_src->priv_data;
+    struct hwupload_mc *mc = s->hwupload_priv_data;
 
     static const float corner[3] = {-1.0, -1.0, 0.0};
     static const float width[3]  = { 2.0,  0.0, 0.0};
     static const float height[3] = { 0.0,  2.0, 0.0};
 
-    if (s->hwupload_fmt == config->format)
-        return 0;
-
-    struct hwupload_mc *mc = calloc(1, sizeof(*mc));
-    if (!mc)
-        return -1;
-
-    s->hwupload_fmt = config->format;
-    s->hwupload_priv_data = mc;
-
-    s->data_format = config->data_format;
+    s->data_format = NGLI_FORMAT_R8G8B8A8_UNORM;
     int ret = ngli_format_get_gl_format_type(gl,
                                              s->data_format,
                                              &s->format,
@@ -122,7 +78,7 @@ int ngli_hwupload_mc_init(struct ngl_node *node,
     if (ret < 0)
         return ret;
 
-    ret = ngli_texture_update_local_texture(node, config->width, config->height, 0, NULL);
+    ret = ngli_texture_update_local_texture(node, frame->width, frame->height, 0, NULL);
     if (ret < 0)
         return ret;
 
@@ -193,9 +149,23 @@ int ngli_hwupload_mc_init(struct ngl_node *node,
     return 0;
 }
 
-int ngli_hwupload_mc_upload(struct ngl_node *node,
-                            struct hwupload_config *config,
-                            struct sxplayer_frame *frame)
+static void mc_uninit(struct ngl_node *node)
+{
+    struct texture *s = node->priv_data;
+    struct hwupload_mc *mc = s->hwupload_priv_data;
+
+    if (mc->rtt)
+        ngli_node_detach_ctx(mc->rtt);
+
+    ngl_node_unrefp(&mc->quad);
+    ngl_node_unrefp(&mc->program);
+    ngl_node_unrefp(&mc->render);
+    ngl_node_unrefp(&mc->texture);
+    ngl_node_unrefp(&mc->target_texture);
+    ngl_node_unrefp(&mc->rtt);
+}
+
+static int mc_map_frame(struct ngl_node *node, struct sxplayer_frame *frame)
 {
     int ret;
 
@@ -219,13 +189,13 @@ int ngli_hwupload_mc_upload(struct ngl_node *node,
         0.0f,  1.0f, 0.0f, 1.0f,
     };
 
-    ret = ngli_texture_update_local_texture(node, config->width, config->height, 0, NULL);
+    ret = ngli_texture_update_local_texture(node, frame->width, frame->height, 0, NULL);
     if (ret < 0)
         return ret;
 
     if (ret) {
-        ngli_hwupload_uninit(node);
-        ret = ngli_hwupload_mc_init(node, config);
+        mc_uninit(node);
+        ret = mc_init(node, frame);
         if (ret < 0)
             return ret;
     }
@@ -256,40 +226,13 @@ int ngli_hwupload_mc_upload(struct ngl_node *node,
     return 0;
 }
 
-void ngli_hwupload_mc_uninit(struct ngl_node *node)
-{
-    struct texture *s = node->priv_data;
-    s->hwupload_fmt = NGLI_HWUPLOAD_FMT_NONE;
-
-    struct hwupload_mc *mc = s->hwupload_priv_data;
-    if (!mc)
-        return;
-
-    if (mc->rtt)
-        ngli_node_detach_ctx(mc->rtt);
-
-    ngl_node_unrefp(&mc->quad);
-    ngl_node_unrefp(&mc->program);
-    ngl_node_unrefp(&mc->render);
-    ngl_node_unrefp(&mc->texture);
-    ngl_node_unrefp(&mc->target_texture);
-    ngl_node_unrefp(&mc->rtt);
-
-    free(s->hwupload_priv_data);
-    s->hwupload_priv_data = NULL;
-}
-
-int ngli_hwupload_mc_dr_init(struct ngl_node *node,
-                             struct hwupload_config *config)
+static int mc_dr_init(struct ngl_node *node, struct sxplayer_frame *frame)
 {
     struct ngl_ctx *ctx = node->ctx;
     struct glcontext *gl = ctx->glcontext;
 
     struct texture *s = node->priv_data;
     struct media *media = s->data_src->priv_data;
-
-    if (s->hwupload_fmt == config->format)
-        return 0;
 
     GLint id = media->android_texture_id;
     GLenum target = media->android_texture_target;
@@ -299,7 +242,6 @@ int ngli_hwupload_mc_dr_init(struct ngl_node *node,
     ngli_glTexParameteri(gl, target, GL_TEXTURE_MAG_FILTER, s->mag_filter);
     ngli_glBindTexture(gl, target, 0);
 
-    s->hwupload_fmt = config->format;
     s->layout = NGLI_TEXTURE_LAYOUT_MEDIACODEC;
     s->planes[0].id = id;
     s->planes[0].target = target;
@@ -307,9 +249,7 @@ int ngli_hwupload_mc_dr_init(struct ngl_node *node,
     return 0;
 }
 
-int ngli_hwupload_mc_dr_upload(struct ngl_node *node,
-                               struct hwupload_config *config,
-                               struct sxplayer_frame *frame)
+static int mc_dr_map_frame(struct ngl_node *node, struct sxplayer_frame *frame)
 {
     struct texture *s = node->priv_data;
 
@@ -330,8 +270,8 @@ int ngli_hwupload_mc_dr_upload(struct ngl_node *node,
         0.0f,  1.0f, 0.0f, 1.0f,
     };
 
-    s->width  = config->width;
-    s->height = config->height;
+    s->width  = frame->width;
+    s->height = frame->height;
 
     ngli_android_surface_render_buffer(media->android_surface, buffer, matrix);
     ngli_mat4_mul(s->coordinates_matrix, flip_matrix, matrix);
@@ -339,8 +279,42 @@ int ngli_hwupload_mc_dr_upload(struct ngl_node *node,
     return 0;
 }
 
-void ngli_hwupload_mc_dr_uninit(struct ngl_node *node)
+static const struct hwmap_class hwmap_mc_class = {
+    .name      = "mediacodec (oes → 2d)",
+    .priv_size = sizeof(struct hwupload_mc),
+    .init      = mc_init,
+    .map_frame = mc_map_frame,
+    .uninit    = mc_uninit,
+};
+
+static const struct hwmap_class hwmap_mc_dr_class = {
+    .name      = "mediacodec (oes zero-copy)",
+    .init      = mc_dr_init,
+    .map_frame = mc_dr_map_frame,
+};
+
+static const struct hwmap_class *mc_get_hwmap(struct ngl_node *node, struct sxplayer_frame *frame)
 {
     struct texture *s = node->priv_data;
-    s->hwupload_fmt = NGLI_HWUPLOAD_FMT_NONE;
+
+    if (s->direct_rendering) {
+        if (s->min_filter != GL_NEAREST && s->min_filter != GL_LINEAR) {
+            LOG(WARNING, "external textures only support nearest and linear filtering: "
+                "disabling direct rendering");
+            s->direct_rendering = 0;
+        } else if (s->wrap_s != GL_CLAMP_TO_EDGE || s->wrap_t != GL_CLAMP_TO_EDGE) {
+            LOG(WARNING, "external textures only support clamp to edge wrapping: "
+                "disabling direct rendering");
+            s->direct_rendering = 0;
+        }
+    }
+
+    if (s->direct_rendering)
+        return &hwmap_mc_dr_class;
+
+    return &hwmap_mc_class;
 }
+
+const struct hwupload_class ngli_hwupload_mc_class = {
+    .get_hwmap = mc_get_hwmap,
+};

libnodegl/hwupload_mediacodec.h
@@ -1,52 +1,0 @@
-/*
- * Copyright 2018 GoPro Inc.
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-#ifndef HWUPLOAD_MC
-#define HWUPLOAD_MC
-
-#include <sxplayer.h>
-
-#include "hwupload.h"
-#include "nodes.h"
-
-int ngli_hwupload_mc_get_config_from_frame(struct ngl_node *node,
-                                           struct sxplayer_frame *frame,
-                                           struct hwupload_config *config);
-
-int ngli_hwupload_mc_init(struct ngl_node *node,
-                          struct hwupload_config *config);
-
-int ngli_hwupload_mc_upload(struct ngl_node *node,
-                            struct hwupload_config *config,
-                            struct sxplayer_frame *frame);
-
-void ngli_hwupload_mc_uninit(struct ngl_node *node);
-
-int ngli_hwupload_mc_dr_init(struct ngl_node *node,
-                             struct hwupload_config *config);
-
-int ngli_hwupload_mc_dr_upload(struct ngl_node *node,
-                               struct hwupload_config *config,
-                               struct sxplayer_frame *frame);
-
-void ngli_hwupload_mc_dr_uninit(struct ngl_node *node);
-
-#endif

commit f5cbab8665a9f90cad526e082644b29fbaea1fa2

hwupload: restore texture timestamp
Fixes regression introduced by 9e87c751c5948bb3ddb4c8ee418871b5d62c597d.

libnodegl/hwupload.c
@@ -73,6 +73,8 @@ int ngli_hwupload_upload_frame(struct ngl_node *node)
         return 0;
     media->frame = NULL;
 
+    s->data_src_ts = frame->ts;
+
     const struct hwmap_class *hwmap_class = get_hwmap_class(node, frame);
     if (!hwmap_class) {
         sxplayer_release_frame(frame);