NopeForge/nope.gl

commit d62e436416d35fbb5944dc01309531c6fedb696e

utils/viewer: fix ui freezes with the native gl view on macos 10.14 (mojave)

pynodegl-utils/pynodegl_utils/ui/gl_view.py
@@ -62,7 +62,7 @@ class _GLWidget(QtWidgets.QWidget):
         super(_GLWidget, self).resizeEvent(event)
 
     def event(self, event):
-        if event.type() == QEvent.UpdateRequest:
+        if event.type() == QEvent.Paint:
             if not self._player:
                 self._player = player.Player(
                     self.winId(),

pynodegl-utils/pynodegl_utils/viewer.py
@@ -23,7 +23,7 @@
 
 import sys
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 
 from ui.main_window import MainWindow
 
@@ -41,6 +41,8 @@ def run():
     pargs = parser.parse_args(sys.argv[1:])
 
     app = QtWidgets.QApplication(sys.argv)
+    # Fixes ui freezes with the native gl view on macos 10.14 (mojave)
+    app.setAttribute(QtCore.Qt.AA_DontCreateNativeWidgetSiblings)
     window = MainWindow(pargs.module, pargs.assets_dir, pargs.hooksdir)
     window.show()
     app.exec_()

commit e68b08639728aa4035f5b0797a27748edd19fb42

utils/viewer: fix gl view paint event handling
Fixes regression introduced by d62e436416d35fbb5944dc01309531c6fedb696e.

pynodegl-utils/pynodegl_utils/ui/gl_view.py
@@ -73,9 +73,8 @@ class _GLWidget(QtWidgets.QWidget):
                 self._player.start()
                 self._player.onFrame.connect(self._set_last_frame_time)
                 self.onPlayerAvailable.emit()
-            return super(_GLWidget, self).event(event)
-        elif event.type() == QEvent.Paint:
-            self._player.draw()
+            else:
+                self._player.draw()
             return super(_GLWidget, self).event(event)
         elif event.type() == QEvent.Close:
             if self._player: