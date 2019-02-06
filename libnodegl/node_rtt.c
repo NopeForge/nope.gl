NopeForge/nope.gl

commit 60efacb6d92dfeacf49844cd5a5550765cf679ce

fbo: rework internal API

libnodegl/node_rtt.c
@@ -103,7 +103,6 @@ static int rtt_prefetch(struct ngl_node *node)
     s->width = params->width;
     s->height = params->height;
 
-
     if (!(gl->features & NGLI_FEATURE_FRAMEBUFFER_OBJECT) &&
         s->samples > 0) {
         LOG(WARNING, "context does not support the framebuffer object feature, multisample anti-aliasing will be disabled");
@@ -120,53 +119,69 @@ static int rtt_prefetch(struct ngl_node *node)
         }
     }
 
-    int ret;
-    struct fbo *fbo = &s->fbo;
-    if ((ret = ngli_fbo_init(fbo, gl, params->width, params->height, 0)) < 0 ||
-        (ret = ngli_fbo_attach_texture(&s->fbo, params->format, t->id))  < 0)
-        return ret;
+    struct texture_params attachment_params = NGLI_TEXTURE_PARAM_DEFAULTS;
+    attachment_params.width = s->width;
+    attachment_params.height = s->height;
+    attachment_params.usage = NGLI_TEXTURE_USAGE_ATTACHMENT_ONLY;
+
+    const struct texture *attachments[2] = {&texture->texture};
+    int nb_attachments = 1;
 
     GLenum depth_format = NGLI_FORMAT_UNDEFINED;
-
     if (depth_texture) {
         struct texture *dt = &depth_texture->texture;
         depth_format = depth_texture_params->format;
         s->features |= FEATURE_DEPTH;
         s->features |= has_stencil(depth_format) ? FEATURE_STENCIL : 0;
-
-        ret = ngli_fbo_attach_texture(&s->fbo, depth_format, dt->id);
-        if (ret < 0)
-            return ret;
+        attachments[nb_attachments++] = dt;
     } else {
         if (s->features & FEATURE_STENCIL)
             depth_format = NGLI_FORMAT_D24_UNORM_S8_UINT;
         else if (s->features & FEATURE_DEPTH)
             depth_format = NGLI_FORMAT_D16_UNORM;
 
-        if (depth_format) {
-            ret = ngli_fbo_create_renderbuffer(fbo, depth_format);
-            if (ret < 0)
-                return ret;
-        }
+        attachment_params.format = depth_format;
+        int ret = ngli_texture_init(&s->fbo_depth, gl, &attachment_params);
+        if (ret < 0)
+            return ret;
+        attachments[nb_attachments++] = &s->fbo_depth;
     }
 
-    ret = ngli_fbo_allocate(fbo);
+    struct fbo_params fbo_params = {
+        .width = s->width,
+        .height = s->height,
+        .nb_attachments = nb_attachments,
+        .attachments = attachments,
+    };
+    int ret = ngli_fbo_init(&s->fbo, gl, &fbo_params);
     if (ret < 0)
         return ret;
 
     if (s->samples > 0) {
-        struct fbo *fbo_ms = &s->fbo_ms;
-        if ((ret = ngli_fbo_init(fbo_ms, gl, s->width, s->height, s->samples)) < 0 ||
-            (ret = ngli_fbo_create_renderbuffer(fbo_ms, params->format)) < 0)
+        const struct texture *attachments[2] = {&s->fbo_ms_color};
+        int nb_attachments = 1;
+
+        attachment_params.samples = s->samples;
+        attachment_params.format = params->format;
+        int ret = ngli_texture_init(&s->fbo_ms_color, gl, &attachment_params);
+        if (ret < 0)
             return ret;
 
-        if ((s->features & FEATURE_DEPTH) || (s->features & FEATURE_STENCIL)) {
-            ret = ngli_fbo_create_renderbuffer(fbo_ms, depth_format);
+        if (depth_format != NGLI_FORMAT_UNDEFINED) {
+            attachment_params.format = depth_format;
+            int ret = ngli_texture_init(&s->fbo_ms_depth, gl, &attachment_params);
             if (ret < 0)
                 return ret;
+            attachments[nb_attachments++] = &s->fbo_ms_depth;
         }
 
-        ret = ngli_fbo_allocate(fbo_ms);
+        struct fbo_params fbo_params = {
+            .width = s->width,
+            .height = s->height,
+            .nb_attachments = nb_attachments,
+            .attachments = attachments,
+        };
+        ret = ngli_fbo_init(&s->fbo_ms, gl, &fbo_params);
         if (ret < 0)
             return ret;
     }
@@ -268,7 +283,11 @@ static void rtt_release(struct ngl_node *node)
     struct rtt_priv *s = node->priv_data;
 
     ngli_fbo_reset(&s->fbo);
+    ngli_texture_reset(&s->fbo_depth);
+
     ngli_fbo_reset(&s->fbo_ms);
+    ngli_texture_reset(&s->fbo_ms_color);
+    ngli_texture_reset(&s->fbo_ms_depth);
 }
 
 const struct node_class ngli_rtt_class = {

libnodegl/fbo.c
@@ -26,22 +26,9 @@
 #include "glcontext.h"
 #include "glincludes.h"
 #include "log.h"
+#include "texture.h"
 
-enum fbo_attachment_type {
-    FBO_ATTACHMENT_TYPE_NONE,
-    FBO_ATTACHMENT_TYPE_RENDERBUFFER,
-    FBO_ATTACHMENT_TYPE_TEXTURE,
-};
-
-struct fbo_attachment {
-    enum fbo_attachment_type type;
-    int is_external;
-    GLuint id;
-    GLenum index;
-    GLenum format;
-};
-
-static GLenum get_gl_index(GLenum format)
+static GLenum get_gl_attachment_index(GLenum format)
 {
     switch (format) {
     case GL_DEPTH_COMPONENT:
@@ -61,126 +48,13 @@ static GLenum get_gl_index(GLenum format)
     }
 }
 
-static int has_attachment(struct fbo *fbo, int index)
-{
-    struct fbo_attachment *attachments = ngli_darray_data(&fbo->attachments);
-    for (int i = 0; i < ngli_darray_count(&fbo->attachments); i++) {
-        struct fbo_attachment *attachment = &attachments[i];
-        if (attachment->index == index)
-            return 1;
-    }
-    return 0;
-}
-
-int ngli_fbo_init(struct fbo *fbo, struct glcontext *gl, int width, int height, int samples)
+int ngli_fbo_init(struct fbo *fbo, struct glcontext *gl, const struct fbo_params *params)
 {
     fbo->gl = gl;
-    fbo->width = width;
-    fbo->height = height;
-    fbo->samples = samples;
-    fbo->id = 0;
-    ngli_darray_init(&fbo->attachments, sizeof(struct fbo_attachment), 0);
+    fbo->width = params->width;
+    fbo->height = params->height;
+
     ngli_darray_init(&fbo->depth_indices, sizeof(GLenum), 0);
-    return 0;
-}
-
-int ngli_fbo_resize(struct fbo *fbo, int width, int height)
-{
-    struct glcontext *gl = fbo->gl;
-
-    fbo->width = width;
-    fbo->height = height;
-
-    struct fbo_attachment *attachments = ngli_darray_data(&fbo->attachments);
-    for (int i = 0; i < ngli_darray_count(&fbo->attachments); i++) {
-        struct fbo_attachment *attachment = &attachments[i];
-        if (!attachment->id || attachment->is_external)
-            continue;
-        if (attachment->type == FBO_ATTACHMENT_TYPE_RENDERBUFFER) {
-            ngli_glBindRenderbuffer(gl, GL_RENDERBUFFER, attachment->id);
-            if (fbo->samples > 0)
-                ngli_glRenderbufferStorageMultisample(gl, GL_RENDERBUFFER, fbo->samples, attachment->format, fbo->width, fbo->height);
-            else
-                ngli_glRenderbufferStorage(gl, GL_RENDERBUFFER, attachment->format, fbo->width, fbo->height);
-            ngli_glBindRenderbuffer(gl, GL_RENDERBUFFER, 0);
-        }
-    }
-
-    return 0;
-}
-
-int ngli_fbo_create_renderbuffer(struct fbo *fbo, int format)
-{
-    struct glcontext *gl = fbo->gl;
-
-    GLenum gl_format;
-    int ret = ngli_format_get_gl_renderbuffer_format(gl, format, (GLint *)&gl_format);
-    if (ret < 0)
-        return ret;
-
-    GLenum gl_index = get_gl_index(gl_format);
-    ngli_assert(!has_attachment(fbo, gl_index));
-
-    if (gl->features & NGLI_FEATURE_INTERNALFORMAT_QUERY) {
-        GLint samples;
-        ngli_glGetInternalformativ(gl, GL_RENDERBUFFER, gl_format, GL_SAMPLES, 1, &samples);
-        if (fbo->samples > samples) {
-            LOG(WARNING, "renderbuffer format 0x%x does not support requested samples %d (maximum %d)", gl_format, fbo->samples, samples);
-            fbo->samples = samples;
-        }
-    }
-
-    struct fbo_attachment *attachment = ngli_darray_push(&fbo->attachments, NULL);
-    if (!attachment)
-        return -1;
-
-    attachment->type = FBO_ATTACHMENT_TYPE_RENDERBUFFER;
-    attachment->is_external = 0;
-    attachment->id = 0;
-    attachment->index = gl_index;
-    attachment->format = gl_format;
-
-    return 0;
-}
-
-static int attach(struct fbo *fbo, enum fbo_attachment_type type, int format, GLuint id)
-{
-    struct glcontext *gl = fbo->gl;
-
-    GLenum gl_format;
-    int ret = ngli_format_get_gl_renderbuffer_format(gl, format, (GLint *)&gl_format);
-    if (ret < 0)
-        return ret;
-
-    GLenum gl_index = get_gl_index(gl_format);
-    ngli_assert(!has_attachment(fbo, gl_index));
-
-    struct fbo_attachment *attachment = ngli_darray_push(&fbo->attachments, NULL);
-    if (!attachment)
-        return -1;
-
-    attachment->type = type;
-    attachment->is_external = 1;
-    attachment->id = id;
-    attachment->index = gl_index;
-    attachment->format = gl_format;
-
-    return 0;
-}
-
-int ngli_fbo_attach_renderbuffer(struct fbo *fbo, int format, GLuint renderbuffer)
-{
-    return attach(fbo, FBO_ATTACHMENT_TYPE_RENDERBUFFER, format, renderbuffer);
-}
-
-int ngli_fbo_attach_texture(struct fbo *fbo, int format, GLuint texture)
-{
-    return attach(fbo, FBO_ATTACHMENT_TYPE_TEXTURE, format, texture);
-}
-
-int ngli_fbo_allocate(struct fbo *fbo)
-{
-    struct glcontext *gl = fbo->gl;
 
     GLuint fbo_id = 0;
     ngli_glGetIntegerv(gl, GL_FRAMEBUFFER_BINDING, (GLint *)&fbo_id);
@@ -188,32 +62,38 @@ int ngli_fbo_allocate(struct fbo *fbo)
     ngli_glGenFramebuffers(gl, 1, &fbo->id);
     ngli_glBindFramebuffer(gl, GL_FRAMEBUFFER, fbo->id);
 
-    struct fbo_attachment *attachments = ngli_darray_data(&fbo->attachments);
-    for (int i = 0; i < ngli_darray_count(&fbo->attachments); i++) {
-        struct fbo_attachment *attachment = &attachments[i];
-        if (attachment->type == FBO_ATTACHMENT_TYPE_RENDERBUFFER) {
-            if (!attachment->is_external) {
-                ngli_glGenRenderbuffers(gl, 1, &attachment->id);
-                ngli_glBindRenderbuffer(gl, GL_RENDERBUFFER, attachment->id);
-                if (fbo->samples > 0)
-                    ngli_glRenderbufferStorageMultisample(gl, GL_RENDERBUFFER, fbo->samples, attachment->format, fbo->width, fbo->height);
-                else
-                    ngli_glRenderbufferStorage(gl, GL_RENDERBUFFER, attachment->format, fbo->width, fbo->height);
-                ngli_glBindRenderbuffer(gl, GL_RENDERBUFFER, 0);
+    int color_index = 0;
+    for (int i = 0; i < params->nb_attachments; i++) {
+        const struct texture *attachment = params->attachments[i];
+
+        GLenum attachment_index = get_gl_attachment_index(attachment->format);
+        const int is_color_attachment = attachment_index == GL_COLOR_ATTACHMENT0;
+        if (is_color_attachment) {
+            if (color_index >= gl->max_color_attachments) {
+                LOG(ERROR, "could not attach color buffer %d (maximum %d)",
+                    color_index, gl->max_color_attachments);
+                ngli_glBindFramebuffer(gl, GL_FRAMEBUFFER, fbo_id);
+                return -1;
             }
-            if (gl->backend == NGL_BACKEND_OPENGLES && gl->version < 300 && attachment->index == GL_DEPTH_STENCIL_ATTACHMENT) {
+            attachment_index = attachment_index + color_index++;
+        }
+
+        switch (attachment->target) {
+        case GL_RENDERBUFFER:
+            if (gl->backend == NGL_BACKEND_OPENGLES && gl->version < 300 && attachment_index == GL_DEPTH_STENCIL_ATTACHMENT) {
                 ngli_glFramebufferRenderbuffer(gl, GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, attachment->id);
                 ngli_glFramebufferRenderbuffer(gl, GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, attachment->id);
             } else {
-                ngli_glFramebufferRenderbuffer(gl, GL_FRAMEBUFFER, attachment->index, GL_RENDERBUFFER, attachment->id);
+                ngli_glFramebufferRenderbuffer(gl, GL_FRAMEBUFFER, attachment_index, GL_RENDERBUFFER, attachment->id);
             }
-        } else if (attachment->type == FBO_ATTACHMENT_TYPE_TEXTURE) {
-            ngli_glFramebufferTexture2D(gl, GL_FRAMEBUFFER, attachment->index, GL_TEXTURE_2D, attachment->id, 0);
-        } else {
+            if (!is_color_attachment)
+                ngli_darray_push(&fbo->depth_indices, &attachment_index);
+            break;
+        case GL_TEXTURE_2D:
+            ngli_glFramebufferTexture2D(gl, GL_FRAMEBUFFER, attachment_index, GL_TEXTURE_2D, attachment->id, 0);
+            break;
+        default:
             ngli_assert(0);
-        }
-        if (attachment->index != GL_COLOR_ATTACHMENT0) {
-            ngli_darray_push(&fbo->depth_indices, &attachment->index);
         }
     }
 
@@ -224,6 +104,7 @@ int ngli_fbo_allocate(struct fbo *fbo)
     }
 
     ngli_glBindFramebuffer(gl, GL_FRAMEBUFFER, fbo_id);
+
     return 0;
 }
 
@@ -282,16 +163,6 @@ void ngli_fbo_reset(struct fbo *fbo)
 
     ngli_glDeleteFramebuffers(gl, 1, &fbo->id);
 
-    struct fbo_attachment *attachments = ngli_darray_data(&fbo->attachments);
-    for (int i = 0; i < ngli_darray_count(&fbo->attachments); i++) {
-        struct fbo_attachment *attachment = &attachments[i];
-        if (attachment->is_external)
-            continue;
-        if (attachment->type == FBO_ATTACHMENT_TYPE_RENDERBUFFER) {
-            ngli_glDeleteRenderbuffers(gl, 1, &attachment->id);
-        }
-    }
-    ngli_darray_reset(&fbo->attachments);
     ngli_darray_reset(&fbo->depth_indices);
 
     memset(fbo, 0, sizeof(*fbo));

libnodegl/fbo.h
@@ -24,24 +24,26 @@
 
 #include "darray.h"
 #include "glcontext.h"
+#include "texture.h"
+
+struct fbo_params {
+    int width;
+    int height;
+    int nb_attachments;
+    const struct texture **attachments;
+};
 
 struct fbo {
     struct glcontext *gl;
     int width;
     int height;
-    int samples;
+
     GLuint id;
     GLuint prev_id;
-    struct darray attachments;
     struct darray depth_indices;
 };
 
-int ngli_fbo_init(struct fbo *fbo, struct glcontext *gl, int width, int height, int samples);
-int ngli_fbo_resize(struct fbo *fbo, int width, int height);
-int ngli_fbo_create_renderbuffer(struct fbo *fbo, int format);
-int ngli_fbo_attach_renderbuffer(struct fbo *fbo, int format, GLuint renderbuffer);
-int ngli_fbo_attach_texture(struct fbo *fbo, int format, GLuint texture);
-int ngli_fbo_allocate(struct fbo *fbo);
+int ngli_fbo_init(struct fbo *fbo, struct glcontext *gl, const struct fbo_params *params);
 int ngli_fbo_bind(struct fbo *fbo);
 int ngli_fbo_unbind(struct fbo *fbo);
 void ngli_fbo_invalidate_depth_buffers(struct fbo *fbo);

libnodegl/glcontext_eagl.m
@@ -41,7 +41,12 @@ struct eagl_priv {
     CVOpenGLESTextureCacheRef texture_cache;
     GLuint colorbuffer;
     struct fbo fbo;
+    struct texture fbo_color;
+    struct texture fbo_depth;
+
     struct fbo fbo_ms;
+    struct texture fbo_ms_color;
+    struct texture fbo_ms_depth;
 };
 
 static int eagl_init_layer(struct glcontext *ctx)
@@ -162,9 +167,13 @@ static int eagl_init_framebuffer(struct glcontext *ctx)
     struct fbo *fbo = &eagl->fbo;
     struct fbo *fbo_ms = &eagl->fbo_ms;
 
-    int ret = ngli_fbo_init(fbo, ctx, ctx->width, ctx->height, 0);
-    if (ret < 0)
-        return ret;
+    const struct texture *attachments[2] = {&eagl->fbo_color};
+    int nb_attachments = 1;
+
+    struct texture_params attachment_params = NGLI_TEXTURE_PARAM_DEFAULTS;
+    attachment_params.format = NGLI_FORMAT_B8G8R8A8_UNORM;
+    attachment_params.width = ctx->width;
+    attachment_params.height = ctx->height;
 
     if (eagl->pixel_buffer) {
         ctx->width = CVPixelBufferGetWidth(eagl->pixel_buffer);
@@ -192,42 +201,74 @@ static int eagl_init_framebuffer(struct glcontext *ctx)
         ngli_glTexParameteri(ctx, GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         ngli_glBindTexture(ctx, GL_TEXTURE_2D, 0);
 
-        if ((ret = ngli_fbo_resize(fbo, ctx->width, ctx->height))                < 0 ||
-            (ret = ngli_fbo_attach_texture(fbo, NGLI_FORMAT_B8G8R8A8_UNORM, id)) < 0)
+        int ret = ngli_texture_wrap(&eagl->fbo_color, ctx, &attachment_params, id);
+        if (ret < 0)
             return ret;
     } else {
         if (ctx->offscreen) {
-            ret = ngli_fbo_create_renderbuffer(fbo, NGLI_FORMAT_B8G8R8A8_UNORM);
+            attachment_params.usage = NGLI_TEXTURE_USAGE_ATTACHMENT_ONLY;
+            int ret = ngli_texture_init(&eagl->fbo_color, ctx, &attachment_params);
             if (ret < 0)
                 return ret;
         } else {
-            ngli_glGenRenderbuffers(ctx, 1, &eagl->colorbuffer);
+            if (!eagl->colorbuffer)
+                ngli_glGenRenderbuffers(ctx, 1, &eagl->colorbuffer);
             ngli_glBindRenderbuffer (ctx, GL_RENDERBUFFER, eagl->colorbuffer);
             [eagl->handle renderbufferStorage:GL_RENDERBUFFER fromDrawable:eagl->layer];
             ngli_glGetRenderbufferParameteriv(ctx, GL_RENDERBUFFER, GL_RENDERBUFFER_WIDTH, &ctx->width);
             ngli_glGetRenderbufferParameteriv(ctx, GL_RENDERBUFFER, GL_RENDERBUFFER_HEIGHT, &ctx->height);
 
-            if ((ret = ngli_fbo_resize(fbo, ctx->width, ctx->height))                                    < 0 ||
-                (ret = ngli_fbo_attach_renderbuffer(fbo, NGLI_FORMAT_B8G8R8A8_UNORM, eagl->colorbuffer)) < 0)
+            attachment_params.width = ctx->width;
+            attachment_params.height = ctx->height;
+            attachment_params.usage = NGLI_TEXTURE_USAGE_ATTACHMENT_ONLY;
+            int ret = ngli_texture_wrap(&eagl->fbo_color, ctx, &attachment_params, eagl->colorbuffer);
+            if (ret < 0)
                 return ret;
         }
     }
 
     if (!ctx->samples) {
-        ret = ngli_fbo_create_renderbuffer(fbo, NGLI_FORMAT_D24_UNORM_S8_UINT);
+        attachment_params.format = NGLI_FORMAT_D24_UNORM_S8_UINT;
+        attachment_params.usage = NGLI_TEXTURE_USAGE_ATTACHMENT_ONLY;
+        int ret = ngli_texture_init(&eagl->fbo_depth, ctx, &attachment_params);
         if (ret < 0)
             return ret;
+        attachments[nb_attachments++] = &eagl->fbo_depth;
     }
 
-    ret = ngli_fbo_allocate(fbo);
+    const struct fbo_params fbo_params = {
+        .width = ctx->width,
+        .height = ctx->height,
+        .nb_attachments = nb_attachments,
+        .attachments = attachments,
+    };
+    int ret = ngli_fbo_init(fbo, ctx, &fbo_params);
     if (ret < 0)
         return ret;
 
     if (ctx->samples > 0) {
-        if ((ret = ngli_fbo_init(fbo_ms, ctx, ctx->width, ctx->height, ctx->samples))   < 0 ||
-            (ret = ngli_fbo_create_renderbuffer(fbo_ms, NGLI_FORMAT_B8G8R8A8_UNORM))    < 0 ||
-            (ret = ngli_fbo_create_renderbuffer(fbo_ms, NGLI_FORMAT_D24_UNORM_S8_UINT)) < 0 ||
-            (ret = ngli_fbo_allocate(fbo_ms))                                           < 0)
+        attachment_params.format = NGLI_FORMAT_B8G8R8A8_UNORM;
+        attachment_params.samples = ctx->samples;
+        attachment_params.usage = NGLI_TEXTURE_USAGE_ATTACHMENT_ONLY;
+        int ret = ngli_texture_init(&eagl->fbo_ms_color, ctx, &attachment_params);
+        if (ret < 0)
+            return ret;
+
+        attachment_params.format = NGLI_FORMAT_D24_UNORM_S8_UINT;
+        ret = ngli_texture_init(&eagl->fbo_ms_depth, ctx, &attachment_params);
+        if (ret < 0)
+            return ret;
+
+        const struct texture *attachments[] = {&eagl->fbo_ms_color, &eagl->fbo_ms_depth};
+        const int nb_attachments = NGLI_ARRAY_NB(attachments);
+        const struct fbo_params fbo_params = {
+            .width = ctx->width,
+            .height = ctx->height,
+            .nb_attachments = nb_attachments,
+            .attachments = attachments,
+        };
+        ret = ngli_fbo_init(fbo_ms, ctx, &fbo_params);
+        if (ret < 0)
             return ret;
     }
 
@@ -238,12 +279,24 @@ static int eagl_init_framebuffer(struct glcontext *ctx)
     return 0;
 }
 
-static void eagl_uninit(struct glcontext *ctx)
+static void eagl_reset_framebuffer(struct glcontext *ctx)
 {
     struct eagl_priv *eagl = ctx->priv_data;
 
     ngli_fbo_reset(&eagl->fbo);
+    ngli_texture_reset(&eagl->fbo_color);
+    ngli_texture_reset(&eagl->fbo_depth);
+
     ngli_fbo_reset(&eagl->fbo_ms);
+    ngli_texture_reset(&eagl->fbo_ms_color);
+    ngli_texture_reset(&eagl->fbo_ms_depth);
+}
+
+static void eagl_uninit(struct glcontext *ctx)
+{
+    struct eagl_priv *eagl = ctx->priv_data;
+
+    eagl_reset_framebuffer(ctx);
 
     if (eagl->colorbuffer)
         ngli_glDeleteRenderbuffers(ctx, 1, &eagl->colorbuffer);
@@ -266,36 +319,9 @@ static void eagl_uninit(struct glcontext *ctx)
 
 static int eagl_resize(struct glcontext *ctx, int width, int height)
 {
-    struct eagl_priv *eagl = ctx->priv_data;
+    eagl_reset_framebuffer(ctx);
 
-    if (![NSThread isMainThread]) {
-        __block int ret;
-        ngli_glcontext_make_current(ctx, 0);
-
-        dispatch_sync(dispatch_get_main_queue(), ^{
-            ngli_glcontext_make_current(ctx, 1);
-            ret = eagl_resize(ctx, width, height);
-            ngli_glcontext_make_current(ctx, 0);
-        });
-
-        ngli_glcontext_make_current(ctx, 1);
-        return ret;
-    }
-
-    glBindRenderbuffer (GL_RENDERBUFFER, eagl->colorbuffer);
-    [eagl->handle renderbufferStorage:GL_RENDERBUFFER fromDrawable:eagl->layer];
-    glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_WIDTH, &ctx->width);
-    glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_HEIGHT, &ctx->height);
-
-    ngli_fbo_resize(&eagl->fbo, ctx->width, ctx->height);
-    ngli_fbo_resize(&eagl->fbo_ms, ctx->width, ctx->height);
-
-    struct fbo *fbo = ctx->samples ? &eagl->fbo_ms : &eagl->fbo;
-    ngli_fbo_bind(fbo);
-
-    glViewport(0, 0, ctx->width, ctx->height);
-
-    return 0;
+    return eagl_init_framebuffer(ctx);
 }
 
 static int eagl_make_current(struct glcontext *ctx, int current)

libnodegl/glcontext_egl.c
@@ -51,6 +51,8 @@ struct egl_priv {
     EGLAPIENTRY EGLBoolean (*DestroyImageKHR)(EGLDisplay, EGLImageKHR);
     EGLAPIENTRY void (*EGLImageTargetTexture2DOES)(GLenum, GLeglImageOES);
     struct fbo fbo;
+    struct texture fbo_color;
+    struct texture fbo_depth;
 };
 
 EGLImageKHR ngli_eglCreateImageKHR(struct glcontext *gl, EGLConfig context, EGLenum target, EGLClientBuffer buffer, const EGLint *attrib_list)
@@ -288,12 +290,34 @@ static int egl_init_framebuffer(struct glcontext *ctx)
         ctx->samples = 0;
     }
 
-    int ret;
-    if ((ret = ngli_fbo_init(&egl->fbo, ctx, ctx->width, ctx->height, ctx->samples))   < 0 ||
-        (ret = ngli_fbo_create_renderbuffer(&egl->fbo, NGLI_FORMAT_R8G8B8A8_UNORM))    < 0 ||
-        (ret = ngli_fbo_create_renderbuffer(&egl->fbo, NGLI_FORMAT_D24_UNORM_S8_UINT)) < 0 ||
-        (ret = ngli_fbo_allocate(&egl->fbo))                                           < 0 ||
-        (ret = ngli_fbo_bind(&egl->fbo))                                               < 0)
+    struct texture_params attachment_params = NGLI_TEXTURE_PARAM_DEFAULTS;
+    attachment_params.format = NGLI_FORMAT_R8G8B8A8_UNORM;
+    attachment_params.width = ctx->width;
+    attachment_params.height = ctx->height;
+    attachment_params.samples = ctx->samples;
+    attachment_params.usage = NGLI_TEXTURE_USAGE_ATTACHMENT_ONLY;
+    int ret = ngli_texture_init(&egl->fbo_color, ctx, &attachment_params);
+    if (ret < 0)
+        return ret;
+
+    attachment_params.format = NGLI_FORMAT_D24_UNORM_S8_UINT;
+    ret = ngli_texture_init(&egl->fbo_depth, ctx, &attachment_params);
+    if (ret < 0)
+        return ret;
+
+    const struct texture *attachments[] = {&egl->fbo_color, &egl->fbo_depth};
+    struct fbo_params fbo_params = {
+        .width = ctx->width,
+        .height = ctx->height,
+        .nb_attachments = NGLI_ARRAY_NB(attachments),
+        .attachments = attachments,
+    };
+    ret = ngli_fbo_init(&egl->fbo, ctx, &fbo_params);
+    if (ret < 0)
+        return ret;
+
+    ret = ngli_fbo_bind(&egl->fbo);
+    if (ret < 0)
         return ret;
 
     return 0;
@@ -304,6 +328,9 @@ static void egl_uninit(struct glcontext *ctx)
     struct egl_priv *egl = ctx->priv_data;
 
     ngli_fbo_reset(&egl->fbo);
+    ngli_texture_reset(&egl->fbo_color);
+    ngli_texture_reset(&egl->fbo_depth);
+
     ngli_glcontext_make_current(ctx, 0);
 
     if (egl->surface)

libnodegl/glcontext_nsgl.m
@@ -41,6 +41,8 @@ struct nsgl_priv {
     int swap_event;
     NSCondition *swap_condition;
     struct fbo fbo;
+    struct texture fbo_color;
+    struct texture fbo_depth;
 };
 
 static CVReturn display_link_cb(CVDisplayLinkRef display_link,
@@ -143,13 +145,34 @@ static int nsgl_init_framebuffer(struct glcontext *ctx)
     if (!ctx->offscreen)
         return 0;
 
-    int ret;
-    struct fbo *fbo = &nsgl->fbo;
-    if ((ret = ngli_fbo_init(fbo, ctx, ctx->width, ctx->height, ctx->samples))   < 0 ||
-        (ret = ngli_fbo_create_renderbuffer(fbo, NGLI_FORMAT_R8G8B8A8_UNORM))    < 0 ||
-        (ret = ngli_fbo_create_renderbuffer(fbo, NGLI_FORMAT_D24_UNORM_S8_UINT)) < 0 ||
-        (ret = ngli_fbo_allocate(fbo))                                           < 0 ||
-        (ret = ngli_fbo_bind(fbo))                                               < 0)
+    struct texture_params attachment_params = NGLI_TEXTURE_PARAM_DEFAULTS;
+    attachment_params.format = NGLI_FORMAT_R8G8B8A8_UNORM;
+    attachment_params.width = ctx->width;
+    attachment_params.height = ctx->height;
+    attachment_params.samples = ctx->samples;
+    attachment_params.usage = NGLI_TEXTURE_USAGE_ATTACHMENT_ONLY;
+    int ret = ngli_texture_init(&nsgl->fbo_color, ctx, &attachment_params);
+    if (ret < 0)
+        return ret;
+
+    attachment_params.format = NGLI_FORMAT_D24_UNORM_S8_UINT;
+    ret = ngli_texture_init(&nsgl->fbo_depth, ctx, &attachment_params);
+    if (ret < 0)
+        return ret;
+
+    const struct texture *attachments[] = {&nsgl->fbo_color, &nsgl->fbo_depth};
+    struct fbo_params fbo_params = {
+        .width = ctx->width,
+        .height = ctx->height,
+        .attachments = attachments,
+        .nb_attachments = NGLI_ARRAY_NB(attachments),
+    };
+    ret = ngli_fbo_init(&nsgl->fbo, ctx, &fbo_params);
+    if (ret < 0)
+        return ret;
+
+    ret = ngli_fbo_bind(&nsgl->fbo);
+    if (ret < 0)
         return ret;
 
     glViewport(0, 0, ctx->width, ctx->height);
@@ -251,6 +274,8 @@ static void nsgl_uninit(struct glcontext *ctx)
         CFRelease(nsgl->swap_condition);
 
     ngli_fbo_reset(&nsgl->fbo);
+    ngli_texture_reset(&nsgl->fbo_color);
+    ngli_texture_reset(&nsgl->fbo_depth);
 
     if (nsgl->framework)
         CFRelease(nsgl->framework);

libnodegl/hwconv.c
@@ -140,11 +140,16 @@ int ngli_hwconv_init(struct hwconv *hwconv, struct glcontext *gl,
     hwconv->gl = gl;
     hwconv->src_layout = src_layout;
 
-    int ret;
     const struct texture_params *params = &dst_texture->params;
-    if ((ret = ngli_fbo_init(&hwconv->fbo, gl, params->width, params->height, 0))      < 0 ||
-        (ret = ngli_fbo_attach_texture(&hwconv->fbo, params->format, dst_texture->id)) < 0 ||
-        (ret = ngli_fbo_allocate(&hwconv->fbo))                                        < 0)
+
+    struct fbo_params fbo_params = {
+        .width = params->width,
+        .height = params->height,
+        .nb_attachments = 1,
+        .attachments = &dst_texture,
+    };
+    int ret = ngli_fbo_init(&hwconv->fbo, gl, &fbo_params);
+    if (ret < 0)
         return ret;
 
     if (src_layout != NGLI_IMAGE_LAYOUT_NV12 &&

libnodegl/hwconv.h
@@ -33,6 +33,8 @@ struct hwconv {
     enum image_layout src_layout;
 
     struct fbo fbo;
+    struct texture color_attachment;
+
     GLuint vao_id;
     GLuint program_id;
     GLuint vertices_id;

commit 134672d18affcf3f4aeefe1d3c7ffd34602d5723

rtt: only attach depth buffer if required
Fixes regression introduced by 60efacb6d92dfeacf49844cd5a5550765cf679ce.

libnodegl/node_rtt.c
@@ -140,11 +140,13 @@ static int rtt_prefetch(struct ngl_node *node)
         else if (s->features & FEATURE_DEPTH)
             depth_format = NGLI_FORMAT_D16_UNORM;
 
+        if (depth_format != NGLI_FORMAT_UNDEFINED) {
         attachment_params.format = depth_format;
         int ret = ngli_texture_init(&s->fbo_depth, gl, &attachment_params);
         if (ret < 0)
             return ret;
         attachments[nb_attachments++] = &s->fbo_depth;
+        }
     }
 
     struct fbo_params fbo_params = {