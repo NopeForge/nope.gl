wmacdonald-gpsw/gopro-lib-node.gl

internal: add drawutils API

libnodegl/drawutils.c
@@ -1,0 +1,202 @@
+/*
+ * Copyright 2019 GoPro Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include "drawutils.h"
+
+#define BYTE_PER_PIXEL 4 /* RGBA */
+
+static const uint8_t font8[128][8] = {
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x18, 0x3C, 0x3C, 0x18, 0x18, 0x00, 0x18, 0x00},
+    {0x36, 0x36, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x36, 0x36, 0x7F, 0x36, 0x7F, 0x36, 0x36, 0x00},
+    {0x0C, 0x3E, 0x03, 0x1E, 0x30, 0x1F, 0x0C, 0x00},
+    {0x00, 0x63, 0x33, 0x18, 0x0C, 0x66, 0x63, 0x00},
+    {0x1C, 0x36, 0x1C, 0x6E, 0x3B, 0x33, 0x6E, 0x00},
+    {0x06, 0x06, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x18, 0x0C, 0x06, 0x06, 0x06, 0x0C, 0x18, 0x00},
+    {0x06, 0x0C, 0x18, 0x18, 0x18, 0x0C, 0x06, 0x00},
+    {0x00, 0x66, 0x3C, 0xFF, 0x3C, 0x66, 0x00, 0x00},
+    {0x00, 0x0C, 0x0C, 0x3F, 0x0C, 0x0C, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x0C, 0x0C, 0x06},
+    {0x00, 0x00, 0x00, 0x3F, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x0C, 0x0C, 0x00},
+    {0x60, 0x30, 0x18, 0x0C, 0x06, 0x03, 0x01, 0x00},
+    {0x3E, 0x63, 0x73, 0x7B, 0x6F, 0x67, 0x3E, 0x00},
+    {0x0C, 0x0E, 0x0C, 0x0C, 0x0C, 0x0C, 0x3F, 0x00},
+    {0x1E, 0x33, 0x30, 0x1C, 0x06, 0x33, 0x3F, 0x00},
+    {0x1E, 0x33, 0x30, 0x1C, 0x30, 0x33, 0x1E, 0x00},
+    {0x38, 0x3C, 0x36, 0x33, 0x7F, 0x30, 0x78, 0x00},
+    {0x3F, 0x03, 0x1F, 0x30, 0x30, 0x33, 0x1E, 0x00},
+    {0x1C, 0x06, 0x03, 0x1F, 0x33, 0x33, 0x1E, 0x00},
+    {0x3F, 0x33, 0x30, 0x18, 0x0C, 0x0C, 0x0C, 0x00},
+    {0x1E, 0x33, 0x33, 0x1E, 0x33, 0x33, 0x1E, 0x00},
+    {0x1E, 0x33, 0x33, 0x3E, 0x30, 0x18, 0x0E, 0x00},
+    {0x00, 0x0C, 0x0C, 0x00, 0x00, 0x0C, 0x0C, 0x00},
+    {0x00, 0x0C, 0x0C, 0x00, 0x00, 0x0C, 0x0C, 0x06},
+    {0x18, 0x0C, 0x06, 0x03, 0x06, 0x0C, 0x18, 0x00},
+    {0x00, 0x00, 0x3F, 0x00, 0x00, 0x3F, 0x00, 0x00},
+    {0x06, 0x0C, 0x18, 0x30, 0x18, 0x0C, 0x06, 0x00},
+    {0x1E, 0x33, 0x30, 0x18, 0x0C, 0x00, 0x0C, 0x00},
+    {0x3E, 0x63, 0x7B, 0x7B, 0x7B, 0x03, 0x1E, 0x00},
+    {0x0C, 0x1E, 0x33, 0x33, 0x3F, 0x33, 0x33, 0x00},
+    {0x3F, 0x66, 0x66, 0x3E, 0x66, 0x66, 0x3F, 0x00},
+    {0x3C, 0x66, 0x03, 0x03, 0x03, 0x66, 0x3C, 0x00},
+    {0x1F, 0x36, 0x66, 0x66, 0x66, 0x36, 0x1F, 0x00},
+    {0x7F, 0x46, 0x16, 0x1E, 0x16, 0x46, 0x7F, 0x00},
+    {0x7F, 0x46, 0x16, 0x1E, 0x16, 0x06, 0x0F, 0x00},
+    {0x3C, 0x66, 0x03, 0x03, 0x73, 0x66, 0x7C, 0x00},
+    {0x33, 0x33, 0x33, 0x3F, 0x33, 0x33, 0x33, 0x00},
+    {0x1E, 0x0C, 0x0C, 0x0C, 0x0C, 0x0C, 0x1E, 0x00},
+    {0x78, 0x30, 0x30, 0x30, 0x33, 0x33, 0x1E, 0x00},
+    {0x67, 0x66, 0x36, 0x1E, 0x36, 0x66, 0x67, 0x00},
+    {0x0F, 0x06, 0x06, 0x06, 0x46, 0x66, 0x7F, 0x00},
+    {0x63, 0x77, 0x7F, 0x7F, 0x6B, 0x63, 0x63, 0x00},
+    {0x63, 0x67, 0x6F, 0x7B, 0x73, 0x63, 0x63, 0x00},
+    {0x1C, 0x36, 0x63, 0x63, 0x63, 0x36, 0x1C, 0x00},
+    {0x3F, 0x66, 0x66, 0x3E, 0x06, 0x06, 0x0F, 0x00},
+    {0x1E, 0x33, 0x33, 0x33, 0x3B, 0x1E, 0x38, 0x00},
+    {0x3F, 0x66, 0x66, 0x3E, 0x36, 0x66, 0x67, 0x00},
+    {0x1E, 0x33, 0x07, 0x0E, 0x38, 0x33, 0x1E, 0x00},
+    {0x3F, 0x2D, 0x0C, 0x0C, 0x0C, 0x0C, 0x1E, 0x00},
+    {0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x3F, 0x00},
+    {0x33, 0x33, 0x33, 0x33, 0x33, 0x1E, 0x0C, 0x00},
+    {0x63, 0x63, 0x63, 0x6B, 0x7F, 0x77, 0x63, 0x00},
+    {0x63, 0x63, 0x36, 0x1C, 0x1C, 0x36, 0x63, 0x00},
+    {0x33, 0x33, 0x33, 0x1E, 0x0C, 0x0C, 0x1E, 0x00},
+    {0x7F, 0x63, 0x31, 0x18, 0x4C, 0x66, 0x7F, 0x00},
+    {0x1E, 0x06, 0x06, 0x06, 0x06, 0x06, 0x1E, 0x00},
+    {0x03, 0x06, 0x0C, 0x18, 0x30, 0x60, 0x40, 0x00},
+    {0x1E, 0x18, 0x18, 0x18, 0x18, 0x18, 0x1E, 0x00},
+    {0x08, 0x1C, 0x36, 0x63, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF},
+    {0x0C, 0x0C, 0x18, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x1E, 0x30, 0x3E, 0x33, 0x6E, 0x00},
+    {0x07, 0x06, 0x06, 0x3E, 0x66, 0x66, 0x3B, 0x00},
+    {0x00, 0x00, 0x1E, 0x33, 0x03, 0x33, 0x1E, 0x00},
+    {0x38, 0x30, 0x30, 0x3e, 0x33, 0x33, 0x6E, 0x00},
+    {0x00, 0x00, 0x1E, 0x33, 0x3f, 0x03, 0x1E, 0x00},
+    {0x1C, 0x36, 0x06, 0x0f, 0x06, 0x06, 0x0F, 0x00},
+    {0x00, 0x00, 0x6E, 0x33, 0x33, 0x3E, 0x30, 0x1F},
+    {0x07, 0x06, 0x36, 0x6E, 0x66, 0x66, 0x67, 0x00},
+    {0x0C, 0x00, 0x0E, 0x0C, 0x0C, 0x0C, 0x1E, 0x00},
+    {0x30, 0x00, 0x30, 0x30, 0x30, 0x33, 0x33, 0x1E},
+    {0x07, 0x06, 0x66, 0x36, 0x1E, 0x36, 0x67, 0x00},
+    {0x0E, 0x0C, 0x0C, 0x0C, 0x0C, 0x0C, 0x1E, 0x00},
+    {0x00, 0x00, 0x33, 0x7F, 0x7F, 0x6B, 0x63, 0x00},
+    {0x00, 0x00, 0x1F, 0x33, 0x33, 0x33, 0x33, 0x00},
+    {0x00, 0x00, 0x1E, 0x33, 0x33, 0x33, 0x1E, 0x00},
+    {0x00, 0x00, 0x3B, 0x66, 0x66, 0x3E, 0x06, 0x0F},
+    {0x00, 0x00, 0x6E, 0x33, 0x33, 0x3E, 0x30, 0x78},
+    {0x00, 0x00, 0x3B, 0x6E, 0x66, 0x06, 0x0F, 0x00},
+    {0x00, 0x00, 0x3E, 0x03, 0x1E, 0x30, 0x1F, 0x00},
+    {0x08, 0x0C, 0x3E, 0x0C, 0x0C, 0x2C, 0x18, 0x00},
+    {0x00, 0x00, 0x33, 0x33, 0x33, 0x33, 0x6E, 0x00},
+    {0x00, 0x00, 0x33, 0x33, 0x33, 0x1E, 0x0C, 0x00},
+    {0x00, 0x00, 0x63, 0x6B, 0x7F, 0x7F, 0x36, 0x00},
+    {0x00, 0x00, 0x63, 0x36, 0x1C, 0x36, 0x63, 0x00},
+    {0x00, 0x00, 0x33, 0x33, 0x33, 0x3E, 0x30, 0x1F},
+    {0x00, 0x00, 0x3F, 0x19, 0x0C, 0x26, 0x3F, 0x00},
+    {0x38, 0x0C, 0x0C, 0x07, 0x0C, 0x0C, 0x38, 0x00},
+    {0x18, 0x18, 0x18, 0x00, 0x18, 0x18, 0x18, 0x00},
+    {0x07, 0x0C, 0x0C, 0x38, 0x0C, 0x0C, 0x07, 0x00},
+    {0x6E, 0x3B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+};
+
+static inline void set_color(uint8_t *p, uint32_t rgba)
+{
+    p[0] = rgba >> 24;
+    p[1] = rgba >> 16 & 0xff;
+    p[2] = rgba >>  8 & 0xff;
+    p[3] = rgba       & 0xff;
+}
+
+static inline uint8_t *get_pixel_pos_buf(const struct canvas *canvas, int px, int py)
+{
+    return canvas->buf + (py * canvas->w + px) * BYTE_PER_PIXEL;
+}
+
+void ngli_drawutils_draw_rect(struct canvas *canvas, const struct rect *rect, uint32_t color)
+{
+    uint8_t *buf = get_pixel_pos_buf(canvas, rect->x, rect->y);
+    for (int y = 0; y < rect->h; y++) {
+        for (int x = 0; x < rect->w; x++)
+            set_color(buf + x * BYTE_PER_PIXEL, color);
+        buf += canvas->w * BYTE_PER_PIXEL;
+    }
+}
+
+void ngli_drawutils_print(struct canvas *canvas, int x, int y, const char *str, uint32_t color)
+{
+    int px = 0, py = 0;
+    for (int i = 0; str[i]; i++) {
+        if (str[i] == '\n') {
+            py++;
+            px = 0;
+            continue;
+        }
+        for (int char_y = 0; char_y < NGLI_FONT_H; char_y++) {
+            for (int char_x = 0; char_x < NGLI_FONT_W; char_x++) {
+                const int pix_x = x + px * NGLI_FONT_W + char_x;
+                const int pix_y = y + py * NGLI_FONT_H + char_y;
+                if (pix_x < 0 || pix_y < 0 || pix_x >= canvas->w || pix_y >= canvas->h)
+                    continue;
+                uint8_t *p = get_pixel_pos_buf(canvas, pix_x, pix_y);
+                if (font8[str[i] & 0x7f][char_y] & (1 << char_x))
+                    set_color(p, color);
+            }
+        }
+        px++;
+    }
+}

NopeForge/nope.gl

libnodegl/drawutils.c
@@ -1,0 +1,203 @@
+/*
+ * Copyright 2019 GoPro Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include "drawutils.h"
+
+#define BYTES_PER_PIXEL 4 /* RGBA */
+
+static const uint8_t font8[128][8] = {
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x18, 0x3C, 0x3C, 0x18, 0x18, 0x00, 0x18, 0x00},
+    {0x36, 0x36, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x36, 0x36, 0x7F, 0x36, 0x7F, 0x36, 0x36, 0x00},
+    {0x0C, 0x3E, 0x03, 0x1E, 0x30, 0x1F, 0x0C, 0x00},
+    {0x00, 0x63, 0x33, 0x18, 0x0C, 0x66, 0x63, 0x00},
+    {0x1C, 0x36, 0x1C, 0x6E, 0x3B, 0x33, 0x6E, 0x00},
+    {0x06, 0x06, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x18, 0x0C, 0x06, 0x06, 0x06, 0x0C, 0x18, 0x00},
+    {0x06, 0x0C, 0x18, 0x18, 0x18, 0x0C, 0x06, 0x00},
+    {0x00, 0x66, 0x3C, 0xFF, 0x3C, 0x66, 0x00, 0x00},
+    {0x00, 0x0C, 0x0C, 0x3F, 0x0C, 0x0C, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x0C, 0x0C, 0x06},
+    {0x00, 0x00, 0x00, 0x3F, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x0C, 0x0C, 0x00},
+    {0x60, 0x30, 0x18, 0x0C, 0x06, 0x03, 0x01, 0x00},
+    {0x3E, 0x63, 0x73, 0x7B, 0x6F, 0x67, 0x3E, 0x00},
+    {0x0C, 0x0E, 0x0C, 0x0C, 0x0C, 0x0C, 0x3F, 0x00},
+    {0x1E, 0x33, 0x30, 0x1C, 0x06, 0x33, 0x3F, 0x00},
+    {0x1E, 0x33, 0x30, 0x1C, 0x30, 0x33, 0x1E, 0x00},
+    {0x38, 0x3C, 0x36, 0x33, 0x7F, 0x30, 0x78, 0x00},
+    {0x3F, 0x03, 0x1F, 0x30, 0x30, 0x33, 0x1E, 0x00},
+    {0x1C, 0x06, 0x03, 0x1F, 0x33, 0x33, 0x1E, 0x00},
+    {0x3F, 0x33, 0x30, 0x18, 0x0C, 0x0C, 0x0C, 0x00},
+    {0x1E, 0x33, 0x33, 0x1E, 0x33, 0x33, 0x1E, 0x00},
+    {0x1E, 0x33, 0x33, 0x3E, 0x30, 0x18, 0x0E, 0x00},
+    {0x00, 0x0C, 0x0C, 0x00, 0x00, 0x0C, 0x0C, 0x00},
+    {0x00, 0x0C, 0x0C, 0x00, 0x00, 0x0C, 0x0C, 0x06},
+    {0x18, 0x0C, 0x06, 0x03, 0x06, 0x0C, 0x18, 0x00},
+    {0x00, 0x00, 0x3F, 0x00, 0x00, 0x3F, 0x00, 0x00},
+    {0x06, 0x0C, 0x18, 0x30, 0x18, 0x0C, 0x06, 0x00},
+    {0x1E, 0x33, 0x30, 0x18, 0x0C, 0x00, 0x0C, 0x00},
+    {0x3E, 0x63, 0x7B, 0x7B, 0x7B, 0x03, 0x1E, 0x00},
+    {0x0C, 0x1E, 0x33, 0x33, 0x3F, 0x33, 0x33, 0x00},
+    {0x3F, 0x66, 0x66, 0x3E, 0x66, 0x66, 0x3F, 0x00},
+    {0x3C, 0x66, 0x03, 0x03, 0x03, 0x66, 0x3C, 0x00},
+    {0x1F, 0x36, 0x66, 0x66, 0x66, 0x36, 0x1F, 0x00},
+    {0x7F, 0x46, 0x16, 0x1E, 0x16, 0x46, 0x7F, 0x00},
+    {0x7F, 0x46, 0x16, 0x1E, 0x16, 0x06, 0x0F, 0x00},
+    {0x3C, 0x66, 0x03, 0x03, 0x73, 0x66, 0x7C, 0x00},
+    {0x33, 0x33, 0x33, 0x3F, 0x33, 0x33, 0x33, 0x00},
+    {0x1E, 0x0C, 0x0C, 0x0C, 0x0C, 0x0C, 0x1E, 0x00},
+    {0x78, 0x30, 0x30, 0x30, 0x33, 0x33, 0x1E, 0x00},
+    {0x67, 0x66, 0x36, 0x1E, 0x36, 0x66, 0x67, 0x00},
+    {0x0F, 0x06, 0x06, 0x06, 0x46, 0x66, 0x7F, 0x00},
+    {0x63, 0x77, 0x7F, 0x7F, 0x6B, 0x63, 0x63, 0x00},
+    {0x63, 0x67, 0x6F, 0x7B, 0x73, 0x63, 0x63, 0x00},
+    {0x1C, 0x36, 0x63, 0x63, 0x63, 0x36, 0x1C, 0x00},
+    {0x3F, 0x66, 0x66, 0x3E, 0x06, 0x06, 0x0F, 0x00},
+    {0x1E, 0x33, 0x33, 0x33, 0x3B, 0x1E, 0x38, 0x00},
+    {0x3F, 0x66, 0x66, 0x3E, 0x36, 0x66, 0x67, 0x00},
+    {0x1E, 0x33, 0x07, 0x0E, 0x38, 0x33, 0x1E, 0x00},
+    {0x3F, 0x2D, 0x0C, 0x0C, 0x0C, 0x0C, 0x1E, 0x00},
+    {0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x3F, 0x00},
+    {0x33, 0x33, 0x33, 0x33, 0x33, 0x1E, 0x0C, 0x00},
+    {0x63, 0x63, 0x63, 0x6B, 0x7F, 0x77, 0x63, 0x00},
+    {0x63, 0x63, 0x36, 0x1C, 0x1C, 0x36, 0x63, 0x00},
+    {0x33, 0x33, 0x33, 0x1E, 0x0C, 0x0C, 0x1E, 0x00},
+    {0x7F, 0x63, 0x31, 0x18, 0x4C, 0x66, 0x7F, 0x00},
+    {0x1E, 0x06, 0x06, 0x06, 0x06, 0x06, 0x1E, 0x00},
+    {0x03, 0x06, 0x0C, 0x18, 0x30, 0x60, 0x40, 0x00},
+    {0x1E, 0x18, 0x18, 0x18, 0x18, 0x18, 0x1E, 0x00},
+    {0x08, 0x1C, 0x36, 0x63, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF},
+    {0x0C, 0x0C, 0x18, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x1E, 0x30, 0x3E, 0x33, 0x6E, 0x00},
+    {0x07, 0x06, 0x06, 0x3E, 0x66, 0x66, 0x3B, 0x00},
+    {0x00, 0x00, 0x1E, 0x33, 0x03, 0x33, 0x1E, 0x00},
+    {0x38, 0x30, 0x30, 0x3e, 0x33, 0x33, 0x6E, 0x00},
+    {0x00, 0x00, 0x1E, 0x33, 0x3f, 0x03, 0x1E, 0x00},
+    {0x1C, 0x36, 0x06, 0x0f, 0x06, 0x06, 0x0F, 0x00},
+    {0x00, 0x00, 0x6E, 0x33, 0x33, 0x3E, 0x30, 0x1F},
+    {0x07, 0x06, 0x36, 0x6E, 0x66, 0x66, 0x67, 0x00},
+    {0x0C, 0x00, 0x0E, 0x0C, 0x0C, 0x0C, 0x1E, 0x00},
+    {0x30, 0x00, 0x30, 0x30, 0x30, 0x33, 0x33, 0x1E},
+    {0x07, 0x06, 0x66, 0x36, 0x1E, 0x36, 0x67, 0x00},
+    {0x0E, 0x0C, 0x0C, 0x0C, 0x0C, 0x0C, 0x1E, 0x00},
+    {0x00, 0x00, 0x33, 0x7F, 0x7F, 0x6B, 0x63, 0x00},
+    {0x00, 0x00, 0x1F, 0x33, 0x33, 0x33, 0x33, 0x00},
+    {0x00, 0x00, 0x1E, 0x33, 0x33, 0x33, 0x1E, 0x00},
+    {0x00, 0x00, 0x3B, 0x66, 0x66, 0x3E, 0x06, 0x0F},
+    {0x00, 0x00, 0x6E, 0x33, 0x33, 0x3E, 0x30, 0x78},
+    {0x00, 0x00, 0x3B, 0x6E, 0x66, 0x06, 0x0F, 0x00},
+    {0x00, 0x00, 0x3E, 0x03, 0x1E, 0x30, 0x1F, 0x00},
+    {0x08, 0x0C, 0x3E, 0x0C, 0x0C, 0x2C, 0x18, 0x00},
+    {0x00, 0x00, 0x33, 0x33, 0x33, 0x33, 0x6E, 0x00},
+    {0x00, 0x00, 0x33, 0x33, 0x33, 0x1E, 0x0C, 0x00},
+    {0x00, 0x00, 0x63, 0x6B, 0x7F, 0x7F, 0x36, 0x00},
+    {0x00, 0x00, 0x63, 0x36, 0x1C, 0x36, 0x63, 0x00},
+    {0x00, 0x00, 0x33, 0x33, 0x33, 0x3E, 0x30, 0x1F},
+    {0x00, 0x00, 0x3F, 0x19, 0x0C, 0x26, 0x3F, 0x00},
+    {0x38, 0x0C, 0x0C, 0x07, 0x0C, 0x0C, 0x38, 0x00},
+    {0x18, 0x18, 0x18, 0x00, 0x18, 0x18, 0x18, 0x00},
+    {0x07, 0x0C, 0x0C, 0x38, 0x0C, 0x0C, 0x07, 0x00},
+    {0x6E, 0x3B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+};
+
+static inline void set_color(uint8_t *p, uint32_t rgba)
+{
+    p[0] = rgba >> 24;
+    p[1] = rgba >> 16 & 0xff;
+    p[2] = rgba >>  8 & 0xff;
+    p[3] = rgba       & 0xff;
+}
+
+static inline uint8_t *get_pixel_pos_buf(const struct canvas *canvas, int px, int py)
+{
+    return canvas->buf + (py * canvas->w + px) * BYTES_PER_PIXEL;
+}
+
+void ngli_drawutils_draw_rect(struct canvas *canvas, const struct rect *rect, uint32_t color)
+{
+    uint8_t *buf = get_pixel_pos_buf(canvas, rect->x, rect->y);
+    const int stride = canvas->w * BYTES_PER_PIXEL;
+    for (int y = 0; y < rect->h; y++) {
+        for (int x = 0; x < rect->w; x++)
+            set_color(buf + x * BYTES_PER_PIXEL, color);
+        buf += stride;
+    }
+}
+
+void ngli_drawutils_print(struct canvas *canvas, int x, int y, const char *str, uint32_t color)
+{
+    int px = 0, py = 0;
+    for (int i = 0; str[i]; i++) {
+        if (str[i] == '\n') {
+            py++;
+            px = 0;
+            continue;
+        }
+        for (int char_y = 0; char_y < NGLI_FONT_H; char_y++) {
+            for (int char_x = 0; char_x < NGLI_FONT_W; char_x++) {
+                const int pix_x = x + px * NGLI_FONT_W + char_x;
+                const int pix_y = y + py * NGLI_FONT_H + char_y;
+                if (pix_x < 0 || pix_y < 0 || pix_x >= canvas->w || pix_y >= canvas->h)
+                    continue;
+                uint8_t *p = get_pixel_pos_buf(canvas, pix_x, pix_y);
+                if (font8[str[i] & 0x7f][char_y] & (1 << char_x))
+                    set_color(p, color);
+            }
+        }
+        px++;
+    }
+}