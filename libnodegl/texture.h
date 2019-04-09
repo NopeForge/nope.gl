NopeForge/nope.gl

commit 8d30481eaebe0d82fd2ab4f1796d69c50eae06de

texture: use internal constants for min and mag filters

libnodegl/texture.h
@@ -38,13 +38,14 @@ enum {
     NGLI_NB_FILTER
 };
 
-GLint ngli_texture_get_gl_min_filter(GLint gl_min_filter, int mipmap_filter);
+int ngli_texture_get_gl_min_filter(int min_filter, int mipmap_filter);
+int ngli_texture_get_gl_mag_filter(int mag_filter);
 
 #define NGLI_TEXTURE_PARAM_DEFAULTS {          \
     .dimensions = 2,                           \
     .format = NGLI_FORMAT_UNDEFINED,           \
-    .min_filter = GL_NEAREST,                  \
-    .mag_filter = GL_NEAREST,                  \
+    .min_filter = NGLI_FILTER_NEAREST,         \
+    .mag_filter = NGLI_FILTER_NEAREST,         \
     .mipmap_filter = NGLI_MIPMAP_FILTER_NONE,  \
     .wrap_s = GL_CLAMP_TO_EDGE,                \
     .wrap_t = GL_CLAMP_TO_EDGE,                \

libnodegl/hwupload_mediacodec.c
@@ -134,10 +134,12 @@ static int mc_dr_init(struct ngl_node *node, struct sxplayer_frame *frame)
 
     GLint id = media->android_texture.id;
     GLenum target = media->android_texture.target;
+    const GLint min_filter = ngli_texture_get_gl_min_filter(params->min_filter, params->mipmap_filter);
+    const GLint mag_filter = ngli_texture_get_gl_mag_filter(params->mag_filter);
 
     ngli_glBindTexture(gl, target, id);
-    ngli_glTexParameteri(gl, target, GL_TEXTURE_MIN_FILTER, params->min_filter);
-    ngli_glTexParameteri(gl, target, GL_TEXTURE_MAG_FILTER, params->mag_filter);
+    ngli_glTexParameteri(gl, target, GL_TEXTURE_MIN_FILTER, min_filter);
+    ngli_glTexParameteri(gl, target, GL_TEXTURE_MAG_FILTER, mag_filter);
     ngli_glBindTexture(gl, target, 0);
 
     ngli_image_init(&s->image, NGLI_IMAGE_LAYOUT_MEDIACODEC, &media->android_texture);

libnodegl/hwupload_videotoolbox_ios.c
@@ -120,9 +120,12 @@ static int vt_ios_common_map_plane(struct ngl_node *node, CVPixelBufferRef cvpix
     }
 
     GLint id = CVOpenGLESTextureGetName(vt->ios_textures[index]);
+    const GLint min_filter = ngli_texture_get_gl_min_filter(plane_params->min_filter, plane_params->mipmap_filter);
+    const GLint mag_filter = ngli_texture_get_gl_mag_filter(plane_params->mag_filter);
+
     ngli_glBindTexture(gl, GL_TEXTURE_2D, id);
-    ngli_glTexParameteri(gl, GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, plane_params->min_filter);
-    ngli_glTexParameteri(gl, GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, plane_params->mag_filter);
+    ngli_glTexParameteri(gl, GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, min_filter);
+    ngli_glTexParameteri(gl, GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, mag_filter);
     ngli_glTexParameteri(gl, GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, plane_params->wrap_s);
     ngli_glTexParameteri(gl, GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, plane_params->wrap_t);
     ngli_glBindTexture(gl, GL_TEXTURE_2D, 0);

libnodegl/node_media.c
@@ -166,8 +166,8 @@ static int media_init(struct ngl_node *node)
     struct texture_params params = {
         .dimensions = 2,
         .format = NGLI_FORMAT_UNDEFINED,
-        .min_filter = GL_NEAREST,
-        .mag_filter = GL_NEAREST,
+        .min_filter = NGLI_FILTER_NEAREST,
+        .mag_filter = NGLI_FILTER_NEAREST,
         .wrap_s = GL_CLAMP_TO_EDGE,
         .wrap_t = GL_CLAMP_TO_EDGE,
         .wrap_r = GL_CLAMP_TO_EDGE,

libnodegl/node_text.c
@@ -112,10 +112,10 @@ static const struct node_param text_params[] = {
                      .desc=NGLI_DOCSTRING("horizontal alignment of the text in the box")},
     {"aspect_ratio", PARAM_TYPE_RATIONAL, OFFSET(aspect_ratio),
                      .desc=NGLI_DOCSTRING("box aspect ratio")},
-    {"min_filter",   PARAM_TYPE_SELECT, OFFSET(min_filter), {.i64=GL_LINEAR},
+    {"min_filter",   PARAM_TYPE_SELECT, OFFSET(min_filter), {.i64=NGLI_FILTER_LINEAR},
                      .choices=&ngli_filter_choices,
                      .desc=NGLI_DOCSTRING("rasterized text texture minifying function")},
-    {"mag_filter",   PARAM_TYPE_SELECT, OFFSET(mag_filter), {.i64=GL_NEAREST},
+    {"mag_filter",   PARAM_TYPE_SELECT, OFFSET(mag_filter), {.i64=NGLI_FILTER_NEAREST},
                      .choices=&ngli_filter_choices,
                      .desc=NGLI_DOCSTRING("rasterized text texture magnification function")},
     {"mipmap_filter", PARAM_TYPE_SELECT, OFFSET(mipmap_filter), {.i64=NGLI_MIPMAP_FILTER_LINEAR},

libnodegl/node_texture.c
@@ -46,8 +46,8 @@ const struct param_choices ngli_mipmap_filter_choices = {
 const struct param_choices ngli_filter_choices = {
     .name = "filter",
     .consts = {
-        {"nearest", GL_NEAREST, .desc=NGLI_DOCSTRING("nearest filtering")},
-        {"linear",  GL_LINEAR,  .desc=NGLI_DOCSTRING("linear filtering")},
+        {"nearest", NGLI_FILTER_NEAREST, .desc=NGLI_DOCSTRING("nearest filtering")},
+        {"linear",  NGLI_FILTER_LINEAR,  .desc=NGLI_DOCSTRING("linear filtering")},
         {NULL}
     }
 };
@@ -135,9 +135,9 @@ static const struct node_param texture2d_params[] = {
               .desc=NGLI_DOCSTRING("width of the texture")},
     {"height", PARAM_TYPE_INT, OFFSET(params.height), {.i64=0},
                .desc=NGLI_DOCSTRING("height of the texture")},
-    {"min_filter", PARAM_TYPE_SELECT, OFFSET(params.min_filter), {.i64=GL_NEAREST}, .choices=&ngli_filter_choices,
+    {"min_filter", PARAM_TYPE_SELECT, OFFSET(params.min_filter), {.i64=NGLI_FILTER_NEAREST}, .choices=&ngli_filter_choices,
                    .desc=NGLI_DOCSTRING("texture minifying function")},
-    {"mag_filter", PARAM_TYPE_SELECT, OFFSET(params.mag_filter), {.i64=GL_NEAREST}, .choices=&ngli_filter_choices,
+    {"mag_filter", PARAM_TYPE_SELECT, OFFSET(params.mag_filter), {.i64=NGLI_FILTER_NEAREST}, .choices=&ngli_filter_choices,
                    .desc=NGLI_DOCSTRING("texture magnification function")},
     {"mipmap_filter", PARAM_TYPE_SELECT, OFFSET(params.mipmap_filter), {.i64=NGLI_MIPMAP_FILTER_NONE},
                       .choices=&ngli_mipmap_filter_choices,
@@ -164,9 +164,9 @@ static const struct node_param texture3d_params[] = {
                .desc=NGLI_DOCSTRING("height of the texture")},
     {"depth", PARAM_TYPE_INT, OFFSET(params.depth), {.i64=0},
                .desc=NGLI_DOCSTRING("depth of the texture")},
-    {"min_filter", PARAM_TYPE_SELECT, OFFSET(params.min_filter), {.i64=GL_NEAREST}, .choices=&ngli_filter_choices,
+    {"min_filter", PARAM_TYPE_SELECT, OFFSET(params.min_filter), {.i64=NGLI_FILTER_NEAREST}, .choices=&ngli_filter_choices,
                    .desc=NGLI_DOCSTRING("texture minifying function")},
-    {"mag_filter", PARAM_TYPE_SELECT, OFFSET(params.mag_filter), {.i64=GL_NEAREST}, .choices=&ngli_filter_choices,
+    {"mag_filter", PARAM_TYPE_SELECT, OFFSET(params.mag_filter), {.i64=NGLI_FILTER_NEAREST}, .choices=&ngli_filter_choices,
                    .desc=NGLI_DOCSTRING("texture magnification function")},
     {"mipmap_filter", PARAM_TYPE_SELECT, OFFSET(params.mipmap_filter), {.i64=NGLI_MIPMAP_FILTER_NONE},
                       .choices=&ngli_mipmap_filter_choices,
@@ -189,9 +189,9 @@ static const struct node_param texturecube_params[] = {
                .desc=NGLI_DOCSTRING("format of the pixel data")},
     {"size", PARAM_TYPE_INT, OFFSET(params.width), {.i64=0},
              .desc=NGLI_DOCSTRING("width and height of the texture")},
-    {"min_filter", PARAM_TYPE_SELECT, OFFSET(params.min_filter), {.i64=GL_NEAREST}, .choices=&ngli_filter_choices,
+    {"min_filter", PARAM_TYPE_SELECT, OFFSET(params.min_filter), {.i64=NGLI_FILTER_NEAREST}, .choices=&ngli_filter_choices,
                    .desc=NGLI_DOCSTRING("texture minifying function")},
-    {"mag_filter", PARAM_TYPE_SELECT, OFFSET(params.mag_filter), {.i64=GL_NEAREST}, .choices=&ngli_filter_choices,
+    {"mag_filter", PARAM_TYPE_SELECT, OFFSET(params.mag_filter), {.i64=NGLI_FILTER_NEAREST}, .choices=&ngli_filter_choices,
                    .desc=NGLI_DOCSTRING("texture magnification function")},
     {"mipmap_filter", PARAM_TYPE_SELECT, OFFSET(params.mipmap_filter), {.i64=NGLI_MIPMAP_FILTER_NONE},
                       .choices=&ngli_mipmap_filter_choices,

libnodegl/texture.c
@@ -41,19 +41,14 @@ static const GLint gl_filter_map[NGLI_NB_FILTER][NGLI_NB_MIPMAP] = {
     },
 };
 
-static int min_filter_gl_to_ngl(GLint gl_min_filter)
+GLint ngli_texture_get_gl_min_filter(int min_filter, int mipmap_filter)
 {
-    switch (gl_min_filter) {
-        case GL_NEAREST: return NGLI_FILTER_NEAREST;
-        case GL_LINEAR:  return NGLI_FILTER_LINEAR;
-    }
-    return 0;
+    return gl_filter_map[min_filter][mipmap_filter];
 }
 
-GLint ngli_texture_get_gl_min_filter(GLint gl_min_filter, int mipmap_filter)
+GLint ngli_texture_get_gl_mag_filter(int mag_filter)
 {
-    const int min_filter = min_filter_gl_to_ngl(gl_min_filter);
-    return gl_filter_map[min_filter][mipmap_filter];
+    return gl_filter_map[mag_filter][NGLI_MIPMAP_FILTER_NONE];
 }
 
 static void texture_set_image(struct texture *s, const uint8_t *data)
@@ -304,8 +299,9 @@ int ngli_texture_init(struct texture *s,
             mipmap_filter = NGLI_MIPMAP_FILTER_NONE;
         }
         const GLint min_filter = ngli_texture_get_gl_min_filter(params->min_filter, mipmap_filter);
+        const GLint mag_filter = ngli_texture_get_gl_mag_filter(params->mag_filter);
         ngli_glTexParameteri(gl, s->target, GL_TEXTURE_MIN_FILTER, min_filter);
-        ngli_glTexParameteri(gl, s->target, GL_TEXTURE_MAG_FILTER, params->mag_filter);
+        ngli_glTexParameteri(gl, s->target, GL_TEXTURE_MAG_FILTER, mag_filter);
         ngli_glTexParameteri(gl, s->target, GL_TEXTURE_WRAP_S, params->wrap_s);
         ngli_glTexParameteri(gl, s->target, GL_TEXTURE_WRAP_T, params->wrap_t);
         if (s->target == GL_TEXTURE_3D || s->target == GL_TEXTURE_CUBE_MAP)

commit a5e7eae7b7d4fc864901b86ce0407bc5295c2fc4

texture: fix min/mag filter types
Fixes issue introduced in 8d30481eaebe0d82fd2ab4f1796d69c50eae06de.

libnodegl/texture.h
@@ -38,8 +38,8 @@ enum {
     NGLI_NB_FILTER
 };
 
-int ngli_texture_get_gl_min_filter(int min_filter, int mipmap_filter);
-int ngli_texture_get_gl_mag_filter(int mag_filter);
+GLint ngli_texture_get_gl_min_filter(int min_filter, int mipmap_filter);
+GLint ngli_texture_get_gl_mag_filter(int mag_filter);
 
 #define NGLI_TEXTURE_PARAM_DEFAULTS {          \
     .dimensions = 2,                           \
@@ -62,8 +62,8 @@ struct texture_params {
     int height;
     int depth;
     int samples;
-    GLint min_filter;
-    GLint mag_filter;
+    int min_filter;
+    int mag_filter;
     int mipmap_filter;
     GLint wrap_s;
     GLint wrap_t;