NopeForge/nope.gl

commit 8bba11be9449ff9775af2392b1ee802f70a321f0

texture: use internal constants for wrap values

libnodegl/texture.c
@@ -51,6 +51,17 @@ GLint ngli_texture_get_gl_mag_filter(int mag_filter)
     return gl_filter_map[mag_filter][NGLI_MIPMAP_FILTER_NONE];
 }
 
+static const GLint gl_wrap_map[NGLI_NB_WRAP] = {
+    [NGLI_WRAP_CLAMP_TO_EDGE]   = GL_CLAMP_TO_EDGE,
+    [NGLI_WRAP_MIRRORED_REPEAT] = GL_MIRRORED_REPEAT,
+    [NGLI_WRAP_REPEAT]          = GL_REPEAT,
+};
+
+static GLint get_gl_wrap(int wrap)
+{
+    return gl_wrap_map[wrap];
+}
+
 static void texture_set_image(struct texture *s, const uint8_t *data)
 {
     struct glcontext *gl = s->gl;
@@ -302,10 +313,10 @@ int ngli_texture_init(struct texture *s,
         const GLint mag_filter = ngli_texture_get_gl_mag_filter(params->mag_filter);
         ngli_glTexParameteri(gl, s->target, GL_TEXTURE_MIN_FILTER, min_filter);
         ngli_glTexParameteri(gl, s->target, GL_TEXTURE_MAG_FILTER, mag_filter);
-        ngli_glTexParameteri(gl, s->target, GL_TEXTURE_WRAP_S, params->wrap_s);
-        ngli_glTexParameteri(gl, s->target, GL_TEXTURE_WRAP_T, params->wrap_t);
+        ngli_glTexParameteri(gl, s->target, GL_TEXTURE_WRAP_S, get_gl_wrap(params->wrap_s));
+        ngli_glTexParameteri(gl, s->target, GL_TEXTURE_WRAP_T, get_gl_wrap(params->wrap_t));
         if (s->target == GL_TEXTURE_3D || s->target == GL_TEXTURE_CUBE_MAP)
-            ngli_glTexParameteri(gl, s->target, GL_TEXTURE_WRAP_R, params->wrap_r);
+            ngli_glTexParameteri(gl, s->target, GL_TEXTURE_WRAP_R, get_gl_wrap(params->wrap_r));
 
         if (!s->external_storage) {
             if (!params->width || !params->height ||

libnodegl/texture.h
@@ -41,15 +41,22 @@ enum {
 GLint ngli_texture_get_gl_min_filter(int min_filter, int mipmap_filter);
 GLint ngli_texture_get_gl_mag_filter(int mag_filter);
 
+enum {
+    NGLI_WRAP_CLAMP_TO_EDGE,
+    NGLI_WRAP_MIRRORED_REPEAT,
+    NGLI_WRAP_REPEAT,
+    NGLI_NB_WRAP
+};
+
 #define NGLI_TEXTURE_PARAM_DEFAULTS {          \
     .dimensions = 2,                           \
     .format = NGLI_FORMAT_UNDEFINED,           \
     .min_filter = NGLI_FILTER_NEAREST,         \
     .mag_filter = NGLI_FILTER_NEAREST,         \
     .mipmap_filter = NGLI_MIPMAP_FILTER_NONE,  \
-    .wrap_s = GL_CLAMP_TO_EDGE,                \
-    .wrap_t = GL_CLAMP_TO_EDGE,                \
-    .wrap_r = GL_CLAMP_TO_EDGE,                \
+    .wrap_s = NGLI_WRAP_CLAMP_TO_EDGE,         \
+    .wrap_t = NGLI_WRAP_CLAMP_TO_EDGE,         \
+    .wrap_r = NGLI_WRAP_CLAMP_TO_EDGE,         \
     .access = GL_READ_WRITE                    \
 }
 
@@ -65,9 +72,9 @@ struct texture_params {
     int min_filter;
     int mag_filter;
     int mipmap_filter;
-    GLint wrap_s;
-    GLint wrap_t;
-    GLint wrap_r;
+    int wrap_s;
+    int wrap_t;
+    int wrap_r;
     GLenum access;
     int immutable;
     int usage;

libnodegl/hwupload_mediacodec.c
@@ -184,7 +184,7 @@ static const struct hwmap_class *mc_get_hwmap(struct ngl_node *node, struct sxpl
             LOG(WARNING, "external textures do not support mipmapping: "
                 "disabling direct rendering");
             s->direct_rendering = 0;
-        } else if (params->wrap_s != GL_CLAMP_TO_EDGE || params->wrap_t != GL_CLAMP_TO_EDGE) {
+        } else if (params->wrap_s != NGLI_WRAP_CLAMP_TO_EDGE || params->wrap_t != NGLI_WRAP_CLAMP_TO_EDGE) {
             LOG(WARNING, "external textures only support clamp to edge wrapping: "
                 "disabling direct rendering");
             s->direct_rendering = 0;

libnodegl/node_media.c
@@ -168,9 +168,9 @@ static int media_init(struct ngl_node *node)
         .format = NGLI_FORMAT_UNDEFINED,
         .min_filter = NGLI_FILTER_NEAREST,
         .mag_filter = NGLI_FILTER_NEAREST,
-        .wrap_s = GL_CLAMP_TO_EDGE,
-        .wrap_t = GL_CLAMP_TO_EDGE,
-        .wrap_r = GL_CLAMP_TO_EDGE,
+        .wrap_s = NGLI_WRAP_CLAMP_TO_EDGE,
+        .wrap_t = NGLI_WRAP_CLAMP_TO_EDGE,
+        .wrap_r = NGLI_WRAP_CLAMP_TO_EDGE,
         .access = GL_READ_WRITE,
         .external_oes = 1,
     };

libnodegl/node_texture.c
@@ -55,9 +55,9 @@ const struct param_choices ngli_filter_choices = {
 static const struct param_choices wrap_choices = {
     .name = "wrap",
     .consts = {
-        {"clamp_to_edge",   GL_CLAMP_TO_EDGE,   .desc=NGLI_DOCSTRING("clamp to edge wrapping")},
-        {"mirrored_repeat", GL_MIRRORED_REPEAT, .desc=NGLI_DOCSTRING("mirrored repeat wrapping")},
-        {"repeat",          GL_REPEAT,          .desc=NGLI_DOCSTRING("repeat pattern wrapping")},
+        {"clamp_to_edge",   NGLI_WRAP_CLAMP_TO_EDGE,   .desc=NGLI_DOCSTRING("clamp to edge wrapping")},
+        {"mirrored_repeat", NGLI_WRAP_MIRRORED_REPEAT, .desc=NGLI_DOCSTRING("mirrored repeat wrapping")},
+        {"repeat",          NGLI_WRAP_REPEAT,          .desc=NGLI_DOCSTRING("repeat pattern wrapping")},
         {NULL}
     }
 };
@@ -142,9 +142,9 @@ static const struct node_param texture2d_params[] = {
     {"mipmap_filter", PARAM_TYPE_SELECT, OFFSET(params.mipmap_filter), {.i64=NGLI_MIPMAP_FILTER_NONE},
                       .choices=&ngli_mipmap_filter_choices,
                       .desc=NGLI_DOCSTRING("texture minifying mipmap function")},
-    {"wrap_s", PARAM_TYPE_SELECT, OFFSET(params.wrap_s), {.i64=GL_CLAMP_TO_EDGE}, .choices=&wrap_choices,
+    {"wrap_s", PARAM_TYPE_SELECT, OFFSET(params.wrap_s), {.i64=NGLI_WRAP_CLAMP_TO_EDGE}, .choices=&wrap_choices,
                .desc=NGLI_DOCSTRING("wrap parameter for the texture on the s dimension (horizontal)")},
-    {"wrap_t", PARAM_TYPE_SELECT, OFFSET(params.wrap_t), {.i64=GL_CLAMP_TO_EDGE}, .choices=&wrap_choices,
+    {"wrap_t", PARAM_TYPE_SELECT, OFFSET(params.wrap_t), {.i64=NGLI_WRAP_CLAMP_TO_EDGE}, .choices=&wrap_choices,
                .desc=NGLI_DOCSTRING("wrap parameter for the texture on the t dimension (vertical)")},
     {"access", PARAM_TYPE_SELECT, OFFSET(params.access), {.i64=GL_READ_WRITE}, .choices=&access_choices,
                .desc=NGLI_DOCSTRING("texture access (only honored by the `Compute` node)")},
@@ -171,11 +171,11 @@ static const struct node_param texture3d_params[] = {
     {"mipmap_filter", PARAM_TYPE_SELECT, OFFSET(params.mipmap_filter), {.i64=NGLI_MIPMAP_FILTER_NONE},
                       .choices=&ngli_mipmap_filter_choices,
                       .desc=NGLI_DOCSTRING("texture minifying mipmap function")},
-    {"wrap_s", PARAM_TYPE_SELECT, OFFSET(params.wrap_s), {.i64=GL_CLAMP_TO_EDGE}, .choices=&wrap_choices,
+    {"wrap_s", PARAM_TYPE_SELECT, OFFSET(params.wrap_s), {.i64=NGLI_WRAP_CLAMP_TO_EDGE}, .choices=&wrap_choices,
                .desc=NGLI_DOCSTRING("wrap parameter for the texture on the s dimension (horizontal)")},
-    {"wrap_t", PARAM_TYPE_SELECT, OFFSET(params.wrap_t), {.i64=GL_CLAMP_TO_EDGE}, .choices=&wrap_choices,
+    {"wrap_t", PARAM_TYPE_SELECT, OFFSET(params.wrap_t), {.i64=NGLI_WRAP_CLAMP_TO_EDGE}, .choices=&wrap_choices,
                .desc=NGLI_DOCSTRING("wrap parameter for the texture on the t dimension (vertical)")},
-    {"wrap_r", PARAM_TYPE_SELECT, OFFSET(params.wrap_r), {.i64=GL_CLAMP_TO_EDGE}, .choices=&wrap_choices,
+    {"wrap_r", PARAM_TYPE_SELECT, OFFSET(params.wrap_r), {.i64=NGLI_WRAP_CLAMP_TO_EDGE}, .choices=&wrap_choices,
                .desc=NGLI_DOCSTRING("wrap parameter for the texture on the r dimension (depth)")},
     {"access", PARAM_TYPE_SELECT, OFFSET(params.access), {.i64=GL_READ_WRITE}, .choices=&access_choices,
                .desc=NGLI_DOCSTRING("texture access (only honored by the `Compute` node)")},
@@ -196,11 +196,11 @@ static const struct node_param texturecube_params[] = {
     {"mipmap_filter", PARAM_TYPE_SELECT, OFFSET(params.mipmap_filter), {.i64=NGLI_MIPMAP_FILTER_NONE},
                       .choices=&ngli_mipmap_filter_choices,
                       .desc=NGLI_DOCSTRING("texture minifying mipmap function")},
-    {"wrap_s", PARAM_TYPE_SELECT, OFFSET(params.wrap_s), {.i64=GL_CLAMP_TO_EDGE}, .choices=&wrap_choices,
+    {"wrap_s", PARAM_TYPE_SELECT, OFFSET(params.wrap_s), {.i64=NGLI_WRAP_CLAMP_TO_EDGE}, .choices=&wrap_choices,
                .desc=NGLI_DOCSTRING("wrap parameter for the texture on the s dimension (horizontal)")},
-    {"wrap_t", PARAM_TYPE_SELECT, OFFSET(params.wrap_t), {.i64=GL_CLAMP_TO_EDGE}, .choices=&wrap_choices,
+    {"wrap_t", PARAM_TYPE_SELECT, OFFSET(params.wrap_t), {.i64=NGLI_WRAP_CLAMP_TO_EDGE}, .choices=&wrap_choices,
                .desc=NGLI_DOCSTRING("wrap parameter for the texture on the t dimension (vertical)")},
-    {"wrap_r", PARAM_TYPE_SELECT, OFFSET(params.wrap_r), {.i64=GL_CLAMP_TO_EDGE}, .choices=&wrap_choices,
+    {"wrap_r", PARAM_TYPE_SELECT, OFFSET(params.wrap_r), {.i64=NGLI_WRAP_CLAMP_TO_EDGE}, .choices=&wrap_choices,
                .desc=NGLI_DOCSTRING("wrap parameter for the texture on the r dimension (depth)")},
     {"access", PARAM_TYPE_SELECT, OFFSET(params.access), {.i64=GL_READ_WRITE}, .choices=&access_choices,
                .desc=NGLI_DOCSTRING("texture access (only honored by the `Compute` node)")},

commit 644670bdc31e5fad3103ac0b6c6df2d2d5248617

hwupload/videotoolbox_ios: fix GL wrap constants
Fixes regression introduced in 8bba11be9449ff9775af2392b1ee802f70a321f0.

libnodegl/hwupload_videotoolbox_ios.c
@@ -122,12 +122,14 @@ static int vt_ios_common_map_plane(struct ngl_node *node, CVPixelBufferRef cvpix
     GLint id = CVOpenGLESTextureGetName(vt->ios_textures[index]);
     const GLint min_filter = ngli_texture_get_gl_min_filter(plane_params->min_filter, plane_params->mipmap_filter);
     const GLint mag_filter = ngli_texture_get_gl_mag_filter(plane_params->mag_filter);
+    const GLint wrap_s = ngli_texture_get_gl_wrap(plane_params->wrap_s);
+    const GLint wrap_t = ngli_texture_get_gl_wrap(plane_params->wrap_t);
 
     ngli_glBindTexture(gl, GL_TEXTURE_2D, id);
     ngli_glTexParameteri(gl, GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, min_filter);
     ngli_glTexParameteri(gl, GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, mag_filter);
-    ngli_glTexParameteri(gl, GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, plane_params->wrap_s);
-    ngli_glTexParameteri(gl, GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, plane_params->wrap_t);
+    ngli_glTexParameteri(gl, GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap_s);
+    ngli_glTexParameteri(gl, GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrap_t);
     ngli_glBindTexture(gl, GL_TEXTURE_2D, 0);
 
     ngli_texture_set_id(plane, id);

libnodegl/texture.c
@@ -57,7 +57,7 @@ static const GLint gl_wrap_map[NGLI_NB_WRAP] = {
     [NGLI_WRAP_REPEAT]          = GL_REPEAT,
 };
 
-static GLint get_gl_wrap(int wrap)
+GLint ngli_texture_get_gl_wrap(int wrap)
 {
     return gl_wrap_map[wrap];
 }
@@ -311,12 +311,15 @@ int ngli_texture_init(struct texture *s,
         }
         const GLint min_filter = ngli_texture_get_gl_min_filter(params->min_filter, mipmap_filter);
         const GLint mag_filter = ngli_texture_get_gl_mag_filter(params->mag_filter);
+        const GLint wrap_s = ngli_texture_get_gl_wrap(params->wrap_s);
+        const GLint wrap_t = ngli_texture_get_gl_wrap(params->wrap_t);
+        const GLint wrap_r = ngli_texture_get_gl_wrap(params->wrap_r);
         ngli_glTexParameteri(gl, s->target, GL_TEXTURE_MIN_FILTER, min_filter);
         ngli_glTexParameteri(gl, s->target, GL_TEXTURE_MAG_FILTER, mag_filter);
-        ngli_glTexParameteri(gl, s->target, GL_TEXTURE_WRAP_S, get_gl_wrap(params->wrap_s));
-        ngli_glTexParameteri(gl, s->target, GL_TEXTURE_WRAP_T, get_gl_wrap(params->wrap_t));
+        ngli_glTexParameteri(gl, s->target, GL_TEXTURE_WRAP_S, wrap_s);
+        ngli_glTexParameteri(gl, s->target, GL_TEXTURE_WRAP_T, wrap_t);
         if (s->target == GL_TEXTURE_3D || s->target == GL_TEXTURE_CUBE_MAP)
-            ngli_glTexParameteri(gl, s->target, GL_TEXTURE_WRAP_R, get_gl_wrap(params->wrap_r));
+            ngli_glTexParameteri(gl, s->target, GL_TEXTURE_WRAP_R, wrap_r);
 
         if (!s->external_storage) {
             if (!params->width || !params->height ||

libnodegl/texture.h
@@ -48,6 +48,8 @@ enum {
     NGLI_NB_WRAP
 };
 
+GLint ngli_texture_get_gl_wrap(int wrap);
+
 #define NGLI_TEXTURE_PARAM_DEFAULTS {          \
     .dimensions = 2,                           \
     .format = NGLI_FORMAT_UNDEFINED,           \