851176b6a500a2efa87b1709903ca21e70d43deb
Clément Bœsch <u@pkh.me>
2019-06-12 10:30:10 +0200

    utils/filetracking: fix file tracking of relative imports

--- pynodegl-utils/pynodegl_utils/filetracker.py
+++ pynodegl-utils/pynodegl_utils/filetracker.py
@@ -48,16 +48,51 @@ class FileTracker:
 
     def _import_hook(self, name, globals={}, locals={}, fromlist=[], level=-1):
         ret = self._builtin_import(name, globals, locals, fromlist, level)
-        if not self._mod_is_blacklisted(ret):
-            mods = [ret]
-            real_mod = sys.modules.get(name)
-            if real_mod:
-                mods.append(real_mod)
-            for mod in mods:
-                path = mod.__file__
-                if path.endswith('.pyc'):
-                    path = path[:-1]
-                self.filelist.update([op.realpath(path)])
+
+        # From the Python 2 documentation (as of 2019/06/12): "When the name
+        # variable is of the form package.module, normally, the top-level
+        # package (the name up till the first dot) is returned, not the module
+        # named by name.  However, when a non-empty fromlist argument is given,
+        # the module named by name is returned."
+        #
+        # Source: https://docs.python.org/2/library/functions.html#__import__
+        #
+        # This means that at this point, `ret` *may* be the parent module:
+        # Doing "import foo.bar.baz", you get ret=<module 'foo'>. In this
+        # situation, we can only look for our child module in sys.modules.
+        #
+        # But beware, we must look into sys.modules ONLY in certain situations:
+        # Indeed, if foo.bar.baz is imported, sometimes we only get "baz" in
+        # the name instead. This will cause a wrong lookup into sys.modules
+        # ("baz" instead of "foo.bar.baz") and may break havok.
+        #
+        # One example of such a scenario is with the relative import of
+        # "exceptions" in the attr modules. The hook only sees
+        # name="exceptions" (with ret=<module 'attr.exceptions'>), and looking
+        # up "exceptions" in sys.modules will return the built-in module
+        # instead of attr.exceptions.
+        #
+        # To identify if `ret` is the top-level package module or the child,
+        # one may innocently look for a '.' in the name. Indeed, the
+        # documentation mentions "the form package.module", but this is
+        # followed by the word "normally". This means that in practice, it's
+        # not always the case. One example is with "from .foo.bar import baz".
+        # In this situation, we get name="foo.bar", but since this is a
+        # relative import, "foo.bar" will not exist in sys.modules. Instead, we
+        # decide to rely on the `fromlist` variable (see the last sentence of
+        # the documentation quote).
+
+        mods = [ret]
+        if not fromlist:
+            mods.append(sys.modules[name])
+
+        for mod in mods:
+           if self._mod_is_blacklisted(mod):
+               continue
+           path = mod.__file__
+           if path.endswith('.pyc'):
+               path = path[:-1]
+           self.filelist.update([op.realpath(path)])
         return ret
 
     def _open_hook(self, name, mode='r', buffering=-1):
