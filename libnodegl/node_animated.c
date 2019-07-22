92dc4e1f92bbb96bcaae3204ab07868a2b274c1e
Clément Bœsch <cboesch@gopro.com>
2019-07-22 17:14:59 +0200

    animated: classify as uniform

--- libnodegl/node_animated.c
+++ libnodegl/node_animated.c
@@ -246,6 +246,7 @@ static int animatedvec_update(struct ngl_node *node, double t)
 #define DEFINE_ANIMATED_CLASS(class_id, class_name, type)       \
 const struct node_class ngli_animated##type##_class = {         \
     .id        = class_id,                                      \
+    .category  = NGLI_NODE_CATEGORY_UNIFORM,                    \
     .name      = class_name,                                    \
     .init      = animated##type##_init,                         \
     .update    = animated##type##_update,                       \
