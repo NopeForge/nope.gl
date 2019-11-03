503ad42a6f71297a7a49dedbe698d0c3fd90cb14
Clément Bœsch <u@pkh.me>
2019-11-03 23:17:06 +0100

    utils: small line break cosmetic

--- pynodegl-utils/pynodegl_utils/ui/main_window.py
+++ pynodegl-utils/pynodegl_utils/ui/main_window.py
@@ -120,7 +120,6 @@ class MainWindow(QtWidgets.QSplitter):
         self._scene_toolbar.clearColorChanged.connect(self._config.set_clear_color)
         self._scene_toolbar.backendChanged.connect(gl_view.set_backend)
         self._scene_toolbar.backendChanged.connect(self._config.set_backend)
-
         self._scene_toolbar.hudChanged.connect(self._config.set_hud)
 
         self._errbuf = QtWidgets.QPlainTextEdit()
