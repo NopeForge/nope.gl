f495f2943d644d6aafa2443661d0b52508786eb6
Clément Bœsch <u@pkh.me>
2019-11-24 14:48:25 +0100

    utils/filetracker: simplify file tracking logic
    Previously the code was trying to hook every import calls to identify
    files. This is actually legacy code from when we were trying to
    reconstruct the whole tree in order to reload modules in the appropriate
    order. This is not required anymore, so we simply make a diff of
    sys.modules before and after the monitored code.
    
    One limitation to keep in mind: it won't detect re-imports of already
    imported modules, but in our case it's not important since the usage of
    the FileTracker is made in a fresh subprocess.
    
    In addition to simplifying the code, this new logic is compatible with
    Python3.

--- pynodegl-utils/pynodegl_utils/filetracker.py
+++ pynodegl-utils/pynodegl_utils/filetracker.py
@@ -31,82 +31,35 @@ class FileTracker:
 
     def __init__(self):
         self.filelist = set()
-
-        self._builtin_import = __builtin__.__import__
         self._builtin_open = __builtin__.open
         self._pysysdir = op.realpath(distutils.sysconfig.get_python_lib(standard_lib=True))
-
-    def _mod_is_blacklisted(self, mod):
-        if not hasattr(mod, '__file__'):
-            return True
-
-        modpath = op.realpath(op.dirname(mod.__file__))
-        if modpath.startswith(self._pysysdir):
-            return True
-
-        return False
-
-    def _import_hook(self, name, globals={}, locals={}, fromlist=[], level=-1):
-        ret = self._builtin_import(name, globals, locals, fromlist, level)
-
-        # From the Python 2 documentation (as of 2019/06/12): "When the name
-        # variable is of the form package.module, normally, the top-level
-        # package (the name up till the first dot) is returned, not the module
-        # named by name.  However, when a non-empty fromlist argument is given,
-        # the module named by name is returned."
-        #
-        # Source: https://docs.python.org/2/library/functions.html#__import__
-        #
-        # This means that at this point, `ret` *may* be the parent module:
-        # Doing "import foo.bar.baz", you get ret=<module 'foo'>. In this
-        # situation, we can only look for our child module in sys.modules.
-        #
-        # But beware, we must look into sys.modules ONLY in certain situations:
-        # Indeed, if foo.bar.baz is imported, sometimes we only get "baz" in
-        # the name instead. This will cause a wrong lookup into sys.modules
-        # ("baz" instead of "foo.bar.baz") and may break havok.
-        #
-        # One example of such a scenario is with the relative import of
-        # "exceptions" in the attr modules. The hook only sees
-        # name="exceptions" (with ret=<module 'attr.exceptions'>), and looking
-        # up "exceptions" in sys.modules will return the built-in module
-        # instead of attr.exceptions.
-        #
-        # To identify if `ret` is the top-level package module or the child,
-        # one may innocently look for a '.' in the name. Indeed, the
-        # documentation mentions "the form package.module", but this is
-        # followed by the word "normally". This means that in practice, it's
-        # not always the case. One example is with "from .foo.bar import baz".
-        # In this situation, we get name="foo.bar", but since this is a
-        # relative import, "foo.bar" will not exist in sys.modules. Instead, we
-        # decide to rely on the `fromlist` variable (see the last sentence of
-        # the documentation quote).
-
-        mods = [ret]
-        if not fromlist:
-            # We can not use name here: "import baz" can lead to the import of
-            # the foo.bar.baz module, meaning name="baz" and
-            # ret.__name__="foo.bar.baz"
-            mods.append(sys.modules[ret.__name__])
-
-        for mod in mods:
-           if self._mod_is_blacklisted(mod):
-               continue
-           path = mod.__file__
-           if path.endswith('.pyc'):
-               path = path[:-1]
-           self.filelist.update([op.realpath(path)])
-        return ret
 
     def _open_hook(self, name, mode='r', buffering=-1):
         ret = self._builtin_open(name, mode, buffering)
         self.filelist.update([op.realpath(name)])
         return ret
 
+    def _get_trackable_files(self):
+        files = set()
+        for mod in sys.modules.values():
+            if not hasattr(mod, '__file__'):
+                continue
+            path = op.realpath(mod.__file__)
+            modpath = op.dirname(path)
+            if modpath.startswith(self._pysysdir):
+                continue
+            if path.endswith('.pyc'):
+                path = path[:-1]
+            elif not path.endswith('.py'):
+                continue
+            files.update([path])
+        return files
+
     def start_hooking(self):
-        __builtin__.__import__ = self._import_hook
+        self._start_files = self._get_trackable_files()
         __builtin__.open = self._open_hook
 
     def end_hooking(self):
-        __builtin__.__import__ = self._builtin_import
+        new_files = self._get_trackable_files() - self._start_files
+        self.filelist.update(new_files)
         __builtin__.open = self._builtin_open
