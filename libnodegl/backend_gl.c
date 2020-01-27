119852ede886e454a6d9197de23b25ee17ead94a
Matthieu Bouron <matthieu.bouron@gmail.com>
2020-01-27 09:59:05 +0100

    backend_gl: do not update config structure in gl_resize()

--- libnodegl/backend_gl.c
+++ libnodegl/backend_gl.c
@@ -351,12 +351,9 @@ static int gl_resize(struct ngl_ctx *s, int width, int height, const int *viewpo
     int ret = ngli_glcontext_resize(gl);
     if (ret < 0)
         return ret;
-    config->width = gl->width;
-    config->height = gl->height;
 
     if (viewport && viewport[2] > 0 && viewport[3] > 0) {
         ngli_gctx_set_viewport(s, viewport);
-        memcpy(config->viewport, viewport, sizeof(config->viewport));
     } else {
         const int default_viewport[] = {0, 0, gl->width, gl->height};
         ngli_gctx_set_viewport(s, default_viewport);
