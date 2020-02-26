gabbe-gpsw/gopro-lib-node.gl

glstate: replace ngli_honor_pending_state() with ngli_glstate_update()

libnodegl/backend_gl.c
@@ -385,7 +385,7 @@ static int gl_post_draw(struct ngl_ctx *s, double t)
     struct glcontext *gl = s->glcontext;
     struct ngl_config *config = &s->config;
 
-    ngli_honor_pending_glstate(s);
+    ngli_glstate_update(s, &s->graphicconfig);
 
     if (s->capture_func)
         s->capture_func(s);

libnodegl/glstate.c
@@ -287,12 +287,12 @@ static int honor_state(const struct glcontext *gl,
     return 1;
 }
 
-void ngli_honor_pending_glstate(struct ngl_ctx *ctx)
+void ngli_glstate_update(struct ngl_ctx *ctx, const struct graphicconfig *config)
 {
     struct glcontext *gl = ctx->glcontext;
 
     struct glstate glstate = {0};
-    init_state(&glstate, &ctx->graphicconfig);
+    init_state(&glstate, config);
 
     int ret = honor_state(gl, &glstate, &ctx->glstate);
     if (ret > 0)

libnodegl/pipeline.c
@@ -607,7 +607,7 @@ void ngli_pipeline_exec(struct pipeline *s)
     struct ngl_ctx *ctx = s->ctx;
     struct glcontext *gl = ctx->glcontext;
 
-    ngli_honor_pending_glstate(ctx);
+    ngli_glstate_update(ctx, &ctx->graphicconfig);
 
     use_program(s, gl);
     set_uniforms(s, gl);

NopeForge/nope.gl

libnodegl/backend_gl.c
@@ -379,7 +379,7 @@ static int gl_post_draw(struct ngl_ctx *s, double t)
     struct glcontext *gl = s->glcontext;
     struct ngl_config *config = &s->config;
 
-    ngli_honor_pending_glstate(s);
+    ngli_glstate_update(s, &s->graphicstate);
 
     if (s->capture_func)
         s->capture_func(s);

libnodegl/glstate.c
@@ -280,12 +280,12 @@ static int honor_state(const struct glcontext *gl,
     return 1;
 }
 
-void ngli_honor_pending_glstate(struct ngl_ctx *ctx)
+void ngli_glstate_update(struct ngl_ctx *ctx, const struct graphicstate *state)
 {
     struct glcontext *gl = ctx->glcontext;
 
     struct glstate glstate = {0};
-    init_state(&glstate, &ctx->graphicstate);
+    init_state(&glstate, state);
 
     int ret = honor_state(gl, &glstate, &ctx->glstate);
     if (ret > 0)

libnodegl/pipeline.c
@@ -560,7 +560,7 @@ void ngli_pipeline_exec(struct pipeline *s)
     struct ngl_ctx *ctx = s->ctx;
     struct glcontext *gl = ctx->glcontext;
 
-    ngli_honor_pending_glstate(ctx);
+    ngli_glstate_update(ctx, &ctx->graphicstate);
 
     use_program(s, gl);
     set_uniforms(s, gl);