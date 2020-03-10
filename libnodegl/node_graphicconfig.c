NopeForge/nope.gl

commit 0f63118d0fe66a888d4a6e1308960580763d214d

pipeline: honor the pipeline graphics state instead of the context one
Since the pipelines are associated with a particular graphics state
during the prepare phase, we can directly honor the pipeline graphics
state instead of the one provided by the context during the draw phase

Moreover, the graphicconfig and hud nodes do not need to set manually
their graphics state in the context during the draw phase anymore.

libnodegl/node_graphicconfig.c
@@ -313,9 +313,7 @@ static void graphicconfig_draw(struct ngl_node *node)
     struct graphicconfig_priv *s = node->priv_data;
     struct ngl_node *child = s->child;
 
-    honor_config(node, 0);
     ngli_node_draw(child);
-    honor_config(node, 1);
 }
 
 const struct node_class ngli_graphicconfig_class = {

libnodegl/node_hud.c
@@ -1369,25 +1369,6 @@ static int hud_update(struct ngl_node *node, double t)
     return widget_latency_update(node, &widgets[0], t);
 }
 
-static void honor_config(struct ngl_node *node, int restore)
-{
-    struct ngl_ctx *ctx = node->ctx;
-    struct hud_priv *s = node->priv_data;
-
-    if (restore) {
-        ctx->graphicstate = s->graphicstate;
-    } else {
-        struct graphicstate *pending = &ctx->graphicstate;
-        s->graphicstate = *pending;
-
-        pending->blend = 1;
-        pending->blend_src_factor = NGLI_BLEND_FACTOR_SRC_ALPHA;
-        pending->blend_dst_factor = NGLI_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
-        pending->blend_src_factor_a = NGLI_BLEND_FACTOR_ZERO;
-        pending->blend_dst_factor_a = NGLI_BLEND_FACTOR_ONE;
-    }
-}
-
 static void hud_draw(struct ngl_node *node)
 {
     struct ngl_ctx *ctx = node->ctx;
@@ -1410,15 +1391,11 @@ static void hud_draw(struct ngl_node *node)
     if (ret < 0)
         return;
 
-    honor_config(node, 0);
-
     const float *modelview_matrix  = ngli_darray_tail(&ctx->modelview_matrix_stack);
     const float *projection_matrix = ngli_darray_tail(&ctx->projection_matrix_stack);
     ngli_pipeline_update_uniform(&s->pipeline, s->modelview_matrix_index, modelview_matrix);
     ngli_pipeline_update_uniform(&s->pipeline, s->projection_matrix_index, projection_matrix);
     ngli_pipeline_exec(&s->pipeline);
-
-    honor_config(node, 1);
 }
 
 static void hud_uninit(struct ngl_node *node)

libnodegl/pipeline.c
@@ -561,7 +561,8 @@ void ngli_pipeline_exec(struct pipeline *s)
     struct glcontext *gl = ctx->glcontext;
 
     if (s->type == NGLI_PIPELINE_TYPE_GRAPHICS) {
-        ngli_glstate_update(ctx, &ctx->graphicstate);
+        struct pipeline_graphics *graphics = &s->graphics;
+        ngli_glstate_update(ctx, &graphics->state);
     }
 
     use_program(s, gl);

commit e7d2b82f209a0824632ddd5533faafa85c13c5e9

graphicconfig: honor scissor at draw instead of prepare
Fixes a regression introduced by
0f63118d0fe66a888d4a6e1308960580763d214d.

libnodegl/node_graphicconfig.c
@@ -64,7 +64,6 @@ struct graphicconfig_priv {
     struct graphicstate graphicstate;
     int use_scissor;
     int scissor[4];
-    int prev_scissor[4];
 };
 
 #define DEFAULT_SCISSOR_F {-1.0f, -1.0f, -1.0f, -1.0f}
@@ -257,8 +256,6 @@ static void honor_config(struct ngl_node *node, int restore)
 
     if (restore) {
         ctx->graphicstate = s->graphicstate;
-        if (s->use_scissor)
-            ngli_gctx_set_scissor(ctx, s->prev_scissor);
     } else {
         struct graphicstate *pending = &ctx->graphicstate;
         s->graphicstate = *pending;
@@ -290,10 +287,6 @@ static void honor_config(struct ngl_node *node, int restore)
         COPY_PARAM(cull_face_mode);
 
         COPY_PARAM(scissor_test);
-        if (s->use_scissor) {
-            ngli_gctx_get_scissor(ctx, s->prev_scissor);
-            ngli_gctx_set_scissor(ctx, s->scissor);
-        }
     }
 }
 
@@ -311,10 +304,20 @@ static int graphicconfig_prepare(struct ngl_node *node)
 
 static void graphicconfig_draw(struct ngl_node *node)
 {
+    struct ngl_ctx *ctx = node->ctx;
     struct graphicconfig_priv *s = node->priv_data;
     struct ngl_node *child = s->child;
 
+    int prev_scissor[4];
+    if (s->use_scissor) {
+        ngli_gctx_get_scissor(ctx, prev_scissor);
+        ngli_gctx_set_scissor(ctx, s->scissor);
+    }
+
     ngli_node_draw(child);
+
+    if (s->use_scissor)
+        ngli_gctx_set_scissor(ctx, prev_scissor);
 }
 
 const struct node_class ngli_graphicconfig_class = {