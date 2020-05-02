NopeForge/nope.gl

commit 214ad53bcbeffae1c0bf9f4be56a0df0564e93fe

utils/examples: drop default shared programs
They are now shared internally, so there is no need to explicitly share
a default program in the tree anymore.

pynodegl-utils/pynodegl_utils/examples/timeranges.py
@@ -9,7 +9,6 @@ def queued_medias(cfg, overlap_time=1., dim=3):
     '''Queue of medias, mainly used as a demonstration for the prefetch/release mechanism'''
     nb_videos = dim * dim
     tqs = []
-    p = ngl.Program()
     q = ngl.Quad((-1, -1, 0), (2, 0, 0), (0, 2, 0))
 
     ag = AutoGrid(range(nb_videos))
@@ -50,7 +49,6 @@ def parallel_playback(cfg, fast=True, segment_time=2., constrained_timeranges=Fa
     underlying media, and thus undesired delays.
     '''
     q = ngl.Quad((-1, -1, 0), (2, 0, 0), (0, 2, 0))
-    p = ngl.Program()
 
     m1 = ngl.Media(cfg.medias[0].filename, label='media #1')
     m2 = ngl.Media(cfg.medias[0].filename, label='media #2')
@@ -58,9 +56,9 @@ def parallel_playback(cfg, fast=True, segment_time=2., constrained_timeranges=Fa
     t1 = ngl.Texture2D(data_src=m1, label='texture #1')
     t2 = ngl.Texture2D(data_src=m2, label='texture #2')
 
-    render1 = ngl.Render(q, p, label='render #1')
+    render1 = ngl.Render(q, label='render #1')
     render1.update_textures(tex0=t1)
-    render2 = ngl.Render(q, p, label='render #2')
+    render2 = ngl.Render(q, label='render #2')
     render2.update_textures(tex0=t2)
 
     text_settings={
@@ -113,7 +111,6 @@ def simple_transition(cfg, transition_start=2, transition_duration=4):
     fragment = cfg.get_frag('tex-mix')
 
     q = ngl.Quad((-1, -1, 0), (2, 0, 0), (0, 2, 0))
-    p = ngl.Program()
     p1_2 = ngl.Program(vertex=vertex, fragment=fragment)
 
     m1 = ngl.Media(cfg.medias[0].filename, label='media #1')
@@ -125,9 +122,9 @@ def simple_transition(cfg, transition_start=2, transition_duration=4):
     t1 = ngl.Texture2D(data_src=m1, label='texture #1')
     t2 = ngl.Texture2D(data_src=m2, label='texture #2')
 
-    render1 = ngl.Render(q, p, label='render #1')
+    render1 = ngl.Render(q, label='render #1')
     render1.update_textures(tex0=t1)
-    render2 = ngl.Render(q, p, label='render #2')
+    render2 = ngl.Render(q, label='render #2')
     render2.update_textures(tex0=t2)
 
     delta_animkf = [ngl.AnimKeyFrameFloat(transition_start, 1.0),

pynodegl-utils/pynodegl_utils/examples/medias.py
@@ -43,8 +43,7 @@ def playback_speed(cfg, speed=1.0):
                    ngl.AnimKeyFrameFloat(cfg.duration, media_duration)]
     m = ngl.Media(m0.filename, time_anim=ngl.AnimatedTime(time_animkf))
     t = ngl.Texture2D(data_src=m)
-    p = ngl.Program()
-    render = ngl.Render(q, p)
+    render = ngl.Render(q)
     render.update_textures(tex0=t)
     return render
 

pynodegl-utils/pynodegl_utils/examples/misc.py
@@ -66,8 +66,7 @@ def buffer_dove(cfg,
     if bilinear_filtering:
         img_tex.set_mag_filter('linear')
     quad = ngl.Quad((-.5, -.5, 0.1), (1, 0, 0), (0, 1, 0))
-    prog = ngl.Program()
-    render = ngl.Render(quad, prog, label='dove')
+    render = ngl.Render(quad, label='dove')
     render.update_textures(tex0=img_tex)
     render = ngl.GraphicConfig(render,
                                blend=True,
@@ -463,14 +462,12 @@ def cube(cfg, display_depth_buffer=False):
         rtt.set_depth_texture(depth_texture)
 
         quad = ngl.Quad((-1.0, -1.0, 0), (1, 0, 0), (0, 1, 0))
-        program = ngl.Program()
-        render = ngl.Render(quad, program)
+        render = ngl.Render(quad)
         render.update_textures(tex0=texture)
         group.add_children(rtt, render)
 
         quad = ngl.Quad((0.0, 0.0, 0), (1, 0, 0), (0, 1, 0))
-        program = ngl.Program()
-        render = ngl.Render(quad, program)
+        render = ngl.Render(quad)
         render.update_textures(tex0=depth_texture)
         group.add_children(rtt, render)
 

pynodegl-utils/pynodegl_utils/examples/transforms.py
@@ -80,8 +80,7 @@ def animated_camera(cfg, rotate=True):
     q = ngl.Quad((-0.5, -0.5, 0), (1, 0, 0), (0, 1, 0))
     m = ngl.Media(cfg.medias[0].filename)
     t = ngl.Texture2D(data_src=m)
-    p = ngl.Program()
-    node = ngl.Render(q, p)
+    node = ngl.Render(q)
     node.update_textures(tex0=t)
     g.add_children(node)
 
@@ -140,8 +139,7 @@ def animated_buffer(cfg, dim=50):
     random_tex = ngl.Texture2D(data_src=random_buffer, width=dim, height=dim)
 
     quad = ngl.Quad((-1, -1, 0), (2, 0, 0), (0, 2, 0))
-    prog = ngl.Program()
-    render = ngl.Render(quad, prog)
+    render = ngl.Render(quad)
     render.update_textures(tex0=random_tex)
     return render
 

commit 0999e43a6409e32f89169f1ce0e7d8269579d61b

utils/examples: fix missing program in timeranges.queued_medias
Regression since 214ad53bcbeffae1c0bf9f4be56a0df0564e93fe.

pynodegl-utils/pynodegl_utils/examples/timeranges.py
@@ -20,7 +20,7 @@ def queued_medias(cfg, overlap_time=1., dim=3):
 
         t = ngl.Texture2D(data_src=m)
 
-        render = ngl.Render(q, p)
+        render = ngl.Render(q)
         render.set_label('render #%d' % video_id)
         render.update_textures(tex0=t)
         render = ag.place_node(render, (col, pos))