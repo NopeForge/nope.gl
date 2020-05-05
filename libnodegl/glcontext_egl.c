NopeForge/nope.gl

commit ef519e22afed75f0203a5da96964475818ebc9f9

glcontext: add width, height parameters to ngli_glcontext_resize()
Required by the Wayland platform.

libnodegl/glcontext_egl.c
@@ -320,7 +320,7 @@ static void egl_uninit(struct glcontext *ctx)
 #endif
 }
 
-static int egl_resize(struct glcontext *ctx)
+static int egl_resize(struct glcontext *ctx, int width, int height)
 {
     struct egl_priv *egl = ctx->priv_data;
 

libnodegl/backend_gl.c
@@ -349,7 +349,7 @@ static int gl_resize(struct ngl_ctx *s, int width, int height, const int *viewpo
     if (gl->offscreen)
         return NGL_ERROR_INVALID_USAGE;
 
-    int ret = ngli_glcontext_resize(gl);
+    int ret = ngli_glcontext_resize(gl, width, height);
     if (ret < 0)
         return ret;
 

libnodegl/glcontext.c
@@ -385,7 +385,7 @@ struct glcontext *ngli_glcontext_new(const struct ngl_config *config)
         ngli_glEnable(glcontext, GL_TEXTURE_CUBE_MAP_SEAMLESS);
 
     if (!glcontext->offscreen) {
-        int ret = ngli_glcontext_resize(glcontext);
+        int ret = ngli_glcontext_resize(glcontext, glcontext->width, glcontext->height);
         if (ret < 0)
             goto fail;
     }
@@ -427,7 +427,7 @@ void ngli_glcontext_set_surface_pts(struct glcontext *glcontext, double t)
         glcontext->class->set_surface_pts(glcontext, t);
 }
 
-int ngli_glcontext_resize(struct glcontext *glcontext)
+int ngli_glcontext_resize(struct glcontext *glcontext, int width, int height)
 {
     if (glcontext->offscreen) {
         LOG(ERROR, "offscreen rendering does not support resize operation");
@@ -435,7 +435,7 @@ int ngli_glcontext_resize(struct glcontext *glcontext)
     }
 
     if (glcontext->class->resize)
-        return glcontext->class->resize(glcontext);
+        return glcontext->class->resize(glcontext, width, height);
 
     return NGL_ERROR_UNSUPPORTED;
 }

libnodegl/glcontext.h
@@ -94,7 +94,7 @@ struct glcontext {
 
 struct glcontext_class {
     int (*init)(struct glcontext *glcontext, uintptr_t display, uintptr_t window, uintptr_t handle);
-    int (*resize)(struct glcontext *glcontext);
+    int (*resize)(struct glcontext *glcontext, int width, int height);
     int (*make_current)(struct glcontext *glcontext, int current);
     void (*swap_buffers)(struct glcontext *glcontext);
     int (*set_swap_interval)(struct glcontext *glcontext, int interval);
@@ -113,7 +113,7 @@ int ngli_glcontext_make_current(struct glcontext *glcontext, int current);
 void ngli_glcontext_swap_buffers(struct glcontext *glcontext);
 int ngli_glcontext_set_swap_interval(struct glcontext *glcontext, int interval);
 void ngli_glcontext_set_surface_pts(struct glcontext *glcontext, double t);
-int ngli_glcontext_resize(struct glcontext *glcontext);
+int ngli_glcontext_resize(struct glcontext *glcontext, int width, int height);
 void *ngli_glcontext_get_proc_address(struct glcontext *glcontext, const char *name);
 void *ngli_glcontext_get_texture_cache(struct glcontext *glcontext);
 uintptr_t ngli_glcontext_get_display(struct glcontext *glcontext);

libnodegl/glcontext_eagl.m
@@ -217,7 +217,7 @@ static void eagl_uninit(struct glcontext *ctx)
         CFRelease(eagl->handle);
 }
 
-static int eagl_resize(struct glcontext *ctx)
+static int eagl_resize(struct glcontext *ctx, int width, int height)
 {
     if (![NSThread isMainThread]) {
         LOG(ERROR, "eagl_resize() must be called from the UI thread");

libnodegl/glcontext_nsgl.m
@@ -139,7 +139,7 @@ static int nsgl_init(struct glcontext *ctx, uintptr_t display, uintptr_t window,
     return 0;
 }
 
-static int nsgl_resize(struct glcontext *ctx)
+static int nsgl_resize(struct glcontext *ctx, int width, int height)
 {
     struct nsgl_priv *nsgl = ctx->priv_data;
 

libnodegl/glcontext_wgl.c
@@ -224,7 +224,7 @@ static void wgl_uninit(struct glcontext *ctx)
         FreeLibrary(wgl->module);
 }
 
-static int wgl_resize(struct glcontext *ctx)
+static int wgl_resize(struct glcontext *ctx, int width, int height)
 {
     struct wgl_priv *wgl = ctx->priv_data;
 

commit 25a08fb3d05dddc4c48fcd93944564cd1f5449ee

glcontext_egl: do not try to re-define width and height variables in egl_resize()
Fixes a regression introduced by
ef519e22afed75f0203a5da96964475818ebc9f9.

libnodegl/glcontext_egl.c
@@ -400,15 +400,15 @@ static int egl_resize(struct glcontext *ctx, int width, int height)
     struct egl_priv *egl = ctx->priv_data;
 
 #if defined(TARGET_ANDROID)
-    const int width = ANativeWindow_getWidth(egl->native_window);
-    const int height = ANativeWindow_getHeight(egl->native_window);
+    const int w_width = ANativeWindow_getWidth(egl->native_window);
+    const int w_height = ANativeWindow_getHeight(egl->native_window);
     GLint format;
     eglGetConfigAttrib(egl->display, egl->config, EGL_NATIVE_VISUAL_ID, &format);
     /* Resize the native window buffers to the native window size. This
      * ensures that the current EGL buffer will be of the same size as the
      * native window after a resize. See:
      * https://www.khronos.org/registry/EGL/sdk/docs/man/html/eglSwapBuffers.xhtml */
-    int ret = ANativeWindow_setBuffersGeometry(egl->native_window, width, height, format);
+    int ret = ANativeWindow_setBuffersGeometry(egl->native_window, w_width, w_height, format);
     if (ret < 0)
         return -1;
 #endif