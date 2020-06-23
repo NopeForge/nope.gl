gabbe-gpsw/gopro-lib-node.gl

gctx: add ngli_get_prefered_depth{_stencil}_format()

libnodegl/gctx.c
@@ -119,3 +119,13 @@ void ngli_gctx_invalidate_depth_stencil(struct ngl_ctx *s)
 void ngli_gctx_flush(struct ngl_ctx *s)
 {
 }
+
+int ngli_gctx_get_prefered_depth_format(struct ngl_ctx *s)
+{
+    return NGLI_FORMAT_D16_UNORM;
+}
+
+int ngli_gctx_get_prefered_depth_stencil_format(struct ngl_ctx *s)
+{
+    return NGLI_FORMAT_D24_UNORM_S8_UINT;
+}

libnodegl/gctx.h
@@ -41,4 +41,7 @@ void ngli_gctx_invalidate_depth_stencil(struct ngl_ctx *s);
 
 void ngli_gctx_flush(struct ngl_ctx *s);
 
+int ngli_gctx_get_prefered_depth_format(struct ngl_ctx *s);
+int ngli_gctx_get_prefered_depth_stencil_format(struct ngl_ctx *s);
+
 #endif

NopeForge/nope.gl

libnodegl/gctx.c
@@ -156,3 +156,13 @@ void ngli_gctx_invalidate_depth_stencil(struct gctx *s)
 {
     s->class->invalidate_depth_stencil(s);
 }
+
+int ngli_gctx_get_prefered_depth_format(struct gctx *s)
+{
+    return s->class->get_prefered_depth_format(s);
+}
+
+int ngli_gctx_get_prefered_depth_stencil_format(struct gctx *s)
+{
+    return s->class->get_prefered_depth_stencil_format(s);
+}

libnodegl/gctx.h
@@ -53,6 +53,8 @@ struct gctx_class {
     void (*clear_color)(struct gctx *s);
     void (*clear_depth_stencil)(struct gctx *s);
     void (*invalidate_depth_stencil)(struct gctx *s);
+    int (*get_prefered_depth_format)(struct gctx *s);
+    int (*get_prefered_depth_stencil_format)(struct gctx *s);
 
     struct buffer *(*buffer_create)(struct gctx *ctx);
     int (*buffer_init)(struct buffer *s, int size, int usage);
@@ -122,5 +124,8 @@ void ngli_gctx_get_clear_color(struct gctx *s, float *color);
 void ngli_gctx_clear_color(struct gctx *s);
 void ngli_gctx_clear_depth_stencil(struct gctx *s);
 void ngli_gctx_invalidate_depth_stencil(struct gctx *s);
+
+int ngli_gctx_get_prefered_depth_format(struct gctx *s);
+int ngli_gctx_get_prefered_depth_stencil_format(struct gctx *s);
 
 #endif