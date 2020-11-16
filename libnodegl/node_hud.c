b1bf2fbce3ad8dbeee1a2c8136ac19c8cea4d00f
Matthieu Bouron <matthieu.bouron@gmail.com>
2020-11-16 10:21:34 +0100

    hud: use nearest filtering for the HUD texture
    Since adbac2c1c21e502611310444248fcdce9da12f65 the HUD rendering is not
    scaled anymore depending on the output surface size, thus, trilinear
    filtering is not needed anymore on the HUD texture.

--- libnodegl/node_hud.c
+++ libnodegl/node_hud.c
@@ -1244,9 +1244,8 @@ static int hud_init(struct ngl_node *node)
         .format        = NGLI_FORMAT_R8G8B8A8_UNORM,
         .width         = s->canvas.w,
         .height        = s->canvas.h,
-        .min_filter    = NGLI_FILTER_LINEAR,
+        .min_filter    = NGLI_FILTER_NEAREST,
         .mag_filter    = NGLI_FILTER_NEAREST,
-        .mipmap_filter = NGLI_MIPMAP_FILTER_LINEAR,
     };
     s->texture = ngli_texture_create(gctx);
     if (!s->texture)
