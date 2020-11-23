4ba3519f0796d820d3ce92886d0443a8ea4fd786
Matthieu Bouron <matthieu.bouron@gmail.com>
2020-11-23 11:00:35 +0100

    pgcraft: add support for GL_OES_texture_3d

--- libnodegl/pgcraft.c
+++ libnodegl/pgcraft.c
@@ -532,6 +532,7 @@ static void set_glsl_header(struct pgcraft *s, struct bstr *b, const struct pgcr
     const int require_image_external_essl3_feature = ngli_darray_count(&s->texture_infos) > 0 && s->glsl_version >= 300;
 #endif
     const int enable_shader_texture_lod = (gctx->features & NGLI_FEATURE_SHADER_TEXTURE_LOD) == NGLI_FEATURE_SHADER_TEXTURE_LOD;
+    const int enable_texture_3d         = (gctx->features & NGLI_FEATURE_TEXTURE_3D) == NGLI_FEATURE_TEXTURE_3D;
 
     const struct {
         int backend;
@@ -552,6 +553,7 @@ static void set_glsl_header(struct pgcraft *s, struct bstr *b, const struct pgcr
         {NGL_BACKEND_OPENGLES, "GL_OES_EGL_image_external_essl3", INT_MAX, require_image_external_essl3_feature},
 #endif
         {NGL_BACKEND_OPENGLES, "GL_EXT_shader_texture_lod",           300, enable_shader_texture_lod},
+        {NGL_BACKEND_OPENGLES, "GL_OES_texture_3D",                   300, enable_texture_3d},
     };
 
     for (int i = 0; i < NGLI_ARRAY_NB(features); i++) {
