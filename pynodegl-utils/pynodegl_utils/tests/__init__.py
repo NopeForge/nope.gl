b6aa81965a8daf4f7c9b2af7ed70e446d08d99f3
Clément Bœsch <u@pkh.me>
2021-02-19 10:27:11 +0100

    tests: adjust and rename GEN semantic to REFGEN

--- pynodegl-utils/pynodegl_utils/tests/__init__.py
+++ pynodegl-utils/pynodegl_utils/tests/__init__.py
@@ -48,14 +48,14 @@ def _get_ref_data(tester, ref_filepath):
 
 def _run_test_default(func_name, tester, ref_filepath, dump=False):
     if not op.exists(ref_filepath):
-        sys.stderr.write(f'{func_name}: reference file {ref_filepath} not found, use GEN=yes to create it\n')
+        sys.stderr.write(f'{func_name}: reference file {ref_filepath} not found, use REFGEN=create to create it\n')
         sys.exit(1)
     ref_data = _get_ref_data(tester, ref_filepath)
     out_data = tester.get_out_data(dump, func_name)
     return _run_test(func_name, tester, ref_data, out_data)
 
 
-def _run_test_gen_yes(func_name, tester, ref_filepath, dump=False):
+def _run_test_gen_create(func_name, tester, ref_filepath, dump=False):
     out_data = tester.get_out_data(dump, func_name)
     if not op.exists(ref_filepath):
         sys.stderr.write(f'{func_name}: creating {ref_filepath}\n')
@@ -89,19 +89,20 @@ def _run_test_gen_force(func_name, tester, ref_filepath, dump=False):
     return []
 
 
-_gen_map = {
-    'yes': _run_test_gen_yes,
+_refgen_map = {
+    'no': _run_test_default,
+    'create': _run_test_gen_create,
     'update': _run_test_gen_update,
     'force': _run_test_gen_force,
 }
 
 
 def run():
-    gen_opt = os.environ.get('GEN')
+    refgen_opt = os.environ.get('REFGEN', 'no')
 
-    allowed_gen_opt = ('yes', 'update', 'force')
-    if gen_opt is not None and gen_opt not in allowed_gen_opt:
-        sys.stderr.write('GEN environment variable must be any of {}\n'.format(', '.join(allowed_gen_opt)))
+    allowed_gen_opt = _refgen_map.keys()
+    if refgen_opt not in allowed_gen_opt:
+        sys.stderr.write('REFGEN environment variable must be any of {}\n'.format(', '.join(allowed_gen_opt)))
         sys.exit(1)
 
     tests_opts = os.environ.get('TESTS_OPTIONS')
@@ -113,13 +114,14 @@ def run():
     dump = tests_opts == 'dump'
 
     if len(sys.argv) not in (3, 4):
-        sys.stderr.write('''Usage: [TESTS_OPTIONS={} GEN={}] {} <script_path> <func_name> [<ref_filepath>]
+        sys.stderr.write('''Usage: [TESTS_OPTIONS={} REFGEN={}] {} <script_path> <func_name> [<ref_filepath>]
 
-    GEN=yes     Create the reference file if not present
-    GEN=update  Same as "yes" and update the reference if the test fails
-    GEN=force   Same as "yes" and always replace the reference file. This may
-                change the references even if the tests are passing due to the
-                threshold/tolerance mechanism.
+    REFGEN=no      Run the test normally without changing the reference (default)
+    REFGEN=create  Create the reference file if not present
+    REFGEN=update  Same as "create" and update the reference if the test fails
+    REFGEN=force   Same as "create" and always replace the reference file. This may
+                   change the references even if the tests are passing due to the
+                   threshold/tolerance mechanism.
 
     TESTS_OPTIONS=dump Dump tests outputs to the filesystem (in <temp directory>/nodegl/tests)
 '''.format('|'.join(allowed_tests_opts), '|'.join(allowed_gen_opt), op.basename(sys.argv[0])))
@@ -136,7 +138,7 @@ def run():
         sys.exit(func())
 
     tester = func.tester
-    test_func = _gen_map.get(gen_opt, _run_test_default)
+    test_func = _refgen_map[refgen_opt]
     err = test_func(func_name, tester, ref_filepath, dump)
     if err:
         sys.stderr.write('\n'.join(err) + '\n')
