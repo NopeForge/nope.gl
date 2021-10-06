8c5133ef42cb8959965126ddf1efc803fe1e67af
Matthieu Bouron <matthieu.bouron@gmail.com>
2021-10-06 17:21:13 +0200

    hwmap_videotoolbox_darwin_gl: add format_desc structure and use it

--- libnodegl/backends/gl/hwmap_videotoolbox_darwin_gl.c
+++ libnodegl/backends/gl/hwmap_videotoolbox_darwin_gl.c
@@ -39,9 +39,35 @@
 #include "internal.h"
 #include "texture_gl.h"
 
+struct format_desc {
+    int layout;
+    int nb_planes;
+    struct {
+        int format;
+    } planes[2];
+};
+
+static int vt_get_format_desc(OSType format, struct format_desc *desc)
+{
+    switch (format) {
+    case kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange:
+        desc->layout = NGLI_IMAGE_LAYOUT_NV12_RECTANGLE;
+        desc->nb_planes = 2;
+        desc->planes[0].format = NGLI_FORMAT_R8_UNORM;
+        desc->planes[1].format = NGLI_FORMAT_R8G8_UNORM;
+        break;
+    default:
+        return NGL_ERROR_UNSUPPORTED;
+    }
+
+    return 0;
+}
+
 struct hwmap_vt_darwin {
     struct sxplayer_frame *frame;
     struct texture *planes[2];
+    OSType format;
+    struct format_desc format_desc;
 };
 
 static int vt_darwin_map_plane(struct hwmap *hwmap, IOSurfaceRef surface, int index)
@@ -61,7 +87,7 @@ static int vt_darwin_map_plane(struct hwmap *hwmap, IOSurfaceRef surface, int in
 
     CGLError err = CGLTexImageIOSurface2D(CGLGetCurrentContext(), plane_gl->target,
                                           plane_gl->internal_format, width, height,
-                                          plane_gl->format, plane_gl->format_type, surface, index);
+                                          plane_gl->format, format_type, surface, index);
     if (err != kCGLNoError) {
         LOG(ERROR, "could not bind IOSurface plane %d to texture %d: %s", index, plane_gl->id, CGLErrorString(err));
         return -1;
@@ -75,24 +101,20 @@ static int vt_darwin_map_plane(struct hwmap *hwmap, IOSurfaceRef surface, int in
 static int vt_darwin_map_frame(struct hwmap *hwmap, struct sxplayer_frame *frame)
 {
     struct hwmap_vt_darwin *vt = hwmap->hwmap_priv_data;
+    CVPixelBufferRef cvpixbuf = (CVPixelBufferRef)frame->data;
+    OSType cvformat = CVPixelBufferGetPixelFormatType(cvpixbuf);
+    ngli_assert(vt->format == cvformat);
 
     sxplayer_release_frame(vt->frame);
     vt->frame = frame;
 
-    CVPixelBufferRef cvpixbuf = (CVPixelBufferRef)frame->data;
     IOSurfaceRef surface = CVPixelBufferGetIOSurface(cvpixbuf);
     if (!surface) {
         LOG(ERROR, "could not get IOSurface from buffer");
         return -1;
     }
 
-    OSType format = IOSurfaceGetPixelFormat(surface);
-    if (format != kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange) {
-        LOG(ERROR, "unsupported IOSurface format: 0x%x", format);
-        return -1;
-    }
-
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < vt->format_desc.nb_planes; i++) {
         int ret = vt_darwin_map_plane(hwmap, surface, i);
         if (ret < 0)
             return ret;
@@ -101,14 +123,24 @@ static int vt_darwin_map_frame(struct hwmap *hwmap, struct sxplayer_frame *frame
     return 0;
 }
 
-static int support_direct_rendering(struct hwmap *hwmap)
+static int support_direct_rendering(struct hwmap *hwmap, struct sxplayer_frame *frame)
 {
     const struct hwmap_params *params = &hwmap->params;
 
-    int direct_rendering = params->image_layouts & (1 << NGLI_IMAGE_LAYOUT_NV12_RECTANGLE);
+    CVPixelBufferRef cvpixbuf = (CVPixelBufferRef)frame->data;
+    OSType cvformat = CVPixelBufferGetPixelFormatType(cvpixbuf);
+    int direct_rendering = 1;
+
+    switch (cvformat) {
+    case kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange:
+        direct_rendering = params->image_layouts & (1 << NGLI_IMAGE_LAYOUT_NV12_RECTANGLE);
+        break;
+    default:
+        ngli_assert(0);
+    }
 
     if (direct_rendering && params->texture_mipmap_filter) {
-        LOG(WARNING, "IOSurface NV12 buffers do not support mipmapping: "
+        LOG(WARNING, "Videotoolbox textures do not support mipmapping: "
             "disabling direct rendering");
         direct_rendering = 0;
     }
@@ -123,10 +155,17 @@ static int vt_darwin_init(struct hwmap *hwmap, struct sxplayer_frame * frame)
     struct hwmap_vt_darwin *vt = hwmap->hwmap_priv_data;
     const struct hwmap_params *params = &hwmap->params;
 
-    for (int i = 0; i < 2; i++) {
+    CVPixelBufferRef cvpixbuf = (CVPixelBufferRef)frame->data;
+    vt->format = CVPixelBufferGetPixelFormatType(cvpixbuf);
+
+    int ret = vt_get_format_desc(vt->format, &vt->format_desc);
+    if (ret < 0)
+        return ret;
+
+    for (int i = 0; i < vt->format_desc.nb_planes; i++) {
         const struct texture_params plane_params = {
             .type             = NGLI_TEXTURE_TYPE_2D,
-            .format           = i == 0 ? NGLI_FORMAT_R8_UNORM : NGLI_FORMAT_R8G8_UNORM,
+            .format           = vt->format_desc.planes[i].format,
             .min_filter       = params->texture_min_filter,
             .mag_filter       = params->texture_mag_filter,
             .wrap_s           = params->texture_wrap_s,
@@ -148,13 +187,13 @@ static int vt_darwin_init(struct hwmap *hwmap, struct sxplayer_frame * frame)
     struct image_params image_params = {
         .width = frame->width,
         .height = frame->height,
-        .layout = NGLI_IMAGE_LAYOUT_NV12_RECTANGLE,
+        .layout = vt->format_desc.layout,
         .color_scale = 1.f,
         .color_info = ngli_color_info_from_sxplayer_frame(frame),
     };
     ngli_image_init(&hwmap->mapped_image, &image_params, vt->planes);
 
-    hwmap->require_hwconv = !support_direct_rendering(hwmap);
+    hwmap->require_hwconv = !support_direct_rendering(hwmap, frame);
 
     return 0;
 }
@@ -171,7 +210,7 @@ static void vt_darwin_uninit(struct hwmap *hwmap)
 }
 
 const struct hwmap_class ngli_hwmap_vt_darwin_gl_class = {
-    .name      = "videotoolbox (iosurface → nv12)",
+    .name      = "videotoolbox (iosurface)",
     .hwformat  = SXPLAYER_PIXFMT_VT,
     .flags     = HWMAP_FLAG_FRAME_OWNER,
     .priv_size = sizeof(struct hwmap_vt_darwin),
