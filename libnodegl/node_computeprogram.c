af6c1d17344b9290d06f4be325927de5080637dc
Clément Bœsch <u@pkh.me>
2021-12-29 23:52:00 +0100

    computeprogram: add group size limit checks

--- libnodegl/node_computeprogram.c
+++ libnodegl/node_computeprogram.c
@@ -20,6 +20,9 @@
  */
 
 #include <stddef.h>
+
+#include "gpu_ctx.h"
+#include "gpu_limits.h"
 #include "log.h"
 #include "nodegl.h"
 #include "internal.h"
@@ -40,10 +43,32 @@ static const struct node_param computeprogram_params[] = {
 static int computeprogram_init(struct ngl_node *node)
 {
     struct program_priv *s = node->priv_data;
+    struct ngl_ctx *ctx = node->ctx;
 
     if (s->workgroup_size[0] <= 0 || s->workgroup_size[1] <= 0 || s->workgroup_size[2] <= 0) {
         LOG(ERROR, "work group size must be > 0 for x, y and z");
         return NGL_ERROR_INVALID_ARG;
+    }
+
+    const struct gpu_ctx *gpu_ctx = ctx->gpu_ctx;
+    const struct gpu_limits *limits = &gpu_ctx->limits;
+
+    if (s->workgroup_size[0] > limits->max_compute_work_group_size[0] ||
+        s->workgroup_size[1] > limits->max_compute_work_group_size[1] ||
+        s->workgroup_size[2] > limits->max_compute_work_group_size[2]) {
+        LOG(ERROR,
+            "compute work group size (%d, %d, %d) exceeds device limits (%d, %d, %d)",
+            NGLI_ARG_VEC3(s->workgroup_size),
+            NGLI_ARG_VEC3(limits->max_compute_work_group_size));
+        return NGL_ERROR_GRAPHICS_LIMIT_EXCEEDED;
+    }
+
+    const uint32_t nb_invocations = s->workgroup_size[0] * s->workgroup_size[1] * s->workgroup_size[2];
+    const uint32_t max_invocations = limits->max_compute_work_group_invocations;
+    if (nb_invocations > max_invocations) {
+        LOG(ERROR, "compute number of invocations (%u) exceeds device limits (%u)",
+            nb_invocations, max_invocations);
+        return NGL_ERROR_GRAPHICS_LIMIT_EXCEEDED;
     }
 
     return 0;
