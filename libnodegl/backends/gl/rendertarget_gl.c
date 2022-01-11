NopeForge/nope.gl

commit de74f3b6e8ce17b70e8dfa999672684eafe54cae

rendertarget_gl: rename ngli_rendertarget_gl_resolve() to ngli_rendertarget_gl_end_pass()
Renames ngli_rendertarget_gl_resolve() to
ngli_rendertarget_gl_end_pass() and inlines
ngli_rendertarget_gl_invalidate() into this new function.

libnodegl/backends/gl/rendertarget_gl.c
@@ -339,7 +339,7 @@ done:;
     return ret;
 }
 
-void ngli_rendertarget_gl_resolve(struct rendertarget *s)
+void ngli_rendertarget_gl_end_pass(struct rendertarget *s)
 {
     const struct rendertarget_gl *s_priv = (const struct rendertarget_gl *)s;
     struct gpu_ctx_gl *gpu_ctx_gl = (struct gpu_ctx_gl *)s->gpu_ctx;
@@ -365,6 +365,8 @@ void ngli_rendertarget_gl_resolve(struct rendertarget *s)
     struct rendertarget_gl *rt_gl = (struct rendertarget_gl *)rt;
     const GLuint fbo_id = rt_gl ? rt_gl->id : ngli_glcontext_get_default_framebuffer(gl);
     ngli_glBindFramebuffer(gl, GL_FRAMEBUFFER, fbo_id);
+
+    s_priv->invalidate(s);
 }
 
 void ngli_rendertarget_gl_begin_pass(struct rendertarget *s)
@@ -398,12 +400,6 @@ void ngli_rendertarget_gl_begin_pass(struct rendertarget *s)
     ngli_glBindFramebuffer(gl, GL_FRAMEBUFFER, s_priv->id);
 
     s_priv->clear(s);
-}
-
-void ngli_rendertarget_gl_invalidate(struct rendertarget *s)
-{
-    const struct rendertarget_gl *s_priv = (struct rendertarget_gl *)s;
-    s_priv->invalidate(s);
 }
 
 void ngli_rendertarget_gl_read_pixels(struct rendertarget *s, uint8_t *data)

libnodegl/backends/gl/gpu_ctx_gl.c
@@ -839,8 +839,7 @@ static void gl_end_render_pass(struct gpu_ctx *s)
     struct gpu_ctx_gl *s_priv = (struct gpu_ctx_gl *)s;
 
     if (s_priv->rendertarget) {
-        ngli_rendertarget_gl_resolve(s_priv->rendertarget);
-        ngli_rendertarget_gl_invalidate(s_priv->rendertarget);
+        ngli_rendertarget_gl_end_pass(s_priv->rendertarget);
     }
 
     s_priv->rendertarget = NULL;

libnodegl/backends/gl/rendertarget_gl.h
@@ -41,9 +41,8 @@ struct rendertarget_gl {
 
 struct rendertarget *ngli_rendertarget_gl_create(struct gpu_ctx *gpu_ctx);
 int ngli_rendertarget_gl_init(struct rendertarget *s, const struct rendertarget_params *params);
-void ngli_rendertarget_gl_resolve(struct rendertarget *s);
+void ngli_rendertarget_gl_end_pass(struct rendertarget *s);
 void ngli_rendertarget_gl_begin_pass(struct rendertarget *s);
-void ngli_rendertarget_gl_invalidate(struct rendertarget *s);
 void ngli_rendertarget_gl_read_pixels(struct rendertarget *s, uint8_t *data);
 void ngli_rendertarget_gl_freep(struct rendertarget **sp);
 

commit 26c49ab28c1c2bc954b2079991bb0fd8757ec7f9

rendertarget_gl: do not skip buffers invalidation if there is no resolve fbo
Fixes a regression introduced by
de74f3b6e8ce17b70e8dfa999672684eafe54cae.

libnodegl/backends/gl/rendertarget_gl.c
@@ -389,17 +389,17 @@ void ngli_rendertarget_gl_end_pass(struct rendertarget *s)
         glstate->scissor_test = 0;
     }
 
-    if (!s_priv->resolve_id)
-        return;
+    if (s_priv->resolve_id) {
+        ngli_glBindFramebuffer(gl, GL_READ_FRAMEBUFFER, s_priv->id);
+        ngli_glBindFramebuffer(gl, GL_DRAW_FRAMEBUFFER, s_priv->resolve_id);
 
-    ngli_glBindFramebuffer(gl, GL_READ_FRAMEBUFFER, s_priv->id);
-    ngli_glBindFramebuffer(gl, GL_DRAW_FRAMEBUFFER, s_priv->resolve_id);
-    s_priv->resolve(s);
+        s_priv->resolve(s);
 
-    struct rendertarget *rt = gpu_ctx_gl->current_rt;
-    struct rendertarget_gl *rt_gl = (struct rendertarget_gl *)rt;
-    const GLuint fbo_id = rt_gl ? rt_gl->id : ngli_glcontext_get_default_framebuffer(gl);
-    ngli_glBindFramebuffer(gl, GL_FRAMEBUFFER, fbo_id);
+        struct rendertarget *rt = gpu_ctx_gl->current_rt;
+        struct rendertarget_gl *rt_gl = (struct rendertarget_gl *)rt;
+        const GLuint fbo_id = rt_gl ? rt_gl->id : ngli_glcontext_get_default_framebuffer(gl);
+        ngli_glBindFramebuffer(gl, GL_FRAMEBUFFER, fbo_id);
+    }
 
     s_priv->invalidate(s);
 }