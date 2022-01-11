NopeForge/nope.gl

commit 119f7a61f9b889f5b43e1cf1936c43f8cddfa1bd

glcontext_eagl: use ngli_glCheckFramebufferStatus()
Uses ngli_glCheckFramebufferStatus() instead of
glCheckFramebufferStatus(). The rest of the code already calls OpenGL
through the ngli wrapper.

libnodegl/backends/gl/glcontext_eagl.m
@@ -159,7 +159,7 @@ static int eagl_init_framebuffer(struct glcontext *ctx)
         ngli_glFramebufferRenderbuffer(ctx, GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, eagl->depthbuffer);
     }
 
-    GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+    GLenum status = ngli_glCheckFramebufferStatus(gl, GL_FRAMEBUFFER);
     if (status != GL_FRAMEBUFFER_COMPLETE) {
        LOG(ERROR, "framebuffer is not complete: 0x%x", status);
        return -1;
@@ -180,7 +180,7 @@ static int eagl_init_framebuffer(struct glcontext *ctx)
         ngli_glFramebufferRenderbuffer(ctx, GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, eagl->depthbuffer);
         ngli_glFramebufferRenderbuffer(ctx, GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, eagl->depthbuffer);
 
-        status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+        status = ngli_glCheckFramebufferStatus(gl, GL_FRAMEBUFFER);
         if (status != GL_FRAMEBUFFER_COMPLETE) {
             LOG(ERROR, "framebuffer is not complete: 0x%x", status);
             return -1;

commit 7e3699762f6f6914d2885329689135b1503b3e5a

glcontext_eagl: fix typo
Fixes a build regression introduced by
119f7a61f9b889f5b43e1cf1936c43f8cddfa1bd.

libnodegl/backends/gl/glcontext_eagl.m
@@ -159,7 +159,7 @@ static int eagl_init_framebuffer(struct glcontext *ctx)
         ngli_glFramebufferRenderbuffer(ctx, GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, eagl->depthbuffer);
     }
 
-    GLenum status = ngli_glCheckFramebufferStatus(gl, GL_FRAMEBUFFER);
+    GLenum status = ngli_glCheckFramebufferStatus(ctx, GL_FRAMEBUFFER);
     if (status != GL_FRAMEBUFFER_COMPLETE) {
        LOG(ERROR, "framebuffer is not complete: 0x%x", status);
        return -1;
@@ -180,7 +180,7 @@ static int eagl_init_framebuffer(struct glcontext *ctx)
         ngli_glFramebufferRenderbuffer(ctx, GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, eagl->depthbuffer);
         ngli_glFramebufferRenderbuffer(ctx, GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, eagl->depthbuffer);
 
-        status = ngli_glCheckFramebufferStatus(gl, GL_FRAMEBUFFER);
+        status = ngli_glCheckFramebufferStatus(ctx, GL_FRAMEBUFFER);
         if (status != GL_FRAMEBUFFER_COMPLETE) {
             LOG(ERROR, "framebuffer is not complete: 0x%x", status);
             return -1;