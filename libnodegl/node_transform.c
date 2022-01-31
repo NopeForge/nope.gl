55432b0f876ee1c91cdd1176c04f60d68cb851b8
Clément Bœsch <u@pkh.me>
2022-01-31 15:03:20 +0100

    transform: use a private context
    Consistent with the other transform.

--- libnodegl/node_transform.c
+++ libnodegl/node_transform.c
@@ -21,35 +21,68 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
+
 #include "log.h"
 #include "nodegl.h"
 #include "internal.h"
 #include "math_utils.h"
 #include "transforms.h"
 
-#define OFFSET(x) offsetof(struct transform, x)
+struct transform_opts {
+    struct ngl_node *child;
+    float matrix[4*4];
+};
+
+struct transform_priv {
+    struct transform trf;
+    struct transform_opts opts;
+};
+
+static int update_matrix(struct ngl_node *node)
+{
+    struct transform_priv *s = node->priv_data;
+    const struct transform_opts *o = &s->opts;
+    memcpy(s->trf.matrix, o->matrix, sizeof(o->matrix));
+    return 0;
+}
+
+#define OFFSET(x) offsetof(struct transform_priv, opts.x)
 static const struct node_param transform_params[] = {
     {"child",  NGLI_PARAM_TYPE_NODE, OFFSET(child), .flags=NGLI_PARAM_FLAG_NON_NULL,
                .desc=NGLI_DOCSTRING("scene to apply the transform to")},
     {"matrix", NGLI_PARAM_TYPE_MAT4, OFFSET(matrix), {.mat=NGLI_MAT4_IDENTITY},
                .flags=NGLI_PARAM_FLAG_ALLOW_LIVE_CHANGE,
+               .update_func=update_matrix,
                .desc=NGLI_DOCSTRING("transformation matrix")},
     {NULL}
 };
 
+NGLI_STATIC_ASSERT(trf_on_top_of_transform, offsetof(struct transform_priv, trf) == 0);
+
+static int transform_init(struct ngl_node *node)
+{
+    struct transform_priv *s = node->priv_data;
+    const struct transform_opts *o = &s->opts;
+    memcpy(s->trf.matrix, o->matrix, sizeof(o->matrix));
+    s->trf.child = o->child;
+    return 0;
+}
+
 static int transform_update(struct ngl_node *node, double t)
 {
-    struct transform *s = node->priv_data;
-    struct ngl_node *child = s->child;
-    return ngli_node_update(child, t);
+    struct transform_priv *s = node->priv_data;
+    const struct transform_opts *o = &s->opts;
+    return ngli_node_update(o->child, t);
 }
 
 const struct node_class ngli_transform_class = {
     .id        = NGL_NODE_TRANSFORM,
     .name      = "Transform",
+    .init      = transform_init,
     .update    = transform_update,
     .draw      = ngli_transform_draw,
-    .priv_size = sizeof(struct transform),
+    .priv_size = sizeof(struct transform_priv),
     .params    = transform_params,
     .file      = __FILE__,
 };
