3cb5726b8bb2695ec97425b67c8dc82d61ecb5b8
Clément Bœsch <u@pkh.me>
2022-01-31 17:53:52 +0100

    group: move node parameters to a dedicated options structure

--- libnodegl/node_group.c
+++ libnodegl/node_group.c
@@ -25,12 +25,16 @@
 #include "nodegl.h"
 #include "internal.h"
 
-struct group_priv {
+struct group_opts {
     struct ngl_node **children;
     int nb_children;
 };
 
-#define OFFSET(x) offsetof(struct group_priv, x)
+struct group_priv {
+    struct group_opts opts;
+};
+
+#define OFFSET(x) offsetof(struct group_priv, opts.x)
 static const struct node_param group_params[] = {
     {"children", NGLI_PARAM_TYPE_NODELIST, OFFSET(children),
                  .desc=NGLI_DOCSTRING("a set of scenes")},
@@ -41,16 +45,17 @@ static int group_prepare(struct ngl_node *node)
 {
     struct ngl_ctx *ctx = node->ctx;
     struct group_priv *s = node->priv_data;
+    const struct group_opts *o = &s->opts;
 
     int ret = 0;
     struct rnode *rnode_pos = ctx->rnode_pos;
-    for (int i = 0; i < s->nb_children; i++) {
+    for (int i = 0; i < o->nb_children; i++) {
         struct rnode *rnode = ngli_rnode_add_child(rnode_pos);
         if (!rnode)
             return NGL_ERROR_MEMORY;
         ctx->rnode_pos = rnode;
 
-        struct ngl_node *child = s->children[i];
+        struct ngl_node *child = o->children[i];
         ret = ngli_node_prepare(child);
         if (ret < 0)
             goto done;
@@ -65,12 +70,13 @@ static void group_draw(struct ngl_node *node)
 {
     struct ngl_ctx *ctx = node->ctx;
     struct group_priv *s = node->priv_data;
+    const struct group_opts *o = &s->opts;
 
     struct rnode *rnode_pos = ctx->rnode_pos;
     struct rnode *rnodes = ngli_darray_data(&rnode_pos->children);
-    for (int i = 0; i < s->nb_children; i++) {
+    for (int i = 0; i < o->nb_children; i++) {
         ctx->rnode_pos = &rnodes[i];
-        struct ngl_node *child = s->children[i];
+        struct ngl_node *child = o->children[i];
         ngli_node_draw(child);
     }
     ctx->rnode_pos = rnode_pos;
