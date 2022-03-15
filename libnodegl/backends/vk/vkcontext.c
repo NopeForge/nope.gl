rlavarec-gpsw/gopro-lib-node.gl

nodegl: add MoltenVK support

libnodegl/backends/vk/vkcontext.c
@@ -28,6 +28,10 @@
 # define VK_USE_PLATFORM_ANDROID_KHR
 #elif defined(TARGET_WINDOWS)
 # define VK_USE_PLATFORM_WIN32_KHR
+#elif defined(TARGET_DARWIN)
+# define VK_USE_PLATFORM_MACOS_MVK
+#elif defined(TARGET_IPHONE)
+# define VK_USE_PLATFORM_IOS_MVK
 #endif
 
 #include <string.h>
@@ -73,6 +77,8 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debug_callback(VkDebugUtilsMessageSeverity
 static const char *platform_ext_names[] = {
     [NGL_PLATFORM_XLIB]    = "VK_KHR_xlib_surface",
     [NGL_PLATFORM_ANDROID] = "VK_KHR_android_surface",
+    [NGL_PLATFORM_MACOS]   = "VK_MVK_macos_surface",
+    [NGL_PLATFORM_IOS]     = "VK_MVK_ios_surface",
     [NGL_PLATFORM_WINDOWS] = "VK_KHR_win32_surface",
     [NGL_PLATFORM_WAYLAND] = "VK_KHR_wayland_surface",
 };
@@ -153,6 +159,10 @@ static VkResult create_instance(struct vkcontext *s, int platform)
     const char *mandatory_extensions[] = {
         VK_KHR_SURFACE_EXTENSION_NAME,
         surface_extension_name,
+#ifdef VK_USE_PLATFORM_MACOS_MVK
+        "VK_MVK_moltenvk",
+        "VK_EXT_metal_surface",
+#endif
     };
 
     struct darray extensions;
@@ -306,6 +316,38 @@ static VkResult create_window_surface(struct vkcontext *s, const struct ngl_conf
             return res;
 #else
         return VK_ERROR_EXTENSION_NOT_PRESENT;
+#endif
+    } else if (platform == NGL_PLATFORM_MACOS) {
+#if defined(TARGET_DARWIN)
+        const VkMacOSSurfaceCreateInfoMVK surface_create_info = {
+            .sType = VK_STRUCTURE_TYPE_MACOS_SURFACE_CREATE_INFO_MVK,
+            .pView = (const void *)config->window,
+        };
+
+        VK_LOAD_FUN(s->instance, CreateMacOSSurfaceMVK);
+        if (!CreateMacOSSurfaceMVK) {
+            return VK_ERROR_EXTENSION_NOT_PRESENT;
+        }
+
+        VkResult res = CreateMacOSSurfaceMVK(s->instance, &surface_create_info, NULL, &s->surface);
+        if (res != VK_SUCCESS)
+            return res;
+#endif
+    } else if (platform == NGL_PLATFORM_IOS) {
+#if defined(TARGET_IPHONE)
+        const VkIOSSurfaceCreateInfoMVK surface_create_info = {
+            .sType = VK_STRUCTURE_TYPE_IOS_SURFACE_CREATE_INFO_MVK,
+            .pView = (const void *)config->window,
+        };
+
+        VK_LOAD_FUN(s->instance, CreateIOSSurfaceMVK);
+        if (!CreateIOSSurfaceMVK) {
+            return VK_ERROR_EXTENSION_NOT_PRESENT;
+        }
+
+        VkResult res = CreateIOSSurfaceMVK(s->instance, &surface_create_info, NULL, &s->surface);
+        if (res != VK_SUCCESS)
+            return res;
 #endif
     } else if (platform == NGL_PLATFORM_WINDOWS) {
 #if defined(TARGET_WINDOWS)

NopeForge/nope.gl

Signed-off-by: Clément Bœsch <u@pkh.me>

libnodegl/backends/vk/vkcontext.c
@@ -28,6 +28,10 @@
 # define VK_USE_PLATFORM_ANDROID_KHR
 #elif defined(TARGET_WINDOWS)
 # define VK_USE_PLATFORM_WIN32_KHR
+#elif defined(TARGET_DARWIN)
+# define VK_USE_PLATFORM_MACOS_MVK
+#elif defined(TARGET_IPHONE)
+# define VK_USE_PLATFORM_IOS_MVK
 #endif
 
 #include <string.h>
@@ -88,6 +92,8 @@ static int has_layer(struct vkcontext *s, const char *name)
 static const char *platform_ext_names[] = {
     [NGL_PLATFORM_XLIB]    = "VK_KHR_xlib_surface",
     [NGL_PLATFORM_ANDROID] = "VK_KHR_android_surface",
+    [NGL_PLATFORM_MACOS]   = "VK_MVK_macos_surface",
+    [NGL_PLATFORM_IOS]     = "VK_MVK_ios_surface",
     [NGL_PLATFORM_WINDOWS] = "VK_KHR_win32_surface",
     [NGL_PLATFORM_WAYLAND] = "VK_KHR_wayland_surface",
 };
@@ -168,6 +174,10 @@ static VkResult create_instance(struct vkcontext *s, int platform)
     const char *mandatory_extensions[] = {
         VK_KHR_SURFACE_EXTENSION_NAME,
         surface_extension_name,
+#ifdef VK_USE_PLATFORM_MACOS_MVK
+        "VK_MVK_moltenvk",
+        "VK_EXT_metal_surface",
+#endif
     };
 
     struct darray extensions;
@@ -309,6 +319,38 @@ static VkResult create_window_surface(struct vkcontext *s, const struct ngl_conf
             return res;
 #else
         return VK_ERROR_EXTENSION_NOT_PRESENT;
+#endif
+    } else if (platform == NGL_PLATFORM_MACOS) {
+#if defined(TARGET_DARWIN)
+        const VkMacOSSurfaceCreateInfoMVK surface_create_info = {
+            .sType = VK_STRUCTURE_TYPE_MACOS_SURFACE_CREATE_INFO_MVK,
+            .pView = (const void *)config->window,
+        };
+
+        VK_LOAD_FUNC(s->instance, CreateMacOSSurfaceMVK);
+        if (!CreateMacOSSurfaceMVK) {
+            return VK_ERROR_EXTENSION_NOT_PRESENT;
+        }
+
+        VkResult res = CreateMacOSSurfaceMVK(s->instance, &surface_create_info, NULL, &s->surface);
+        if (res != VK_SUCCESS)
+            return res;
+#endif
+    } else if (platform == NGL_PLATFORM_IOS) {
+#if defined(TARGET_IPHONE)
+        const VkIOSSurfaceCreateInfoMVK surface_create_info = {
+            .sType = VK_STRUCTURE_TYPE_IOS_SURFACE_CREATE_INFO_MVK,
+            .pView = (const void *)config->window,
+        };
+
+        VK_LOAD_FUN(s->instance, CreateIOSSurfaceMVK);
+        if (!CreateIOSSurfaceMVK) {
+            return VK_ERROR_EXTENSION_NOT_PRESENT;
+        }
+
+        VkResult res = CreateIOSSurfaceMVK(s->instance, &surface_create_info, NULL, &s->surface);
+        if (res != VK_SUCCESS)
+            return res;
 #endif
     } else if (platform == NGL_PLATFORM_WINDOWS) {
 #if defined(TARGET_WINDOWS)