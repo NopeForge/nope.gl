NopeForge/nope.gl

commit 6c6d74f860638a9fcc5c8117022d7743a4ae6170

nodegl: re-introduce external OpenGL context support

libnodegl/backends/gl/glcontext_wgl.c
@@ -240,6 +240,29 @@ static int wgl_init(struct glcontext *ctx, uintptr_t display, uintptr_t window, 
     return 0;
 }
 
+static int wgl_init_external(struct glcontext *ctx, uintptr_t display, uintptr_t window, uintptr_t other)
+{
+    struct wgl_priv *wgl = ctx->priv_data;
+
+    wgl->module = LoadLibrary("opengl32.dll");
+    if (!wgl->module) {
+        LOG(ERROR, "could not load opengl32.dll (%lu)", GetLastError());
+        return -1;
+    }
+
+    wgl->device_context = wglGetCurrentDC();
+    if (!wgl->device_context) {
+        LOG(ERROR, "could not retrieve current device context");
+        return -1;
+    }
+
+    wgl->rendering_context = wglGetCurrentContext();
+    if (!wgl->rendering_context) {
+        LOG(ERROR, "could not retrieve current rendering context");
+        return -1;
+    }
+}
+
 static void wgl_uninit(struct glcontext *ctx)
 {
     struct wgl_priv *wgl = ctx->priv_data;
@@ -249,6 +272,14 @@ static void wgl_uninit(struct glcontext *ctx)
 
     if (ctx->offscreen && wgl->window)
         DestroyWindow(wgl->window);
+
+    if (wgl->module)
+        FreeLibrary(wgl->module);
+}
+
+static void wgl_uninit_external(struct glcontext *ctx)
+{
+    struct wgl_priv *wgl = ctx->priv_data;
 
     if (wgl->module)
         FreeLibrary(wgl->module);
@@ -325,6 +356,15 @@ const struct glcontext_class ngli_glcontext_wgl_class = {
     .make_current = wgl_make_current,
     .swap_buffers = wgl_swap_buffers,
     .set_swap_interval = wgl_set_swap_interval,
+    .get_proc_address = wgl_get_proc_address,
+    .get_handle = wgl_get_handle,
+    .priv_size = sizeof(struct wgl_priv),
+};
+
+const struct glcontext_class ngli_glcontext_wgl_external_class = {
+    .init = wgl_init_external,
+    .uninit = wgl_uninit_external,
+    .make_current = wgl_make_current,
     .get_proc_address = wgl_get_proc_address,
     .get_handle = wgl_get_handle,
     .priv_size = sizeof(struct wgl_priv),

libnodegl/api.c
@@ -621,6 +621,12 @@ int ngl_configure(struct ngl_ctx *s, struct ngl_config *config)
         return NGL_ERROR_INVALID_ARG;
     }
 
+    if (config->backend == NGL_BACKEND_AUTO && config->backend_config) {
+        LOG(ERROR, "backend specific configuration is not allowed "
+                   "while automatic backend selection is used");
+        return NGL_ERROR_INVALID_USAGE;
+    }
+
     if (config->backend == NGL_BACKEND_AUTO)
         config->backend = DEFAULT_BACKEND;
     if (config->platform == NGL_PLATFORM_AUTO)
@@ -705,6 +711,26 @@ int ngl_draw(struct ngl_ctx *s, double t)
 
     return s->api_impl->draw(s, t);
 }
+
+int ngl_gl_wrap_framebuffer(struct ngl_ctx *s, uint32_t framebuffer)
+{
+    if (!s->configured) {
+        LOG(ERROR, "context must be configured before wrapping a new external OpenGL framebuffer");
+        return NGL_ERROR_INVALID_USAGE;
+    }
+
+    if (!s->api_impl->gl_wrap_framebuffer) {
+        LOG(ERROR, "wrapping external OpenGL framebuffer is not supported by context");
+        return NGL_ERROR_UNSUPPORTED;
+    }
+
+    int ret = s->api_impl->gl_wrap_framebuffer(s, framebuffer);
+    if (ret < 0) {
+        s->configured = 0;
+        return ret;
+    }
+    return 0;
+ }
 
 int ngl_livectls_get(struct ngl_node *scene, int *nb_livectlsp, struct ngl_livectl **livectlsp)
 {

libnodegl/backends/gl/api_gl.c
@@ -23,6 +23,7 @@
 
 #include "backends/gl/gpu_ctx_gl.h"
 #include "internal.h"
+#include "log.h"
 
 static int cmd_make_current(struct ngl_ctx *s, void *arg)
 {
@@ -54,6 +55,17 @@ static int gl_configure(struct ngl_ctx *s, const struct ngl_config *config)
     }
 
     return ngli_ctx_dispatch_cmd(s, cmd_configure, (void *)config);
+}
+
+static int glw_configure(struct ngl_ctx *s, const struct ngl_config *config)
+{
+    int ret = ngli_ctx_configure(s, config);
+    if (ret < 0)
+        return ret;
+
+    ngli_gpu_ctx_gl_reset_state(s->gpu_ctx);
+
+    return 0;
 }
 
 struct resize_params {
@@ -94,6 +106,11 @@ static int gl_resize(struct ngl_ctx *s, int width, int height, const int *viewpo
     return ngli_ctx_dispatch_cmd(s, cmd_resize, &params);
 }
 
+static int glw_resize(struct ngl_ctx *s, int width, int height, const int *viewport)
+{
+    return ngli_ctx_resize(s, width, height, viewport);
+}
+
 static int cmd_set_capture_buffer(struct ngl_ctx *s, void *capture_buffer)
 {
     return ngli_ctx_set_capture_buffer(s, capture_buffer);
@@ -102,6 +119,12 @@ static int cmd_set_capture_buffer(struct ngl_ctx *s, void *capture_buffer)
 static int gl_set_capture_buffer(struct ngl_ctx *s, void *capture_buffer)
 {
     return ngli_ctx_dispatch_cmd(s, cmd_set_capture_buffer, capture_buffer);
+}
+
+static int glw_set_capture_buffer(struct ngl_ctx *s, void *capture_buffer)
+{
+    LOG(ERROR, "capture_buffer is not supported by external OpenGL context");
+    return NGL_ERROR_UNSUPPORTED;
 }
 
 static int cmd_set_scene(struct ngl_ctx *s, void *arg)
@@ -115,6 +138,14 @@ static int gl_set_scene(struct ngl_ctx *s, struct ngl_node *node)
     return ngli_ctx_dispatch_cmd(s, cmd_set_scene, node);
 }
 
+static int glw_set_scene(struct ngl_ctx *s, struct ngl_node *node)
+{
+    ngli_gpu_ctx_gl_reset_state(s->gpu_ctx);
+    int ret = ngli_ctx_set_scene(s, node);
+    ngli_gpu_ctx_gl_reset_state(s->gpu_ctx);
+    return ret;
+}
+
 static int cmd_prepare_draw(struct ngl_ctx *s, void *arg)
 {
     const double t = *(double *)arg;
@@ -126,6 +157,14 @@ static int gl_prepare_draw(struct ngl_ctx *s, double t)
     return ngli_ctx_dispatch_cmd(s, cmd_prepare_draw, &t);
 }
 
+static int glw_prepare_draw(struct ngl_ctx *s, double t)
+{
+    ngli_gpu_ctx_gl_reset_state(s->gpu_ctx);
+    int ret = ngli_ctx_prepare_draw(s, t);
+    ngli_gpu_ctx_gl_reset_state(s->gpu_ctx);
+    return ret;
+}
+
 static int cmd_draw(struct ngl_ctx *s, void *arg)
 {
     const double t = *(double *)arg;
@@ -135,6 +174,14 @@ static int cmd_draw(struct ngl_ctx *s, void *arg)
 static int gl_draw(struct ngl_ctx *s, double t)
 {
     return ngli_ctx_dispatch_cmd(s, cmd_draw, &t);
+}
+
+static int glw_draw(struct ngl_ctx *s, double t)
+{
+    ngli_gpu_ctx_gl_reset_state(s->gpu_ctx);
+    int ret = ngli_ctx_draw(s, t);
+    ngli_gpu_ctx_gl_reset_state(s->gpu_ctx);
+    return ret;
 }
 
 static int cmd_reset(struct ngl_ctx *s, void *arg)
@@ -149,12 +196,85 @@ static void gl_reset(struct ngl_ctx *s, int action)
     ngli_ctx_dispatch_cmd(s, cmd_reset, &action);
 }
 
+static void glw_reset(struct ngl_ctx *s, int action)
+{
+    ngli_ctx_reset(s, action);
+}
+
+static int gl_wrap_framebuffer(struct ngl_ctx *s, uint32_t framebuffer)
+{
+    LOG(ERROR, "wrapping external OpenGL framebuffer is not supported by context");
+    return NGL_ERROR_UNSUPPORTED;
+}
+
+static int glw_wrap_framebuffer(struct ngl_ctx *s, uint32_t framebuffer)
+{
+    int ret = ngli_gpu_ctx_gl_wrap_framebuffer(s->gpu_ctx, framebuffer);
+    if (ret < 0) {
+        ngli_ctx_reset(s, NGLI_ACTION_KEEP_SCENE);
+        return ret;
+    }
+
+    struct ngl_config *config = &s->config;
+    struct ngl_config_gl *config_gl = config->backend_config;
+    config_gl->external_framebuffer = framebuffer;
+
+    return 0;
+}
+
+static int is_glw(const struct ngl_config *config)
+{
+    const struct ngl_config_gl *config_gl = config->backend_config;
+    return config_gl && config_gl->external;
+}
+
+static int glv_configure(struct ngl_ctx *s, const struct ngl_config *config)
+{
+    return is_glw(config) ? glw_configure(s, config) : gl_configure(s, config);
+}
+
+static int glv_resize(struct ngl_ctx *s, int width, int height, const int *viewport)
+{
+    return is_glw(&s->config) ? glw_resize(s, width, height, viewport) : gl_resize(s, width, height, viewport);
+}
+
+static int glv_set_capture_buffer(struct ngl_ctx *s, void *capture_buffer)
+{
+    return is_glw(&s->config) ? glw_set_capture_buffer(s, capture_buffer) : gl_set_capture_buffer(s, capture_buffer);
+}
+
+static int glv_set_scene(struct ngl_ctx *s, struct ngl_node *node)
+{
+    return is_glw(&s->config) ? glw_set_scene(s, node) : gl_set_scene(s, node);
+}
+
+static int glv_prepare_draw(struct ngl_ctx *s, double t)
+{
+    return is_glw(&s->config) ? glw_prepare_draw(s, t) : gl_prepare_draw(s, t);
+}
+
+static int glv_draw(struct ngl_ctx *s, double t)
+{
+    return is_glw(&s->config) ? glw_draw(s, t) : gl_draw(s, t);
+}
+
+static void glv_reset(struct ngl_ctx *s, int action)
+{
+    is_glw(&s->config) ? glw_reset(s, action) : gl_reset(s, action);
+}
+
+static int glv_wrap_framebuffer(struct ngl_ctx *s, uint32_t framebuffer)
+{
+    return is_glw(&s->config) ? glw_wrap_framebuffer(s, framebuffer) : gl_wrap_framebuffer(s, framebuffer);
+}
+
 const struct api_impl api_gl = {
-    .configure          = gl_configure,
-    .resize             = gl_resize,
-    .set_capture_buffer = gl_set_capture_buffer,
-    .set_scene          = gl_set_scene,
-    .prepare_draw       = gl_prepare_draw,
-    .draw               = gl_draw,
-    .reset              = gl_reset,
+    .configure           = glv_configure,
+    .resize              = glv_resize,
+    .set_capture_buffer  = glv_set_capture_buffer,
+    .set_scene           = glv_set_scene,
+    .prepare_draw        = glv_prepare_draw,
+    .draw                = glv_draw,
+    .reset               = glv_reset,
+    .gl_wrap_framebuffer = glv_wrap_framebuffer,
 };

libnodegl/backends/gl/glcontext.c
@@ -54,32 +54,51 @@ enum {
 
 #ifdef HAVE_GLPLATFORM_EGL
 extern const struct glcontext_class ngli_glcontext_egl_class;
+extern const struct glcontext_class ngli_glcontext_egl_external_class;
 #endif
 
 #ifdef HAVE_GLPLATFORM_NSGL
 extern const struct glcontext_class ngli_glcontext_nsgl_class;
+extern const struct glcontext_class ngli_glcontext_nsgl_external_class;
 #endif
 
 #ifdef HAVE_GLPLATFORM_EAGL
 extern const struct glcontext_class ngli_glcontext_eagl_class;
+extern const struct glcontext_class ngli_glcontext_eagl_external_class;
 #endif
 
 #ifdef HAVE_GLPLATFORM_WGL
 extern const struct glcontext_class ngli_glcontext_wgl_class;
+extern const struct glcontext_class ngli_glcontext_wgl_external_class;
 #endif
 
-static const struct glcontext_class *glcontext_class_map[] = {
+static const struct {
+    const struct glcontext_class *cls;
+    const struct glcontext_class *external_cls;
+} glcontext_class_map[] = {
 #ifdef HAVE_GLPLATFORM_EGL
-    [GLPLATFORM_EGL] = &ngli_glcontext_egl_class,
+    [GLPLATFORM_EGL] = {
+        .cls = &ngli_glcontext_egl_class,
+        .external_cls = &ngli_glcontext_egl_external_class,
+    },
 #endif
 #ifdef HAVE_GLPLATFORM_NSGL
-    [GLPLATFORM_NSGL] = &ngli_glcontext_nsgl_class,
+    [GLPLATFORM_NSGL] = {
+        .cls = &ngli_glcontext_nsgl_class,
+        .external_cls = &ngli_glcontext_nsgl_external_class,
+    },
 #endif
 #ifdef HAVE_GLPLATFORM_EAGL
-    [GLPLATFORM_EAGL] = &ngli_glcontext_eagl_class,
+    [GLPLATFORM_EAGL] = {
+        .cls = &ngli_glcontext_eagl_class,
+        .external_cls = &ngli_glcontext_eagl_external_class,
+    },
 #endif
 #ifdef HAVE_GLPLATFORM_WGL
-    [GLPLATFORM_WGL] = &ngli_glcontext_wgl_class,
+    [GLPLATFORM_WGL] = {
+        .cls = &ngli_glcontext_wgl_class,
+        .external_cls = &ngli_glcontext_wgl_external_class,
+    },
 #endif
 };
 
@@ -429,7 +448,11 @@ struct glcontext *ngli_glcontext_new(const struct glcontext_params *params)
     struct glcontext *glcontext = ngli_calloc(1, sizeof(*glcontext));
     if (!glcontext)
         return NULL;
-    glcontext->cls = glcontext_class_map[glplatform];
+    if (params->external) {
+        glcontext->cls = glcontext_class_map[glplatform].external_cls;
+    } else {
+        glcontext->cls = glcontext_class_map[glplatform].cls;
+    }
 
     if (glcontext->cls->priv_size) {
         glcontext->priv_data = ngli_calloc(1, glcontext->cls->priv_size);
@@ -441,6 +464,7 @@ struct glcontext *ngli_glcontext_new(const struct glcontext_params *params)
 
     glcontext->platform = params->platform;
     glcontext->backend = params->backend;
+    glcontext->external = params->external;
     glcontext->offscreen = params->offscreen;
     glcontext->width = params->width;
     glcontext->height = params->height;
@@ -464,13 +488,13 @@ struct glcontext *ngli_glcontext_new(const struct glcontext_params *params)
         (glcontext->features & NGLI_FEATURE_GL_TEXTURE_CUBE_MAP))
         ngli_glEnable(glcontext, GL_TEXTURE_CUBE_MAP_SEAMLESS);
 
-    if (!glcontext->offscreen) {
+    if (!glcontext->external && !glcontext->offscreen) {
         int ret = ngli_glcontext_resize(glcontext, glcontext->width, glcontext->height);
         if (ret < 0)
             goto fail;
     }
 
-    if (params->swap_interval >= 0)
+    if (!params->external && params->swap_interval >= 0)
         ngli_glcontext_set_swap_interval(glcontext, params->swap_interval);
 
     return glcontext;
@@ -511,6 +535,11 @@ int ngli_glcontext_resize(struct glcontext *glcontext, int width, int height)
 {
     if (glcontext->offscreen) {
         LOG(ERROR, "offscreen context does not support resize operation");
+        return NGL_ERROR_INVALID_USAGE;
+    }
+
+    if (glcontext->external) {
+        LOG(ERROR, "external context does not support resize operation");
         return NGL_ERROR_INVALID_USAGE;
     }
 

libnodegl/backends/gl/glcontext.h
@@ -34,6 +34,7 @@ struct glcontext_class;
 struct glcontext_params {
     int platform;
     int backend;
+    int external;
     uintptr_t display;
     uintptr_t window;
     uintptr_t shared_ctx;
@@ -52,6 +53,7 @@ struct glcontext {
     /* User options */
     int platform;
     int backend;
+    int external;
     int offscreen;
     int width;
     int height;

libnodegl/backends/gl/glcontext_eagl.m
@@ -128,6 +128,46 @@ static int eagl_init(struct glcontext *ctx, uintptr_t display, uintptr_t window,
     return 0;
 }
 
+static int eagl_init_external(struct glcontext *ctx, uintptr_t display, uintptr_t window, uintptr_t other)
+{
+    struct eagl_priv *eagl = ctx->priv_data;
+
+    if (ctx->backend != NGL_BACKEND_OPENGLES) {
+        LOG(ERROR, "unsupported backend: %d, only OpenGLES is supported by EAGL", ctx->backend);
+        return -1;
+    }
+
+    CFBundleRef framework = CFBundleGetBundleWithIdentifier(CFSTR("com.apple.opengles"));
+    if (!framework) {
+        LOG(ERROR, "could not retrieve OpenGLES framework");
+        return -1;
+    }
+
+    eagl->framework = (CFBundleRef)CFRetain(framework);
+    if (!eagl->framework) {
+        LOG(ERROR, "could not retain OpenGL framework object");
+        return -1;
+    }
+
+    eagl->handle = [EAGLContext currentContext];
+    if (!eagl->handle) {
+        LOG(ERROR, "could not retrieve EAGL context");
+        return -1;
+    }
+
+    CVReturn err = CVOpenGLESTextureCacheCreate(kCFAllocatorDefault,
+                                                NULL,
+                                                eagl->handle,
+                                                NULL,
+                                                &eagl->texture_cache);
+    if (err != noErr) {
+        LOG(ERROR, "could not create CoreVideo texture cache: 0x%x", err);
+        return -1;
+    }
+
+    return 0;
+}
+
 static int eagl_init_framebuffer(struct glcontext *ctx)
 {
     struct eagl_priv *eagl = ctx->priv_data;
@@ -213,6 +253,17 @@ static void eagl_uninit(struct glcontext *ctx)
 
     if (eagl->handle)
         CFRelease(eagl->handle);
+}
+
+static void eagl_uninit_external(struct glcontext *ctx)
+{
+    struct eagl_priv *eagl = ctx->priv_data;
+
+    if (eagl->framework)
+        CFRelease(eagl->framework);
+
+    if (eagl->texture_cache)
+        CFRelease(eagl->texture_cache);
 }
 
 static int eagl_resize(struct glcontext *ctx, int width, int height)
@@ -303,5 +354,15 @@ const struct glcontext_class ngli_glcontext_eagl_class = {
     .get_proc_address = eagl_get_proc_address,
     .get_handle = eagl_get_handle,
     .get_default_framebuffer = eagl_get_default_framebuffer,
+    .priv_size = sizeof(struct eagl_priv),
+};
+
+const struct glcontext_class ngli_glcontext_eagl_external_class = {
+    .init = eagl_init_external,
+    .uninit = eagl_uninit_external,
+    .make_current = eagl_make_current,
+    .get_texture_cache = eagl_get_texture_cache,
+    .get_proc_address = eagl_get_proc_address,
+    .get_handle = eagl_get_handle,
     .priv_size = sizeof(struct eagl_priv),
 };

libnodegl/backends/gl/glcontext_egl.c
@@ -396,6 +396,37 @@ try_again:;
     return 0;
 }
 
+static int egl_init_external(struct glcontext *ctx, uintptr_t display, uintptr_t window, uintptr_t other)
+{
+    struct egl_priv *egl = ctx->priv_data;
+
+    egl->handle = other ? (EGLContext)other : eglGetCurrentContext();
+    if (!egl->handle) {
+        LOG(ERROR, "could not retrieve EGL context");
+        return -1;
+    }
+
+    egl->display = eglGetCurrentDisplay();
+    if (!egl->display) {
+        LOG(ERROR, "could not retrieve EGL display");
+        return -1;
+    }
+
+    egl->surface = eglGetCurrentSurface(EGL_DRAW);
+
+    egl->extensions = eglQueryString(egl->display, EGL_EXTENSIONS);
+    if (!egl->extensions) {
+        LOG(ERROR, "could not retrieve EGL extensions");
+        return -1;
+    }
+
+    int ret = egl_probe_extensions(ctx);
+    if (ret < 0)
+        return ret;
+
+    return 0;
+}
+
 static void egl_uninit(struct glcontext *ctx)
 {
     struct egl_priv *egl = ctx->priv_data;
@@ -530,6 +561,15 @@ const struct glcontext_class ngli_glcontext_egl_class = {
     .swap_buffers = egl_swap_buffers,
     .set_swap_interval = egl_set_swap_interval,
     .set_surface_pts = egl_set_surface_pts,
+    .get_proc_address = egl_get_proc_address,
+    .get_handle = egl_get_handle,
+    .get_display = get_display,
+    .priv_size = sizeof(struct egl_priv),
+};
+
+const struct glcontext_class ngli_glcontext_egl_external_class = {
+    .init = egl_init_external,
+    .make_current = egl_make_current,
     .get_proc_address = egl_get_proc_address,
     .get_handle = egl_get_handle,
     .get_display = get_display,

libnodegl/backends/gl/glcontext_nsgl.m
@@ -118,6 +118,36 @@ static int nsgl_init(struct glcontext *ctx, uintptr_t display, uintptr_t window,
     return 0;
 }
 
+static int nsgl_init_external(struct glcontext *ctx, uintptr_t display, uintptr_t window, uintptr_t other)
+{
+    struct nsgl_priv *nsgl = ctx->priv_data;
+
+    if (ctx->backend != NGL_BACKEND_OPENGL) {
+        LOG(ERROR, "unsupported backend: %d, only OpenGL is supported by NSGL", ctx->backend);
+        return -1;
+    }
+
+    CFBundleRef framework = CFBundleGetBundleWithIdentifier(CFSTR("com.apple.opengl"));
+    if (!framework) {
+        LOG(ERROR, "could not retrieve OpenGL framework");
+        return -1;
+    }
+
+    nsgl->framework = (CFBundleRef)CFRetain(framework);
+    if (!nsgl->framework) {
+        LOG(ERROR, "could not retain OpenGL framework object");
+        return -1;
+    }
+
+    nsgl->handle = [NSOpenGLContext currentContext];
+    if (!nsgl->handle) {
+        LOG(ERROR, "could not retrieve NSGL context");
+        return -1;
+    }
+
+    return 0;
+}
+
 static int nsgl_resize(struct glcontext *ctx, int width, int height)
 {
     struct nsgl_priv *nsgl = ctx->priv_data;
@@ -201,6 +231,14 @@ static void nsgl_uninit(struct glcontext *ctx)
         CFRelease(nsgl->pixel_format);
 }
 
+static void nsgl_uninit_external(struct glcontext *ctx)
+{
+    struct nsgl_priv *nsgl = ctx->priv_data;
+
+    if (nsgl->framework)
+        CFRelease(nsgl->framework);
+}
+
 const struct glcontext_class ngli_glcontext_nsgl_class = {
     .init = nsgl_init,
     .uninit = nsgl_uninit,
@@ -208,6 +246,15 @@ const struct glcontext_class ngli_glcontext_nsgl_class = {
     .make_current = nsgl_make_current,
     .swap_buffers = nsgl_swap_buffers,
     .set_swap_interval = nsgl_set_swap_interval,
+    .get_proc_address = nsgl_get_proc_address,
+    .get_handle = nsgl_get_handle,
+    .priv_size = sizeof(struct nsgl_priv),
+};
+
+const struct glcontext_class ngli_glcontext_nsgl_external_class = {
+    .init = nsgl_init_external,
+    .uninit = nsgl_uninit_external,
+    .make_current = nsgl_make_current,
     .get_proc_address = nsgl_get_proc_address,
     .get_handle = nsgl_get_handle,
     .priv_size = sizeof(struct nsgl_priv),

commit f27faead9d143d40cc992ffcb37443e4ef0d8c92

glcontext_wgl: add missing return
Regression since 6c6d74f860638a9fcc5c8117022d7743a4ae6170.

libnodegl/backends/gl/glcontext_wgl.c
@@ -261,6 +261,8 @@ static int wgl_init_external(struct glcontext *ctx, uintptr_t display, uintptr_t
         LOG(ERROR, "could not retrieve current rendering context");
         return -1;
     }
+
+    return 0;
 }
 
 static void wgl_uninit(struct glcontext *ctx)