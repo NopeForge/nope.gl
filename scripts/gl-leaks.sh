3667577bf573fdb2119502dbe7c7bb121dd1d45f
Clément Bœsch <u@pkh.me>
2022-06-21 16:52:47 +0200

    scripts/gl-leaks: sanitize script following shellcheck recommendations

--- scripts/gl-leaks.sh
+++ scripts/gl-leaks.sh
@@ -3,11 +3,11 @@
 set -ue
 
 tmpfile=$(mktemp --suffix _ngl.trace)
-trap "rm -f $tmpfile" EXIT
-apitrace trace -a egl -o $tmpfile $@
+trap 'rm -f "$tmpfile"' EXIT
+apitrace trace -a egl -o "$tmpfile" "$@"
 
 # executable doesn't exit in error so we look for "errors" in stderr
-leaks=$(apitrace leaks $tmpfile 2>&1 > /dev/null)
+leaks=$(apitrace leaks "$tmpfile" 2>&1 > /dev/null)
 if echo "$leaks" | grep -q "error:"; then
     echo "GPU leaks detected: YES"
     echo "$leaks"
