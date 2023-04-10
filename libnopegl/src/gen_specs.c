96fdfa323adb3ec479b59ff316e0ff5041aec49c
Clément Bœsch <u@pkh.me>
2023-04-10 15:54:53 +0200

    gen_specs: make the specs much more detailed

--- libnopegl/src/gen_specs.c
+++ libnopegl/src/gen_specs.c
@@ -19,11 +19,14 @@
  * under the License.
  */
 
+#include <string.h>
+
 #include "hmap.h"
 #include "memory.h"
 #include "nopegl.h"
 #include "internal.h"
 #include "nodes_register.h"
+#include "params.h"
 
 #define CLASS_LIST(type_name, cls) extern const struct node_class cls;
 NODE_MAP_TYPE2CLASS(CLASS_LIST)
@@ -31,17 +34,143 @@ NODE_MAP_TYPE2CLASS(CLASS_LIST)
 extern const struct node_param ngli_base_node_params[];
 extern const struct param_specs ngli_params_specs[];
 
+#define REGISTER_NODE(type_name, cls)           \
+    case type_name: {                           \
+        extern const struct node_class cls;     \
+        ngli_assert(cls.id == type_name);       \
+        return &cls;                            \
+    }                                           \
+
+static const struct node_class *get_node_class(int type)
+{
+    switch (type) {
+        NODE_MAP_TYPE2CLASS(REGISTER_NODE)
+    }
+    return NULL;
+}
+
+static char *escape_json_str(const char *s)
+{
+    if (!s)
+        return NULL;
+    char *p = malloc(strlen(s) * 2 + 1);
+    if (!p)
+        abort();
+    char *ret = p;
+    while (*s) {
+        if (*s == '"')
+            *p++ = '\\';
+        *p++ = *s++;
+    }
+    *p = 0;
+    return ret;
+}
+
+#define I "      " /* keys indent */
+#define D I "\"default\": " /* default key */
+
 static void print_node_params(const char *name, const struct node_param *p)
 {
     printf("  \"%s\": [\n", name);
     if (p) {
         while (p->key) {
-            printf("    [\"%s\", \"%s\", \"%s%s%s\"]%s\n", p->key, ngli_params_specs[p->type].name,
-                   (p->flags & NGLI_PARAM_FLAG_ALLOW_LIVE_CHANGE) ? "L" : "",  /* [L]ive */
-                   (p->flags & NGLI_PARAM_FLAG_ALLOW_NODE)        ? "N" : "",  /* [N]ode */
-                   (p->flags & NGLI_PARAM_FLAG_NON_NULL)          ? "M" : "",  /* [M]andatory */
-                   (p + 1)->key ? "," : ""
-            );
+            printf("    {\n");
+            printf(I "\"name\": \"%s\",\n", p->key);
+            printf(I "\"type\": \"%s\",\n", ngli_params_specs[p->type].name);
+            switch (p->type) {
+            case NGLI_PARAM_TYPE_SELECT: {
+                const int v = (int)p->def_value.i32;
+                const char *s = ngli_params_get_select_str(p->choices->consts, v);
+                ngli_assert(s);
+                printf(D "\"%s\",\n", s);
+                printf(I "\"choices\": [");
+                for (int i = 0; p->choices->consts[i].key; i++)
+                    printf("%s\"%s\"", i ? ", " : "", p->choices->consts[i].key);
+                printf("],\n");
+                break;
+            }
+            case NGLI_PARAM_TYPE_FLAGS: {
+                const int v = (int)p->def_value.i32;
+                char *s = ngli_params_get_flags_str(p->choices->consts, v);
+                if (!s)
+                    abort();
+                ngli_assert(*s);
+                printf(D "\"%s\",\n", s);
+                ngli_free(s);
+                printf(I "\"choices\": [");
+                for (int i = 0; p->choices->consts[i].key; i++)
+                    printf("%s\"%s\"", i ? ", " : "", p->choices->consts[i].key);
+                printf("],\n");
+                break;
+            }
+            case NGLI_PARAM_TYPE_F32:
+                printf(D "%f,\n", p->def_value.f32);
+                break;
+            case NGLI_PARAM_TYPE_F64:
+                printf(D "%f,\n", p->def_value.f64);
+                break;
+            case NGLI_PARAM_TYPE_BOOL:
+                if (p->def_value.i32 < 0)
+                    printf(D "\"unset\",\n");
+                else
+                    printf(D "%d,\n", p->def_value.i32);
+                break;
+            case NGLI_PARAM_TYPE_I32:
+                printf(D "%d,\n", p->def_value.i32);
+                break;
+            case NGLI_PARAM_TYPE_U32:
+                printf(D "%u,\n", p->def_value.u32);
+                break;
+            case NGLI_PARAM_TYPE_IVEC2: printf(D "[%d,%d],\n",       NGLI_ARG_VEC2(p->def_value.ivec)); break;
+            case NGLI_PARAM_TYPE_IVEC3: printf(D "[%d,%d,%d],\n",    NGLI_ARG_VEC3(p->def_value.ivec)); break;
+            case NGLI_PARAM_TYPE_IVEC4: printf(D "[%d,%d,%d,%d],\n", NGLI_ARG_VEC4(p->def_value.ivec)); break;
+            case NGLI_PARAM_TYPE_UVEC2: printf(D "[%u,%u],\n",       NGLI_ARG_VEC2(p->def_value.uvec)); break;
+            case NGLI_PARAM_TYPE_UVEC3: printf(D "[%u,%u,%u],\n",    NGLI_ARG_VEC3(p->def_value.uvec)); break;
+            case NGLI_PARAM_TYPE_UVEC4: printf(D "[%u,%u,%u,%u],\n", NGLI_ARG_VEC4(p->def_value.uvec)); break;
+            case NGLI_PARAM_TYPE_VEC2:  printf(D "[%f,%f],\n",       NGLI_ARG_VEC2(p->def_value.vec));  break;
+            case NGLI_PARAM_TYPE_VEC3:  printf(D "[%f,%f,%f],\n",    NGLI_ARG_VEC3(p->def_value.vec));  break;
+            case NGLI_PARAM_TYPE_VEC4:  printf(D "[%f,%f,%f,%f],\n", NGLI_ARG_VEC4(p->def_value.vec));  break;
+            case NGLI_PARAM_TYPE_MAT4:
+                printf(D "[%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f],\n", NGLI_ARG_MAT4(p->def_value.mat));
+                break;
+            case NGLI_PARAM_TYPE_STR:
+                if (p->def_value.str)
+                    printf(D "\"%s\",\n", p->def_value.str);
+                break;
+            case NGLI_PARAM_TYPE_RATIONAL:
+                printf(D "[%d,%d],\n", NGLI_ARG_VEC2(p->def_value.r));
+                break;
+            }
+            if (p->node_types) {
+                printf(I "\"node_types\": [");
+                for (int i = 0; p->node_types[i] != -1; i++) {
+                    const struct node_class *cls = get_node_class(p->node_types[i]);
+                    ngli_assert(cls);
+                    printf("%s\"%s\"", i ? ", " : "", cls->name);
+                }
+                printf("],\n");
+            }
+            static const struct {
+                const char *name;
+                const int flag;
+            } flag_names[] = {
+                {"live", NGLI_PARAM_FLAG_ALLOW_LIVE_CHANGE},
+                {"node", NGLI_PARAM_FLAG_ALLOW_NODE},
+                {"nonull", NGLI_PARAM_FLAG_NON_NULL},
+            };
+            printf(I "\"flags\": [");
+            int ihasflag = 0;
+            for (int i = 0; i < NGLI_ARRAY_NB(flag_names); i++) {
+                if (!(p->flags & flag_names[i].flag))
+                    continue;
+                printf("%s\"%s\"", ihasflag ? ", " : "", flag_names[i].name);
+                ihasflag = 1;
+            }
+            printf("],\n");
+            char *esc_desc = escape_json_str(p->desc);
+            printf(I "\"desc\": \"%s\"\n", esc_desc ? esc_desc : "");
+            free(esc_desc);
+            printf("    }%s\n", (p + 1)->key ? "," : "");
             p++;
         }
     }
