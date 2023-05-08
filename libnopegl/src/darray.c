NopeForge/nope.gl

commit 609332e822b0c0afb766cef2dd975741c7ecd0c5

hmap: add overflow checks

libnopegl/src/hmap.c
@@ -229,14 +229,22 @@ int ngli_hmap_set(struct hmap *hm, const char *key, void *data)
     if (hm->count * 3 / 4 >= hm->size) {
         struct hmap old_hm = *hm;
 
+#if HAVE_BUILTIN_OVERFLOW
+        size_t new_size;
+        if (__builtin_mul_overflow(hm->size, 2, &new_size))
+            return NGL_ERROR_LIMIT_EXCEEDED;
+#else
+        /* Also includes the realloc overflow check */
         if (hm->size >= 1 << (sizeof(hm->size)*8 - 2))
             return NGL_ERROR_LIMIT_EXCEEDED;
+        size_t new_size = hm->size * 2;
+#endif
 
-        struct bucket *new_buckets = ngli_calloc(hm->size << 1, sizeof(*new_buckets));
+        struct bucket *new_buckets = ngli_calloc(new_size, sizeof(*new_buckets));
         if (new_buckets) {
             hm->buckets = new_buckets;
             hm->count = 0;
-            hm->size <<= 1;
+            hm->size = new_size;
             hm->mask = hm->size - 1;
             hm->first = hm->last = NO_REF;
 

commit fa39cb8c551face838f62e9baea65dbd4c83fec3

hmap, darray: fix invalid shifts
Fixes the following warnings on Windows:
- hmap.c(241,54): warning C4293: '<<': shift count negative or too big,
  undefined behavior
- darray.c(90,70): warning C4293: '<<': shift count negative or too big,
  undefined behavior

Fixes runtime errors on Windows due to regressions introduced by
609332e822b0c0afb766cef2dd975741c7ecd0c5 and
081ace38da7f29871906c7d44db351dd96a97a32.

libnopegl/src/darray.c
@@ -87,7 +87,7 @@ void *ngli_darray_push(struct darray *darray, const void *element)
             return NULL;
 #else
         /* Also includes the realloc overflow check */
-        if (darray->capacity >= 1 << (sizeof(darray->capacity)*8 - 2))
+        if (darray->capacity >= 1ULL << (sizeof(darray->capacity)*8 - 2))
             return NULL;
         size_t new_capacity = darray->capacity * 2;
 #endif

libnopegl/src/hmap.c
@@ -235,7 +235,7 @@ int ngli_hmap_set(struct hmap *hm, const char *key, void *data)
             return NGL_ERROR_LIMIT_EXCEEDED;
 #else
         /* Also includes the realloc overflow check */
-        if (hm->size >= 1 << (sizeof(hm->size)*8 - 2))
+        if (hm->size >= 1ULL << (sizeof(hm->size)*8 - 2))
             return NGL_ERROR_LIMIT_EXCEEDED;
         size_t new_size = hm->size * 2;
 #endif