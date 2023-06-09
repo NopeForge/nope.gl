7b4bd1de4166118243e5b1f2f1a67e15d92ac53c
Matthieu Bouron <matthieu.bouron@gmail.com>
2023-06-09 11:37:26 +0200

    glcontext: cap max_color_attachments to what nope.gl actually supports

--- libnopegl/src/backends/gl/glcontext.c
+++ libnopegl/src/backends/gl/glcontext.c
@@ -349,6 +349,7 @@ static int glcontext_probe_settings(struct glcontext *glcontext)
     GET(GL_MAX_ARRAY_TEXTURE_LAYERS, &limits->max_texture_array_layers);
     GET(GL_MAX_SAMPLES, &limits->max_samples);
     GET(GL_MAX_COLOR_ATTACHMENTS, &limits->max_color_attachments);
+    limits->max_color_attachments = NGLI_MIN(limits->max_color_attachments, NGLI_MAX_COLOR_ATTACHMENTS);
     GET(GL_MAX_UNIFORM_BLOCK_SIZE, &limits->max_uniform_block_size);
     GET(GL_UNIFORM_BUFFER_OFFSET_ALIGNMENT, &limits->min_uniform_block_offset_alignment);
 
