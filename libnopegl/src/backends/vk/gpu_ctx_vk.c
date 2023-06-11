10bcd56df0010e7fa36edeb5e528731d0be275f5
Matthieu Bouron <matthieu.bouron@gmail.com>
2023-06-11 10:30:30 +0200

    gpu_ctx: rename NGLI_FEATURE_BUFFER_MAP to NGLI_FEATURE_BUFFER_MAP_PERSISTENT
    All supported backends support buffer mapping however not all supported
    backend support persistent buffer mapping (notably OpenGL < 4.3 and
    GLES < 3.2).

--- libnopegl/src/backends/vk/gpu_ctx_vk.c
+++ libnopegl/src/backends/vk/gpu_ctx_vk.c
@@ -879,7 +879,7 @@ static int vk_init(struct gpu_ctx *s)
     s->features = NGLI_FEATURE_COMPUTE |
                   NGLI_FEATURE_IMAGE_LOAD_STORE |
                   NGLI_FEATURE_STORAGE_BUFFER |
-                  NGLI_FEATURE_BUFFER_MAP;
+                  NGLI_FEATURE_BUFFER_MAP_PERSISTENT;
 
     struct vkcontext *vk = s_priv->vkcontext;
     const VkPhysicalDeviceLimits *limits = &vk->phy_device_props.limits;
