NopeForge/nope.gl

commit 9fb1c47a60375bc2fc343a763327601aed671fcb

gl: remove support for extensions already in OpenGL 3.3/OpenGL 3.0 core profile

libnopegl/scripts/gen-gl-wrappers.py
@@ -25,35 +25,15 @@ import textwrap
 from xml.etree import ElementTree as ET
 
 cmds_optional = [
-    # Framebuffer
-    "glBlitFramebuffer",
-    "glInvalidateFramebuffer",
-    "glGetFramebufferAttachmentParameteriv",
-    "glFramebufferTexture3D",
-    "glFramebufferTextureLayer",
-    # Renderbuffer
-    "glRenderbufferStorageMultisample",
     # Texture
-    "glTexImage3D",
     "glTexStorage2D",
     "glTexStorage3D",
-    "glTexSubImage3D",
     "glBindImageTexture",
-    # Vertex Arrays
-    "glBindVertexArray",
-    "glDeleteVertexArrays",
-    "glGenVertexArrays",
-    # Vertex Arrays OES
-    "glBindVertexArrayOES",
-    "glDeleteVertexArraysOES",
-    "glGenVertexArraysOES",
     # Barrier
     "glMemoryBarrier",
     #  Buffers
     "glBindBufferBase",
     "glBindBufferRange",
-    "glMapBufferRange",
-    "glUnmapBuffer",
     "glBufferStorage",
     # Compute shaders
     "glDispatchCompute",
@@ -83,33 +63,8 @@ cmds_optional = [
     "glDeleteQueriesEXT",
     "glQueryCounterEXT",
     "glGetQueryObjectui64vEXT",
-    # Instancing
-    "glDrawArraysInstanced",
-    "glDrawElementsInstanced",
-    "glVertexAttribDivisor",
-    # Uniform Block Object
-    "glGetUniformBlockIndex",
-    "glUniformBlockBinding",
-    "glGetActiveUniformBlockName",
-    "glGetActiveUniformBlockiv",
     # EGL OES image
     "glEGLImageTargetTexture2DOES",
-    # Sync object
-    "glFenceSync",
-    "glWaitSync",
-    "glClientWaitSync",
-    # Read/Draw Buffer
-    "glReadBuffer",
-    "glDrawBuffer",
-    "glDrawBuffers",
-    # Clear Buffer
-    "glClearBufferfv",
-    "glClearBufferfi",
-    # UInt uniforms
-    "glUniform1uiv",
-    "glUniform2uiv",
-    "glUniform3uiv",
-    "glUniform4uiv",
 ]
 
 cmds = [
@@ -144,6 +99,10 @@ cmds = [
     # Draw
     "glDrawArrays",
     "glDrawElements",
+    # Instancing
+    "glDrawArraysInstanced",
+    "glDrawElementsInstanced",
+    "glVertexAttribDivisor",
     # Texture
     "glActiveTexture",
     "glBindTexture",
@@ -154,6 +113,8 @@ cmds = [
     "glTexImage2D",
     "glTexParameteri",
     "glTexSubImage2D",
+    "glTexImage3D",
+    "glTexSubImage3D",
     # Framebuffer
     "glCheckFramebufferStatus",
     "glBindFramebuffer",
@@ -162,6 +123,13 @@ cmds = [
     "glFramebufferTexture2D",
     "glGenFramebuffers",
     "glReadPixels",
+    "glBlitFramebuffer",
+    "glInvalidateFramebuffer",
+    "glGetFramebufferAttachmentParameteriv",
+    "glFramebufferTexture3D",
+    "glFramebufferTextureLayer",
+    # Renderbuffer
+    "glRenderbufferStorageMultisample",
     # Buffer
     "glBindBuffer",
     "glBufferData",
@@ -207,12 +175,16 @@ cmds = [
     "glUniform1fv",
     "glUniform1i",
     "glUniform1iv",
+    "glUniform1uiv",
     "glUniform2fv",
     "glUniform2iv",
+    "glUniform2uiv",
     "glUniform3fv",
     "glUniform3iv",
+    "glUniform3uiv",
     "glUniform4fv",
     "glUniform4iv",
+    "glUniform4uiv",
     "glUniformMatrix2fv",
     "glUniformMatrix3fv",
     "glUniformMatrix4fv",
@@ -228,6 +200,29 @@ cmds = [
     # Sync
     "glFlush",
     "glFinish",
+    # Vertex Arrays
+    "glBindVertexArray",
+    "glDeleteVertexArrays",
+    "glGenVertexArrays",
+    # Uniform Block Object
+    "glGetUniformBlockIndex",
+    "glUniformBlockBinding",
+    "glGetActiveUniformBlockName",
+    "glGetActiveUniformBlockiv",
+    # Sync object
+    "glFenceSync",
+    "glWaitSync",
+    "glClientWaitSync",
+    # Read/Draw Buffer
+    "glReadBuffer",
+    "glDrawBuffer",
+    "glDrawBuffers",
+    # Clear Buffer
+    "glClearBufferfv",
+    "glClearBufferfi",
+    #  Map buffer range
+    "glMapBufferRange",
+    "glUnmapBuffer",
 ] + cmds_optional
 
 

libnopegl/src/backends/gl/gldefinitions_data.h
@@ -25,23 +25,22 @@ static const struct gldefinition {
     {"glBindImageTexture", offsetof(struct glfunctions, BindImageTexture), 0},
     {"glBindRenderbuffer", offsetof(struct glfunctions, BindRenderbuffer), M},
     {"glBindTexture", offsetof(struct glfunctions, BindTexture), M},
-    {"glBindVertexArray", offsetof(struct glfunctions, BindVertexArray), 0},
-    {"glBindVertexArrayOES", offsetof(struct glfunctions, BindVertexArrayOES), 0},
+    {"glBindVertexArray", offsetof(struct glfunctions, BindVertexArray), M},
     {"glBlendColor", offsetof(struct glfunctions, BlendColor), M},
     {"glBlendEquation", offsetof(struct glfunctions, BlendEquation), M},
     {"glBlendEquationSeparate", offsetof(struct glfunctions, BlendEquationSeparate), M},
     {"glBlendFunc", offsetof(struct glfunctions, BlendFunc), M},
     {"glBlendFuncSeparate", offsetof(struct glfunctions, BlendFuncSeparate), M},
-    {"glBlitFramebuffer", offsetof(struct glfunctions, BlitFramebuffer), 0},
+    {"glBlitFramebuffer", offsetof(struct glfunctions, BlitFramebuffer), M},
     {"glBufferData", offsetof(struct glfunctions, BufferData), M},
     {"glBufferStorage", offsetof(struct glfunctions, BufferStorage), 0},
     {"glBufferSubData", offsetof(struct glfunctions, BufferSubData), M},
     {"glCheckFramebufferStatus", offsetof(struct glfunctions, CheckFramebufferStatus), M},
     {"glClear", offsetof(struct glfunctions, Clear), M},
-    {"glClearBufferfi", offsetof(struct glfunctions, ClearBufferfi), 0},
-    {"glClearBufferfv", offsetof(struct glfunctions, ClearBufferfv), 0},
+    {"glClearBufferfi", offsetof(struct glfunctions, ClearBufferfi), M},
+    {"glClearBufferfv", offsetof(struct glfunctions, ClearBufferfv), M},
     {"glClearColor", offsetof(struct glfunctions, ClearColor), M},
-    {"glClientWaitSync", offsetof(struct glfunctions, ClientWaitSync), 0},
+    {"glClientWaitSync", offsetof(struct glfunctions, ClientWaitSync), M},
     {"glColorMask", offsetof(struct glfunctions, ColorMask), M},
     {"glCompileShader", offsetof(struct glfunctions, CompileShader), M},
     {"glCreateProgram", offsetof(struct glfunctions, CreateProgram), M},
@@ -56,8 +55,7 @@ static const struct gldefinition {
     {"glDeleteRenderbuffers", offsetof(struct glfunctions, DeleteRenderbuffers), M},
     {"glDeleteShader", offsetof(struct glfunctions, DeleteShader), M},
     {"glDeleteTextures", offsetof(struct glfunctions, DeleteTextures), M},
-    {"glDeleteVertexArrays", offsetof(struct glfunctions, DeleteVertexArrays), 0},
-    {"glDeleteVertexArraysOES", offsetof(struct glfunctions, DeleteVertexArraysOES), 0},
+    {"glDeleteVertexArrays", offsetof(struct glfunctions, DeleteVertexArrays), M},
     {"glDepthFunc", offsetof(struct glfunctions, DepthFunc), M},
     {"glDepthMask", offsetof(struct glfunctions, DepthMask), M},
     {"glDetachShader", offsetof(struct glfunctions, DetachShader), M},
@@ -65,41 +63,40 @@ static const struct gldefinition {
     {"glDisableVertexAttribArray", offsetof(struct glfunctions, DisableVertexAttribArray), M},
     {"glDispatchCompute", offsetof(struct glfunctions, DispatchCompute), 0},
     {"glDrawArrays", offsetof(struct glfunctions, DrawArrays), M},
-    {"glDrawArraysInstanced", offsetof(struct glfunctions, DrawArraysInstanced), 0},
-    {"glDrawBuffer", offsetof(struct glfunctions, DrawBuffer), 0},
-    {"glDrawBuffers", offsetof(struct glfunctions, DrawBuffers), 0},
+    {"glDrawArraysInstanced", offsetof(struct glfunctions, DrawArraysInstanced), M},
+    {"glDrawBuffer", offsetof(struct glfunctions, DrawBuffer), M},
+    {"glDrawBuffers", offsetof(struct glfunctions, DrawBuffers), M},
     {"glDrawElements", offsetof(struct glfunctions, DrawElements), M},
-    {"glDrawElementsInstanced", offsetof(struct glfunctions, DrawElementsInstanced), 0},
+    {"glDrawElementsInstanced", offsetof(struct glfunctions, DrawElementsInstanced), M},
     {"glEGLImageTargetTexture2DOES", offsetof(struct glfunctions, EGLImageTargetTexture2DOES), 0},
     {"glEnable", offsetof(struct glfunctions, Enable), M},
     {"glEnableVertexAttribArray", offsetof(struct glfunctions, EnableVertexAttribArray), M},
     {"glEndQuery", offsetof(struct glfunctions, EndQuery), 0},
     {"glEndQueryEXT", offsetof(struct glfunctions, EndQueryEXT), 0},
-    {"glFenceSync", offsetof(struct glfunctions, FenceSync), 0},
+    {"glFenceSync", offsetof(struct glfunctions, FenceSync), M},
     {"glFinish", offsetof(struct glfunctions, Finish), M},
     {"glFlush", offsetof(struct glfunctions, Flush), M},
     {"glFramebufferRenderbuffer", offsetof(struct glfunctions, FramebufferRenderbuffer), M},
     {"glFramebufferTexture2D", offsetof(struct glfunctions, FramebufferTexture2D), M},
-    {"glFramebufferTexture3D", offsetof(struct glfunctions, FramebufferTexture3D), 0},
-    {"glFramebufferTextureLayer", offsetof(struct glfunctions, FramebufferTextureLayer), 0},
+    {"glFramebufferTexture3D", offsetof(struct glfunctions, FramebufferTexture3D), M},
+    {"glFramebufferTextureLayer", offsetof(struct glfunctions, FramebufferTextureLayer), M},
     {"glGenBuffers", offsetof(struct glfunctions, GenBuffers), M},
     {"glGenFramebuffers", offsetof(struct glfunctions, GenFramebuffers), M},
     {"glGenQueries", offsetof(struct glfunctions, GenQueries), 0},
     {"glGenQueriesEXT", offsetof(struct glfunctions, GenQueriesEXT), 0},
     {"glGenRenderbuffers", offsetof(struct glfunctions, GenRenderbuffers), M},
     {"glGenTextures", offsetof(struct glfunctions, GenTextures), M},
-    {"glGenVertexArrays", offsetof(struct glfunctions, GenVertexArrays), 0},
-    {"glGenVertexArraysOES", offsetof(struct glfunctions, GenVertexArraysOES), 0},
+    {"glGenVertexArrays", offsetof(struct glfunctions, GenVertexArrays), M},
     {"glGenerateMipmap", offsetof(struct glfunctions, GenerateMipmap), M},
     {"glGetActiveAttrib", offsetof(struct glfunctions, GetActiveAttrib), M},
     {"glGetActiveUniform", offsetof(struct glfunctions, GetActiveUniform), M},
-    {"glGetActiveUniformBlockName", offsetof(struct glfunctions, GetActiveUniformBlockName), 0},
-    {"glGetActiveUniformBlockiv", offsetof(struct glfunctions, GetActiveUniformBlockiv), 0},
+    {"glGetActiveUniformBlockName", offsetof(struct glfunctions, GetActiveUniformBlockName), M},
+    {"glGetActiveUniformBlockiv", offsetof(struct glfunctions, GetActiveUniformBlockiv), M},
     {"glGetAttachedShaders", offsetof(struct glfunctions, GetAttachedShaders), M},
     {"glGetAttribLocation", offsetof(struct glfunctions, GetAttribLocation), M},
     {"glGetBooleanv", offsetof(struct glfunctions, GetBooleanv), M},
     {"glGetError", offsetof(struct glfunctions, GetError), M},
-    {"glGetFramebufferAttachmentParameteriv", offsetof(struct glfunctions, GetFramebufferAttachmentParameteriv), 0},
+    {"glGetFramebufferAttachmentParameteriv", offsetof(struct glfunctions, GetFramebufferAttachmentParameteriv), M},
     {"glGetIntegeri_v", offsetof(struct glfunctions, GetIntegeri_v), M},
     {"glGetIntegerv", offsetof(struct glfunctions, GetIntegerv), M},
     {"glGetInternalformativ", offsetof(struct glfunctions, GetInternalformativ), 0},
@@ -118,22 +115,22 @@ static const struct gldefinition {
     {"glGetShaderiv", offsetof(struct glfunctions, GetShaderiv), M},
     {"glGetString", offsetof(struct glfunctions, GetString), M},
     {"glGetStringi", offsetof(struct glfunctions, GetStringi), M},
-    {"glGetUniformBlockIndex", offsetof(struct glfunctions, GetUniformBlockIndex), 0},
+    {"glGetUniformBlockIndex", offsetof(struct glfunctions, GetUniformBlockIndex), M},
     {"glGetUniformLocation", offsetof(struct glfunctions, GetUniformLocation), M},
     {"glGetUniformiv", offsetof(struct glfunctions, GetUniformiv), M},
-    {"glInvalidateFramebuffer", offsetof(struct glfunctions, InvalidateFramebuffer), 0},
+    {"glInvalidateFramebuffer", offsetof(struct glfunctions, InvalidateFramebuffer), M},
     {"glLinkProgram", offsetof(struct glfunctions, LinkProgram), M},
-    {"glMapBufferRange", offsetof(struct glfunctions, MapBufferRange), 0},
+    {"glMapBufferRange", offsetof(struct glfunctions, MapBufferRange), M},
     {"glMemoryBarrier", offsetof(struct glfunctions, MemoryBarrier), 0},
     {"glPixelStorei", offsetof(struct glfunctions, PixelStorei), M},
     {"glPolygonMode", offsetof(struct glfunctions, PolygonMode), 0},
     {"glQueryCounter", offsetof(struct glfunctions, QueryCounter), 0},
     {"glQueryCounterEXT", offsetof(struct glfunctions, QueryCounterEXT), 0},
-    {"glReadBuffer", offsetof(struct glfunctions, ReadBuffer), 0},
+    {"glReadBuffer", offsetof(struct glfunctions, ReadBuffer), M},
     {"glReadPixels", offsetof(struct glfunctions, ReadPixels), M},
     {"glReleaseShaderCompiler", offsetof(struct glfunctions, ReleaseShaderCompiler), M},
     {"glRenderbufferStorage", offsetof(struct glfunctions, RenderbufferStorage), M},
-    {"glRenderbufferStorageMultisample", offsetof(struct glfunctions, RenderbufferStorageMultisample), 0},
+    {"glRenderbufferStorageMultisample", offsetof(struct glfunctions, RenderbufferStorageMultisample), M},
     {"glScissor", offsetof(struct glfunctions, Scissor), M},
     {"glShaderBinary", offsetof(struct glfunctions, ShaderBinary), M},
     {"glShaderSource", offsetof(struct glfunctions, ShaderSource), M},
@@ -144,33 +141,33 @@ static const struct gldefinition {
     {"glStencilOp", offsetof(struct glfunctions, StencilOp), M},
     {"glStencilOpSeparate", offsetof(struct glfunctions, StencilOpSeparate), M},
     {"glTexImage2D", offsetof(struct glfunctions, TexImage2D), M},
-    {"glTexImage3D", offsetof(struct glfunctions, TexImage3D), 0},
+    {"glTexImage3D", offsetof(struct glfunctions, TexImage3D), M},
     {"glTexParameteri", offsetof(struct glfunctions, TexParameteri), M},
     {"glTexStorage2D", offsetof(struct glfunctions, TexStorage2D), 0},
     {"glTexStorage3D", offsetof(struct glfunctions, TexStorage3D), 0},
     {"glTexSubImage2D", offsetof(struct glfunctions, TexSubImage2D), M},
-    {"glTexSubImage3D", offsetof(struct glfunctions, TexSubImage3D), 0},
+    {"glTexSubImage3D", offsetof(struct glfunctions, TexSubImage3D), M},
     {"glUniform1fv", offsetof(struct glfunctions, Uniform1fv), M},
     {"glUniform1i", offsetof(struct glfunctions, Uniform1i), M},
     {"glUniform1iv", offsetof(struct glfunctions, Uniform1iv), M},
-    {"glUniform1uiv", offsetof(struct glfunctions, Uniform1uiv), 0},
+    {"glUniform1uiv", offsetof(struct glfunctions, Uniform1uiv), M},
     {"glUniform2fv", offsetof(struct glfunctions, Uniform2fv), M},
     {"glUniform2iv", offsetof(struct glfunctions, Uniform2iv), M},
-    {"glUniform2uiv", offsetof(struct glfunctions, Uniform2uiv), 0},
+    {"glUniform2uiv", offsetof(struct glfunctions, Uniform2uiv), M},
     {"glUniform3fv", offsetof(struct glfunctions, Uniform3fv), M},
     {"glUniform3iv", offsetof(struct glfunctions, Uniform3iv), M},
-    {"glUniform3uiv", offsetof(struct glfunctions, Uniform3uiv), 0},
+    {"glUniform3uiv", offsetof(struct glfunctions, Uniform3uiv), M},
     {"glUniform4fv", offsetof(struct glfunctions, Uniform4fv), M},
     {"glUniform4iv", offsetof(struct glfunctions, Uniform4iv), M},
-    {"glUniform4uiv", offsetof(struct glfunctions, Uniform4uiv), 0},
-    {"glUniformBlockBinding", offsetof(struct glfunctions, UniformBlockBinding), 0},
+    {"glUniform4uiv", offsetof(struct glfunctions, Uniform4uiv), M},
+    {"glUniformBlockBinding", offsetof(struct glfunctions, UniformBlockBinding), M},
     {"glUniformMatrix2fv", offsetof(struct glfunctions, UniformMatrix2fv), M},
     {"glUniformMatrix3fv", offsetof(struct glfunctions, UniformMatrix3fv), M},
     {"glUniformMatrix4fv", offsetof(struct glfunctions, UniformMatrix4fv), M},
-    {"glUnmapBuffer", offsetof(struct glfunctions, UnmapBuffer), 0},
+    {"glUnmapBuffer", offsetof(struct glfunctions, UnmapBuffer), M},
     {"glUseProgram", offsetof(struct glfunctions, UseProgram), M},
-    {"glVertexAttribDivisor", offsetof(struct glfunctions, VertexAttribDivisor), 0},
+    {"glVertexAttribDivisor", offsetof(struct glfunctions, VertexAttribDivisor), M},
     {"glVertexAttribPointer", offsetof(struct glfunctions, VertexAttribPointer), M},
     {"glViewport", offsetof(struct glfunctions, Viewport), M},
-    {"glWaitSync", offsetof(struct glfunctions, WaitSync), 0},
+    {"glWaitSync", offsetof(struct glfunctions, WaitSync), M},
 };

libnopegl/src/backends/gl/feature_gl.h
@@ -22,50 +22,29 @@
 #ifndef FEATURE_GL_H
 #define FEATURE_GL_H
 
-#define NGLI_FEATURE_GL_VERTEX_ARRAY_OBJECT                        (1ULL << 0)
-#define NGLI_FEATURE_GL_TEXTURE_3D                                 (1ULL << 1)
 #define NGLI_FEATURE_GL_TEXTURE_STORAGE                            (1ULL << 2)
 #define NGLI_FEATURE_GL_COMPUTE_SHADER                             (1ULL << 3)
 #define NGLI_FEATURE_GL_PROGRAM_INTERFACE_QUERY                    (1ULL << 4)
 #define NGLI_FEATURE_GL_SHADER_IMAGE_LOAD_STORE                    (1ULL << 5)
 #define NGLI_FEATURE_GL_SHADER_STORAGE_BUFFER_OBJECT               (1ULL << 6)
-#define NGLI_FEATURE_GL_FRAMEBUFFER_OBJECT                         (1ULL << 7)
 #define NGLI_FEATURE_GL_INTERNALFORMAT_QUERY                       (1ULL << 8)
-#define NGLI_FEATURE_GL_PACKED_DEPTH_STENCIL                       (1ULL << 9)
 #define NGLI_FEATURE_GL_TIMER_QUERY                                (1ULL << 10)
 #define NGLI_FEATURE_GL_EXT_DISJOINT_TIMER_QUERY                   (1ULL << 11)
-#define NGLI_FEATURE_GL_DRAW_INSTANCED                             (1ULL << 12)
-#define NGLI_FEATURE_GL_INSTANCED_ARRAY                            (1ULL << 13)
-#define NGLI_FEATURE_GL_UNIFORM_BUFFER_OBJECT                      (1ULL << 14)
 #define NGLI_FEATURE_GL_INVALIDATE_SUBDATA                         (1ULL << 15)
 #define NGLI_FEATURE_GL_OES_EGL_EXTERNAL_IMAGE                     (1ULL << 16)
-#define NGLI_FEATURE_GL_DEPTH_TEXTURE                              (1ULL << 17)
-#define NGLI_FEATURE_GL_RGB8_RGBA8                                 (1ULL << 18)
 #define NGLI_FEATURE_GL_OES_EGL_IMAGE                              (1ULL << 19)
 #define NGLI_FEATURE_GL_EGL_IMAGE_BASE_KHR                         (1ULL << 20)
 #define NGLI_FEATURE_GL_EGL_EXT_IMAGE_DMA_BUF_IMPORT               (1ULL << 21)
-#define NGLI_FEATURE_GL_SYNC                                       (1ULL << 22)
 #define NGLI_FEATURE_GL_YUV_TARGET                                 (1ULL << 23)
-#define NGLI_FEATURE_GL_TEXTURE_NPOT                               (1ULL << 24)
-#define NGLI_FEATURE_GL_TEXTURE_CUBE_MAP                           (1ULL << 25)
-#define NGLI_FEATURE_GL_DRAW_BUFFERS                               (1ULL << 26)
-#define NGLI_FEATURE_GL_ROW_LENGTH                                 (1ULL << 27)
 #define NGLI_FEATURE_GL_SOFTWARE                                   (1ULL << 28)
-#define NGLI_FEATURE_GL_UINT_UNIFORMS                              (1ULL << 29)
 #define NGLI_FEATURE_GL_EGL_ANDROID_GET_IMAGE_NATIVE_CLIENT_BUFFER (1ULL << 30)
 #define NGLI_FEATURE_GL_KHR_DEBUG                                  (1ULL << 31)
-#define NGLI_FEATURE_GL_CLEAR_BUFFER                               (1ULL << 32)
 #define NGLI_FEATURE_GL_SHADER_IMAGE_SIZE                          (1ULL << 33)
 #define NGLI_FEATURE_GL_SHADING_LANGUAGE_420PACK                   (1ULL << 34)
-#define NGLI_FEATURE_GL_SHADER_TEXTURE_LOD                         (1ULL << 35)
 #define NGLI_FEATURE_GL_COLOR_BUFFER_FLOAT                         (1ULL << 36)
 #define NGLI_FEATURE_GL_COLOR_BUFFER_HALF_FLOAT                    (1ULL << 37)
-#define NGLI_FEATURE_GL_MAP_BUFFER_RANGE                           (1ULL << 38)
 #define NGLI_FEATURE_GL_BUFFER_STORAGE                             (1ULL << 39)
-#define NGLI_FEATURE_GL_OES_STANDARD_DERIVATIVES                   (1ULL << 40)
 #define NGLI_FEATURE_GL_EGL_MESA_QUERY_DRIVER                      (1ULL << 41)
-#define NGLI_FEATURE_GL_TEXTURE_2D_ARRAY                           (1ULL << 42)
-#define NGLI_FEATURE_GL_OES_VERTEX_ARRAY_OBJECT                    (1ULL << 43)
 
 #define NGLI_FEATURE_GL_COMPUTE_SHADER_ALL (NGLI_FEATURE_GL_COMPUTE_SHADER           | \
                                             NGLI_FEATURE_GL_PROGRAM_INTERFACE_QUERY  | \

libnopegl/src/backends/gl/glcontext.c
@@ -344,23 +344,13 @@ static int glcontext_probe_settings(struct glcontext *glcontext)
     GET(GL_MAX_TEXTURE_IMAGE_UNITS, &limits->max_texture_image_units);
     GET(GL_MAX_TEXTURE_SIZE, &limits->max_texture_dimension_1d);
     GET(GL_MAX_TEXTURE_SIZE, &limits->max_texture_dimension_2d);
-    if (glcontext->features & NGLI_FEATURE_GL_TEXTURE_3D)
-        GET(GL_MAX_3D_TEXTURE_SIZE, &limits->max_texture_dimension_3d);
-    if (glcontext->features & NGLI_FEATURE_GL_TEXTURE_CUBE_MAP)
-        GET(GL_MAX_CUBE_MAP_TEXTURE_SIZE, &limits->max_texture_dimension_cube);
-    if (glcontext->features & NGLI_FEATURE_GL_TEXTURE_2D_ARRAY)
-        GET(GL_MAX_ARRAY_TEXTURE_LAYERS, &limits->max_texture_array_layers);
-
-    limits->max_color_attachments = 1;
-    if (glcontext->features & NGLI_FEATURE_GL_FRAMEBUFFER_OBJECT) {
-        GET(GL_MAX_SAMPLES, &limits->max_samples);
-        GET(GL_MAX_COLOR_ATTACHMENTS, &limits->max_color_attachments);
-    }
-
-    if (glcontext->features & NGLI_FEATURE_GL_UNIFORM_BUFFER_OBJECT) {
-        GET(GL_MAX_UNIFORM_BLOCK_SIZE, &limits->max_uniform_block_size);
-        GET(GL_UNIFORM_BUFFER_OFFSET_ALIGNMENT, &limits->min_uniform_block_offset_alignment);
-    }
+    GET(GL_MAX_3D_TEXTURE_SIZE, &limits->max_texture_dimension_3d);
+    GET(GL_MAX_CUBE_MAP_TEXTURE_SIZE, &limits->max_texture_dimension_cube);
+    GET(GL_MAX_ARRAY_TEXTURE_LAYERS, &limits->max_texture_array_layers);
+    GET(GL_MAX_SAMPLES, &limits->max_samples);
+    GET(GL_MAX_COLOR_ATTACHMENTS, &limits->max_color_attachments);
+    GET(GL_MAX_UNIFORM_BLOCK_SIZE, &limits->max_uniform_block_size);
+    GET(GL_UNIFORM_BUFFER_OFFSET_ALIGNMENT, &limits->min_uniform_block_offset_alignment);
 
     if (glcontext->features & NGLI_FEATURE_GL_SHADER_STORAGE_BUFFER_OBJECT) {
         GET(GL_MAX_SHADER_STORAGE_BLOCK_SIZE, &limits->max_storage_block_size);
@@ -385,10 +375,7 @@ static int glcontext_probe_settings(struct glcontext *glcontext)
         GET(GL_MAX_COMPUTE_SHARED_MEMORY_SIZE, &limits->max_compute_shared_memory_size);
     }
 
-    limits->max_draw_buffers = 1;
-    if (glcontext->features & NGLI_FEATURE_GL_DRAW_BUFFERS) {
-        GET(GL_MAX_DRAW_BUFFERS, &limits->max_draw_buffers);
-    }
+    GET(GL_MAX_DRAW_BUFFERS, &limits->max_draw_buffers);
 
     return 0;
 }
@@ -503,8 +490,7 @@ struct glcontext *ngli_glcontext_new(const struct glcontext_params *params)
     if (ret < 0)
         goto fail;
 
-    if (glcontext->backend == NGL_BACKEND_OPENGL &&
-        (glcontext->features & NGLI_FEATURE_GL_TEXTURE_CUBE_MAP))
+    if (glcontext->backend == NGL_BACKEND_OPENGL)
         ngli_glEnable(glcontext, GL_TEXTURE_CUBE_MAP_SEAMLESS);
 
     if (!glcontext->external && !glcontext->offscreen) {

commit d8fce7bc71eb2f474d3707d7229f01de9691b9b6

gl: make glInvalidateFramebuffer() non-mandatory
glInvalidateFramebuffer() is only present if GL_ARB_invalidate_subdata
is supported, which is not the case on macOS.

Fixes a regression introduced by 9fb1c47a60375bc2fc343a763327601aed671fcb.

libnopegl/scripts/gen-gl-wrappers.py
@@ -65,6 +65,8 @@ cmds_optional = [
     "glGetQueryObjectui64vEXT",
     # EGL OES image
     "glEGLImageTargetTexture2DOES",
+    # Invalidate subdat
+    "glInvalidateFramebuffer",
 ]
 
 cmds = [
@@ -124,7 +126,6 @@ cmds = [
     "glGenFramebuffers",
     "glReadPixels",
     "glBlitFramebuffer",
-    "glInvalidateFramebuffer",
     "glGetFramebufferAttachmentParameteriv",
     "glFramebufferTexture3D",
     "glFramebufferTextureLayer",

libnopegl/src/backends/gl/gldefinitions_data.h
@@ -118,7 +118,7 @@ static const struct gldefinition {
     {"glGetUniformBlockIndex", offsetof(struct glfunctions, GetUniformBlockIndex), M},
     {"glGetUniformLocation", offsetof(struct glfunctions, GetUniformLocation), M},
     {"glGetUniformiv", offsetof(struct glfunctions, GetUniformiv), M},
-    {"glInvalidateFramebuffer", offsetof(struct glfunctions, InvalidateFramebuffer), M},
+    {"glInvalidateFramebuffer", offsetof(struct glfunctions, InvalidateFramebuffer), 0},
     {"glLinkProgram", offsetof(struct glfunctions, LinkProgram), M},
     {"glMapBufferRange", offsetof(struct glfunctions, MapBufferRange), M},
     {"glMemoryBarrier", offsetof(struct glfunctions, MemoryBarrier), 0},