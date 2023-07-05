51a267376c697939653922a1682694232260d5ec
Matthieu Bouron <matthieu.bouron@gmail.com>
2023-07-05 11:06:28 +0200

    pass: rename map to uniform_map

--- libnopegl/src/pass.c
+++ libnopegl/src/pass.c
@@ -651,9 +651,9 @@ int ngli_pass_exec(struct pass *s)
         ngli_pipeline_compat_update_uniform(pipeline_compat, desc->normal_matrix_index, normal_matrix);
     }
 
-    const struct uniform_map *map = ngli_darray_data(&desc->uniforms_map);
+    const struct uniform_map *uniform_map = ngli_darray_data(&desc->uniforms_map);
     for (size_t i = 0; i < ngli_darray_count(&desc->uniforms_map); i++)
-        ngli_pipeline_compat_update_uniform(pipeline_compat, map[i].index, map[i].data);
+        ngli_pipeline_compat_update_uniform(pipeline_compat, uniform_map[i].index, uniform_map[i].data);
 
     const struct darray *texture_infos_array = ngli_pgcraft_get_texture_infos(desc->crafter);
     const struct pgcraft_texture_info *texture_infos = ngli_darray_data(texture_infos_array);
