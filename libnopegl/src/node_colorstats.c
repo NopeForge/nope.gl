291089d8700a8d235a056cf7f5e335edab14940f
Matthieu Bouron <matthieu.bouron@gmail.com>
2023-07-06 18:45:49 +0200

    colorstats: only update pipeline texture info on image change
    Makes pynopegl_utils.examples.misc.scope work with the Vulkan backend.

--- libnopegl/src/node_colorstats.c
+++ libnopegl/src/node_colorstats.c
@@ -79,6 +79,7 @@ struct colorstats_priv {
         uint32_t wg_count;
         int32_t block_index;
         const struct pgcraft_texture_info *texture_info;
+        size_t texture_image_rev;
     } waveform;
 
     /* Summary-scale compute */
@@ -183,6 +184,7 @@ static int setup_waveform_compute(struct colorstats_priv *s, const struct pgcraf
 
     const struct darray *texture_infos_array = ngli_pgcraft_get_texture_infos(s->waveform.crafter);
     s->waveform.texture_info = ngli_darray_get(texture_infos_array, 0);
+    s->waveform.texture_image_rev = SIZE_MAX;
 
     s->waveform.block_index = ngli_pgcraft_get_block_index(s->waveform.crafter, block->name, block->stage);
 
@@ -416,7 +418,13 @@ static void colorstats_draw(struct ngl_node *node)
     ngli_pipeline_compat_dispatch(s->init.pipeline_compat, s->init.wg_count, 1, 1);
 
     /* Waveform */
-    ngli_pipeline_compat_update_texture_info(s->waveform.pipeline_compat, s->waveform.texture_info);
+    const struct image *image = s->waveform.texture_info->image;
+    ngli_assert(image);
+    if (s->waveform.texture_image_rev != image->rev) {
+        ngli_pipeline_compat_update_texture_info(s->waveform.pipeline_compat, s->waveform.texture_info);
+        s->waveform.texture_image_rev = image->rev;
+    }
+
     ngli_pipeline_compat_dispatch(s->waveform.pipeline_compat, s->waveform.wg_count, 1, 1);
 
     /* Summary-scale */
