21d163100471f0d1b77e4981a26183f685755623
Clément Bœsch <u@pkh.me>
2023-07-29 23:26:11 +0200

    glsl/helper_misc_utils: add ngli_linearstep()

--- libnopegl/src/glsl/helper_misc_utils.glsl
+++ libnopegl/src/glsl/helper_misc_utils.glsl
@@ -22,6 +22,7 @@
 
 #define ngli_sat(x) clamp(x, 0.0, 1.0)
 #define ngli_linear(a, b, x) (((x) - (a)) / ((b) - (a)))
+#define ngli_linearstep(a, b, x) ngli_sat(ngli_linear(a, b, x))
 
 const vec3 ngli_luma_weights = vec3(.2126, .7152, .0722); // BT.709
 const float ngli_pi = 3.14159265358979323846;
