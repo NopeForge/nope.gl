NopeForge/nope.gl

commit 7d297568172172e4047beb7d731a3a9dd91c41d5

internal: switch to nope.media 11.0.0

libnopegl/src/backends/gl/hwmap_vaapi_gl.c
@@ -164,8 +164,7 @@ static void vaapi_uninit(struct hwmap *hwmap)
         vaapi->surface_acquired = 0;
     }
 
-    nmd_release_frame(vaapi->frame);
-    vaapi->frame = NULL;
+    nmd_frame_releasep(&vaapi->frame);
 }
 
 static int vaapi_map_frame(struct hwmap *hwmap, struct nmd_frame *frame)
@@ -177,8 +176,7 @@ static int vaapi_map_frame(struct hwmap *hwmap, struct nmd_frame *frame)
     struct glcontext *gl = gpu_ctx_gl->glcontext;
     struct hwmap_vaapi *vaapi = hwmap->hwmap_priv_data;
 
-    nmd_release_frame(vaapi->frame);
-    vaapi->frame = frame;
+    nmd_frame_releasep(&vaapi->frame);
 
     if (vaapi->surface_acquired) {
         for (size_t i = 0; i < 2; i++) {

.github/workflows/ci_android.yml
@@ -19,7 +19,7 @@ jobs:
         arch: [x86_64, arm, aarch64]
         ffmpeg_version: [5.0.1]
         glslang_version: [11.9.0]
-        nopemd_version: [10.0.0]
+        nopemd_version: [11.0.0]
 
     steps:
     - uses: actions/checkout@v2

.github/workflows/ci_ios.yml
@@ -18,7 +18,7 @@ jobs:
       matrix:
         arch: [x86_64, arm64]
         ffmpeg_version: [5.0.1]
-        nopemd_version: [10.0.0]
+        nopemd_version: [11.0.0]
 
     steps:
     - uses: actions/checkout@v2

configure.py
@@ -52,10 +52,10 @@ _EXTERNAL_DEPS = dict(
         sha256="1416af28ebd2e52018276a88fe0a279cdb2660dd70a5afac327b13e856373425",
     ),
     nopemd=dict(
-        version="10.0.0",
+        version="11.0.0",
         url="https://github.com/nope-project/nope.media/archive/v@VERSION@.tar.gz",
         dst_file="nope.media-@VERSION@.tar.gz",
-        sha256="b11cad7ed46b507312410386000261797f41eaa0d7e6e108da52e5adfbc6090c",
+        sha256="066febe24914cbad9895911e9b3a9006332a269f026c22e3cf4183026ac300fe",
     ),
     egl_registry=dict(
         version="9ab6036",

libnopegl/meson.build
@@ -257,7 +257,6 @@ hosts_cfg = {
     'frameworks': ['CoreVideo', 'CoreFoundation', 'AppKit', 'IOSurface'],
   },
   'android': {
-    'deps': ['libavcodec'],
     'libs': ['android'],
     'src': files(
       'src/jni_utils.c',
@@ -277,7 +276,7 @@ hosts_cfg = {
 
 lib_deps = [
   cc.find_library('m', required: false),
-  dependency('libnopemd', version: '>= 10.0.0'),
+  dependency('libnopemd', version: '>= 11.0.0'),
   dependency('threads'),
 ]
 

libnopegl/src/android_surface.c
@@ -21,7 +21,6 @@
 
 #include <jni.h>
 #include <time.h>
-#include <libavcodec/mediacodec.h>
 
 #include "android_utils.h"
 #include "jni_utils.h"
@@ -324,7 +323,7 @@ fail:
     return ret;
 }
 
-int ngli_android_surface_render_buffer(struct android_surface *surface, AVMediaCodecBuffer *buffer, float *matrix)
+int ngli_android_surface_render_frame(struct android_surface *surface, struct nmd_frame **framep, float *matrix)
 {
     int ret = 0;
 
@@ -332,13 +331,15 @@ int ngli_android_surface_render_buffer(struct android_surface *surface, AVMediaC
         return 0;
 
     JNIEnv *env = ngli_jni_get_env();
-    if (!env)
+    if (!env) {
+        nmd_mc_frame_render_and_releasep(framep);
         return NGL_ERROR_EXTERNAL;
+    }
 
     pthread_mutex_lock(&surface->lock);
     surface->on_frame_available = 0;
 
-    if (av_mediacodec_release_buffer(buffer, 1) < 0) {
+    if (nmd_mc_frame_render_and_releasep(framep) < 0) {
         pthread_mutex_unlock(&surface->lock);
         ret = NGL_ERROR_EXTERNAL;
         goto fail;

libnopegl/src/android_surface.h
@@ -22,7 +22,7 @@
 #ifndef ANDROID_SURFACE_H
 #define ANDROID_SURFACE_H
 
-#include <libavcodec/mediacodec.h>
+#include <nopemd.h>
 
 struct android_surface;
 struct android_surface *ngli_android_surface_new(int tex_id, void *handler);
@@ -30,7 +30,7 @@ void ngli_android_surface_free(struct android_surface **surface);
 void *ngli_android_surface_get_surface(struct android_surface *surface);
 int ngli_android_surface_attach_to_gl_context(struct android_surface *surface, int tex_id);
 int ngli_android_surface_detach_from_gl_context(struct android_surface *surface);
-int ngli_android_surface_render_buffer(struct android_surface *surface, AVMediaCodecBuffer *buffer, float *matrix);
+int ngli_android_surface_render_frame(struct android_surface *surface, struct nmd_frame **framep, float *matrix);
 void ngli_android_surface_signal_frame(struct android_surface *surface);
 
 #endif /* ANDROID_SURFACE_H */

libnopegl/src/backends/gl/hwmap_mediacodec_gl.c
@@ -23,7 +23,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <nopemd.h>
-#include <libavcodec/mediacodec.h>
 #include <android/hardware_buffer.h>
 #include <android/hardware_buffer_jni.h>
 
@@ -142,7 +141,8 @@ static int mc_map_frame_surfacetexture(struct hwmap *hwmap, struct nmd_frame *fr
 {
     const struct hwmap_params *params = &hwmap->params;
     struct hwmap_mc *mc = hwmap->hwmap_priv_data;
-    AVMediaCodecBuffer *buffer = (AVMediaCodecBuffer *)frame->datap[0];
+
+    ngli_texture_gl_set_dimensions(mc->texture, frame->width, frame->height, 0);
 
     NGLI_ALIGNED_MAT(flip_matrix) = {
         1.0f, 0.0f, 0.0f, 0.0f,
@@ -152,10 +152,9 @@ static int mc_map_frame_surfacetexture(struct hwmap *hwmap, struct nmd_frame *fr
     };
 
     float *matrix = hwmap->mapped_image.coordinates_matrix;
-    ngli_android_surface_render_buffer(params->android_surface, buffer, matrix);
+    ngli_android_surface_render_frame(params->android_surface, &frame, matrix);
     ngli_mat4_mul(matrix, matrix, flip_matrix);
 
-    ngli_texture_gl_set_dimensions(mc->texture, frame->width, frame->height, 0);
 
     return 0;
 }
@@ -170,8 +169,7 @@ static int mc_map_frame_imagereader(struct hwmap *hwmap, struct nmd_frame *frame
     struct glcontext *gl = gpu_ctx_gl->glcontext;
     struct android_ctx *android_ctx = &ctx->android_ctx;
 
-    AVMediaCodecBuffer *buffer = (AVMediaCodecBuffer *)frame->datap[0];
-    int ret = av_mediacodec_release_buffer(buffer, 1);
+    int ret = nmd_mc_frame_render_and_releasep(&frame);
     if (ret < 0)
         return ret;
 
@@ -268,6 +266,7 @@ const struct hwmap_class ngli_hwmap_mc_gl_class = {
         NGLI_IMAGE_LAYOUT_MEDIACODEC,
         NGLI_IMAGE_LAYOUT_NONE
     },
+    .flags     = HWMAP_FLAG_FRAME_OWNER,
     .priv_size = sizeof(struct hwmap_mc),
     .init      = mc_init,
     .map_frame = mc_map_frame,

commit fba6fc5073d829e1a77c6e7d1097884020d8efc4

hwmap_vaapi_gl: fix VAAPI frame mapping
Fixes a regression introduced by 7d297568172172e4047beb7d731a3a9dd91c41d5.

libnopegl/src/backends/gl/hwmap_vaapi_gl.c
@@ -177,6 +177,7 @@ static int vaapi_map_frame(struct hwmap *hwmap, struct nmd_frame *frame)
     struct hwmap_vaapi *vaapi = hwmap->hwmap_priv_data;
 
     nmd_frame_releasep(&vaapi->frame);
+    vaapi->frame = frame;
 
     if (vaapi->surface_acquired) {
         for (size_t i = 0; i < 2; i++) {