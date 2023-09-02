8c6b1cb07442b36185e0b8078e2ed4370c0c515a
Matthieu Bouron <matthieu.bouron@gmail.com>
2023-09-02 11:49:47 +0200

    scripts/pyinstaller: use proper $PATH separator
    The original intent was to use os.pathsep not os.path.sep.

--- scripts/pyinstaller/nope-viewer.py
+++ scripts/pyinstaller/nope-viewer.py
@@ -4,7 +4,7 @@ import sys
 
 from pynopegl_utils.viewer import run
 
-os.environ["PATH"] = op.sep.join([op.join(sys._MEIPASS, "bin"), os.environ["PATH"]])
+os.environ["PATH"] = os.pathsep.join([op.join(sys._MEIPASS, "bin"), os.environ["PATH"]])
 
 if __name__ == "__main__":
     run()
