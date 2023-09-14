37ba90d19388c6f34e839ef4772e74a8586e9926
Clément Bœsch <u@pkh.me>
2023-09-14 12:51:17 +0200

    doc/renders: replace mire with cat video in the simple render video example

--- doc/_ext/nope/renders/video.py
+++ doc/_ext/nope/renders/video.py
@@ -5,7 +5,7 @@ import pynopegl as ngl
 
 @scene()
 def video(cfg: SceneCfg):
-    video = load_media(cfg, "mire")  # Replace "mire" with a path to your video
+    video = load_media(cfg, "cat")  # Replace "cat" with a path to your video
 
     cfg.aspect_ratio = video.width, video.height
     cfg.duration = max(video.duration, 5)  # Limit to 5 seconds maximum
