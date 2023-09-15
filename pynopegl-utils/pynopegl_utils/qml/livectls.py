094e06a464ea697a278a7812006c349a01d579c9
Matthieu Bouron <matthieu.bouron@gmail.com>
2023-09-15 12:47:48 +0200

    utils/qml: extend live controls support
    Adds support for UniformIVec*, UniformUIVec* types.

--- pynopegl-utils/pynopegl_utils/qml/livectls.py
+++ pynopegl-utils/pynopegl_utils/qml/livectls.py
@@ -20,27 +20,38 @@
 # under the License.
 #
 
+from dataclasses import dataclass
 from typing import Any, Dict, List
 
 from PySide6.QtGui import QColor
 
 import pynopegl as ngl
 
-_NODE_TYPES_MODEL_MAP = dict(
-    UniformColor="color",
-    UniformBool="bool",
-    UniformVec2="vector",
-    UniformVec3="vector",
-    UniformVec4="vector",
-    UniformMat4="vector",
-    Text="text",
-)
 
-_NODE_TYPES_CMP_MAP = dict(
-    UniformVec2=2,
-    UniformVec3=3,
-    UniformVec4=4,
-    UniformMat4=4 * 4,
+@dataclass
+class _ControlSpec:
+    control_type: str
+    base_type: str
+    components: int
+
+
+_NODE_TYPES_SPEC_MAP = dict(
+    Text=_ControlSpec("text", "str", 1),
+    UniformBool=_ControlSpec("bool", "bool", 1),
+    UniformColor=_ControlSpec("color", "float", 3),
+    UniformFloat=_ControlSpec("range", "float", 1),
+    UniformInt=_ControlSpec("range", "int", 1),
+    UniformIVec2=_ControlSpec("vector", "int", 2),
+    UniformIVec3=_ControlSpec("vector", "int", 3),
+    UniformIVec4=_ControlSpec("vector", "int", 4),
+    UniformMat4=_ControlSpec("vector", "float", 16),
+    UniformUInt=_ControlSpec("range", "uint", 1),
+    UniformUIVec2=_ControlSpec("vector", "uint", 2),
+    UniformUIVec3=_ControlSpec("vector", "uint", 3),
+    UniformUIVec4=_ControlSpec("vector", "uint", 4),
+    UniformVec2=_ControlSpec("vector", "float", 2),
+    UniformVec3=_ControlSpec("vector", "float", 3),
+    UniformVec4=_ControlSpec("vector", "float", 4),
 )
 
 
@@ -48,17 +59,25 @@ def get_model_data(scene) -> List[Dict[str, Any]]:
     livectls = ngl.get_livectls(scene)
     model_data = []
     for label, ctl in livectls.items():
+        node_type = ctl["node_type"]
+        spec = _NODE_TYPES_SPEC_MAP.get(node_type)
+        if spec is None:
+            print(f"live control type {node_type} is not yet supported")
+            continue
         data = dict(
-            type=_NODE_TYPES_MODEL_MAP.get(ctl["node_type"], "range"),
+            type=spec.control_type,
             label=label,
             val=ctl["val"],
             node=ctl["node"],
         )
+
+        if spec.base_type in ("int", "uint"):
+            data["step"] = 1
+        if data["type"] == "vector":
+            data["n"] = spec.components
         if data["type"] not in ("bool", "text"):
             data["min"] = ctl["min"]
             data["max"] = ctl["max"]
-        if data["type"] == "vector":
-            data["n"] = _NODE_TYPES_CMP_MAP.get(ctl["node_type"], 0)
         if data["type"] == "color":
             data["val"] = QColor.fromRgbF(*ctl["val"])
         model_data.append(data)
