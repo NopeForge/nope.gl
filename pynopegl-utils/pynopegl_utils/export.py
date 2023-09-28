NopeForge/nope.gl

commit 13c115510f2d0ed07c084852e5b1585c942c46a0

utils: switch to ngl.{scene,SceneCfg,SceneInfo}

pynopegl-utils/pynopegl_utils/export.py
@@ -30,7 +30,7 @@ import tempfile
 from dataclasses import dataclass
 from typing import List, Optional
 
-from pynopegl_utils.misc import SceneInfo, get_backend, get_viewport
+from pynopegl_utils.misc import get_backend, get_viewport
 
 import pynopegl as ngl
 
@@ -119,7 +119,7 @@ def export_workers(scene_info: SceneInfo, filename: str, resolution: str, profil
 
 
 def _export_worker(
-    scene_info: SceneInfo,
+    scene_info: ngl.SceneInfo,
     filename: str,
     resolution: str,
     extra_enc_args: Optional[List[str]] = None,

pynopegl-utils/pynopegl_utils/exporter.py
@@ -22,7 +22,6 @@
 from typing import Callable, Optional
 
 from pynopegl_utils.export import export_workers
-from pynopegl_utils.misc import SceneInfo
 from PySide6 import QtCore
 
 
@@ -33,7 +32,7 @@ class Exporter(QtCore.QThread):
 
     def __init__(
         self,
-        get_scene_info: Callable[..., Optional[SceneInfo]],
+        get_scene_info: Callable[..., Optional[ngl.SceneInfo]],
         filename,
         res_id: str,
         profile_id: str,

pynopegl-utils/pynopegl_utils/com.py
@@ -27,9 +27,10 @@ import traceback
 from dataclasses import dataclass, field
 from typing import Any, Callable, Optional, Set
 
-from pynopegl_utils.misc import SceneCfg, SceneInfo
 from pynopegl_utils.module import load_script
 from pynopegl_utils.resourcetracker import ResourceTracker
+
+import pynopegl as ngl
 
 
 @dataclass
@@ -68,7 +69,7 @@ def _wrap_query(func) -> Callable[..., QueryInfo]:
 
 
 @_wrap_query
-def query_scene(pkg, func, cfg: SceneCfg, extra_args=None) -> SceneInfo:
+def query_scene(pkg, func, cfg: ngl.SceneCfg, extra_args=None) -> ngl.SceneInfo:
     # Call user constructing function
     if extra_args is None:
         extra_args = {}

pynopegl-utils/pynopegl_utils/hooks.py
@@ -26,9 +26,10 @@ import tempfile
 import time
 from typing import Callable, Optional
 
-from pynopegl_utils.misc import SceneInfo
 from pynopegl_utils.module import load_script
 from PySide6 import QtCore
+
+import pynopegl as ngl
 
 
 class _HooksCaller:
@@ -56,7 +57,7 @@ class _HooksCaller:
             uint_color |= comp_val << (24 - i * 8)
         return uint_color
 
-    def scene_change(self, session_id, local_scene, scene_info: SceneInfo):
+    def scene_change(self, session_id, local_scene, scene_info: ngl.SceneInfo):
         return self._module.scene_change(
             session_id,
             local_scene,
@@ -109,7 +110,7 @@ class HooksCaller:
         caller, session_id = self._get_caller_session_id(session_id)
         return caller.get_session_info(session_id)
 
-    def scene_change(self, session_id, local_scene, scene_info: SceneInfo):
+    def scene_change(self, session_id, local_scene, scene_info: ngl.SceneInfo):
         caller, session_id = self._get_caller_session_id(session_id)
         return caller.scene_change(session_id, local_scene, scene_info)
 
@@ -161,7 +162,7 @@ class _SceneChangeWorker(QtCore.QObject):
     success = QtCore.Signal(str, str, float)
     error = QtCore.Signal(str, str)
 
-    def __init__(self, get_scene_info: Callable[..., Optional[SceneInfo]], hooks_caller):
+    def __init__(self, get_scene_info: Callable[..., Optional[ngl.SceneInfo]], hooks_caller):
         super().__init__()
         self._get_scene_info = get_scene_info
         self._hooks_caller = hooks_caller
@@ -247,7 +248,7 @@ class HooksController(QtCore.QObject):
     session_removed = QtCore.Signal(str)
     session_info_changed = QtCore.Signal(object)
 
-    def __init__(self, get_scene_info: Callable[..., Optional[SceneInfo]], hooks_caller):
+    def __init__(self, get_scene_info: Callable[..., Optional[ngl.SceneInfo]], hooks_caller):
         super().__init__()
         self._get_scene_info = get_scene_info
         self._hooks_caller = hooks_caller

pynopegl-utils/pynopegl_utils/misc.py
@@ -98,7 +98,7 @@ def get_shader(name: str) -> str:
     return data.decode()
 
 
-def load_media(cfg: SceneCfg, file: str):
+def load_media(cfg: ngl.SceneCfg, file: str):
     db_file = MEDIA_FILES_DB.get(file)
     if db_file:
         file = op.join(op.dirname(__file__), "assets", db_file)

pynopegl-utils/pynopegl_utils/tests/cmp.py
@@ -24,7 +24,7 @@ import os
 import os.path as op
 from typing import Any, Callable, Generator, Optional, Sequence, Tuple, Union
 
-from pynopegl_utils.misc import SceneCfg, SceneInfo, get_backend, get_nopegl_tempdir
+from pynopegl_utils.misc import get_backend, get_nopegl_tempdir
 
 import pynopegl as ngl
 
@@ -71,7 +71,7 @@ class CompareBase:
 class CompareSceneBase(CompareBase):
     def __init__(
         self,
-        scene_func: Callable[..., SceneInfo],
+        scene_func: Callable[..., ngl.SceneInfo],
         width: int = 1280,
         height: int = 800,
         keyframes: Union[int, Sequence[float]] = 1,  # either a number of keyframes or a sequence of absolute times
@@ -96,7 +96,7 @@ class CompareSceneBase(CompareBase):
         self._hud_export_filename = None
 
     def render_frames(self) -> Generator[Tuple[int, int, bytearray], None, None]:
-        cfg = SceneCfg(samples=self._samples, clear_color=self._clear_color)
+        cfg = ngl.SceneCfg(samples=self._samples, clear_color=self._clear_color)
 
         backend = os.environ.get("BACKEND")
         if backend:

pynopegl-utils/pynopegl_utils/tests/data.py
@@ -22,7 +22,6 @@
 import colorsys
 import random
 
-from pynopegl_utils.misc import SceneCfg
 from pynopegl_utils.tests.debug import get_debug_points
 
 import pynopegl as ngl
@@ -271,7 +270,7 @@ _FUNCS = dict(
 )
 
 
-def get_field_scene(cfg: SceneCfg, spec, category, field_type, seed, debug_positions, layout, color_tint):
+def get_field_scene(cfg: ngl.SceneCfg, spec, category, field_type, seed, debug_positions, layout, color_tint):
     """
     Build a scene testing that a given data has been properly uploaded to the
     GPU memory.

pynopegl-utils/pynopegl_utils/tests/debug.py
@@ -21,14 +21,13 @@
 
 from typing import Mapping, Tuple
 
-from pynopegl_utils.misc import SceneCfg
 from pynopegl_utils.toolbox.colors import COLORS
 
 import pynopegl as ngl
 
 
 def get_debug_points(
-    cfg: SceneCfg,
+    cfg: ngl.SceneCfg,
     points: Mapping[str, Tuple[float, float]],
     radius: float = 0.025,
     color: Tuple[float, float, float] = COLORS.green,

commit c36f3cc31f0ef2daf115ca72ba97b4559ab7dc65

utils/export: fix SceneInfo references
Regression since 13c115510f2d0ed07c084852e5b1585c942c46a0. Chunk was
forgotten during a rebase.

pynopegl-utils/pynopegl_utils/export.py
@@ -96,7 +96,7 @@ ENCODE_PROFILES = dict(
 )
 
 
-def export_workers(scene_info: SceneInfo, filename: str, resolution: str, profile_id: str):
+def export_workers(scene_info: ngl.SceneInfo, filename: str, resolution: str, profile_id: str):
     profile = ENCODE_PROFILES[profile_id]
     if profile.format == "gif":
         with tempfile.NamedTemporaryFile(prefix="palette-", suffix=".png") as palette:

pynopegl-utils/pynopegl_utils/exporter.py
@@ -24,6 +24,8 @@ from typing import Callable, Optional
 from pynopegl_utils.export import export_workers
 from PySide6 import QtCore
 
+import pynopegl as ngl
+
 
 class Exporter(QtCore.QThread):
     progressed = QtCore.Signal(int)