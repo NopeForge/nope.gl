NopeForge/nope.gl

commit f600c598cc7bcd028ba11cd8e6b984fa9d57ba9c

utils/viewer: filter profiles based on available encoders

pynopegl-utils/pynopegl_utils/viewer/__init__.py
@@ -141,10 +141,6 @@ class _Viewer:
         export_res = self._config.get("export_res")
         export_res_index = choices["export_res"].index(export_res)
 
-        export_profile_names = [ENCODE_PROFILES[p].name for p in choices["export_profile"]]
-        export_profile = self._config.get("export_profile")
-        export_profile_index = choices["export_profile"].index(export_profile)
-
         export_samples_names = ["Disabled" if s == 0 else f"x{s}" for s in choices["export_samples"]]
         export_samples = self._config.get("export_samples")
         export_samples_index = choices["export_samples"].index(export_samples)
@@ -161,6 +157,13 @@ class _Viewer:
         except Exception:
             app_window.disable_export("No working `ffmpeg` command found,\nexport is disabled.")
 
+        encoders = self._get_available_encoders()
+        export_profile_names = [
+            ENCODE_PROFILES[p].name for p in choices["export_profile"] if ENCODE_PROFILES[p].encoder in encoders
+        ]
+        export_profile = self._config.get("export_profile")
+        export_profile_index = choices["export_profile"].index(export_profile)
+
         app_window.set_params_model(self._params_model)
         app_window.set_controls_model(self._livectls_model)
         app_window.set_export_name_filters(f"Supported videos ({extensions})")
@@ -172,6 +175,13 @@ class _Viewer:
         app_window.set_framerates(framerate_names, framerate_index)
 
         self._test_settings()
+
+    @staticmethod
+    def _get_available_encoders():
+        output = subprocess.run(["ffmpeg", "-hide_banner", "-encoders"], capture_output=True).stdout.decode()
+        line_iter = iter(output.splitlines())
+        any(line.strip() == "------" for line in line_iter)  # skip to start of the list
+        return [line.split(maxsplit=3)[1] for line in line_iter]  # pick 2nd column
 
     @staticmethod
     def _uri_to_path(uri):

commit 0445b5f070341c0da7c4713e7ccef49c4fe5306f

utils/viewer: fix export profile_id mismatch
The profile index given by the QML GUI only works with the filtered
profile list based on available encoders.

Fixes an issue introduced by f600c598cc7bcd028ba11cd8e6b984fa9d57ba9c.

pynopegl-utils/pynopegl_utils/qml/viewer.qml
@@ -37,9 +37,9 @@ ApplicationWindow {
     signal selectFramerate(int index)
     signal selectExportFile(string export_file)
     signal selectExportResolution(int index)
-    signal selectExportProfile(int index)
+    signal selectExportProfile(string profile_id)
     signal selectExportSamples(int index)
-    signal exportVideo(string filename, int res, int profile, int samples)
+    signal exportVideo(string filename, int res, string profile_id, int samples)
     signal cancelExport()
 
     function set_script(script_name) {
@@ -233,11 +233,11 @@ ApplicationWindow {
         }
     }
 
-    function start_export(filename, res, profile, samples) {
+    function start_export(filename, res, profile_id, samples) {
         exportBar.value = 0;
         exportStack.currentIndex = 0;
         exportPopup.visible = true;
-        exportVideo(filename, res, profile, samples);
+        exportVideo(filename, res, profile_id, samples);
     }
 
     function finish_export() {
@@ -651,7 +651,9 @@ ApplicationWindow {
                             ComboBox {
                                 id: profileList
                                 Layout.fillWidth: true
-                                onActivated: selectExportProfile(currentIndex)
+                                onActivated: selectExportProfile(currentValue)
+                                valueRole: "profile_id"
+                                textRole: "title"
                             }
 
                             Label { text: "MSAA:" }
@@ -665,7 +667,7 @@ ApplicationWindow {
                         Button {
                             text: "Export"
                             Layout.alignment: Qt.AlignHCenter
-                            onClicked: start_export(exportFile.text, resList.currentIndex, profileList.currentIndex, samplesList.currentIndex)
+                            onClicked: start_export(exportFile.text, resList.currentIndex, profileList.currentValue, samplesList.currentIndex)
                         }
                     }
 

pynopegl-utils/pynopegl_utils/viewer/__init__.py
@@ -157,18 +157,25 @@ class _Viewer:
             app_window.disable_export("No working `ffmpeg` command found,\nexport is disabled.")
 
         encoders = self._get_available_encoders()
-        export_profile_names = [
-            ENCODE_PROFILES[p].name for p in choices["export_profile"] if ENCODE_PROFILES[p].encoder in encoders
+        export_profiles_model = [
+            dict(profile_id=p, title=ENCODE_PROFILES[p].name)
+            for p in choices["export_profile"]
+            if ENCODE_PROFILES[p].encoder in encoders
         ]
+        export_profile_ids = [p["profile_id"] for p in export_profiles_model]
         export_profile = self._config.get("export_profile")
-        export_profile_index = choices["export_profile"].index(export_profile)
+        assert export_profile is not None
+        try:
+            export_profile_index = export_profile_ids.index(export_profile)
+        except ValueError:
+            export_profile_index = 0
 
         app_window.set_params_model(self._params_model)
         app_window.set_controls_model(self._livectls_model)
         app_window.set_export_name_filters(f"Supported videos ({extensions})")
         app_window.set_export_file(export_filename)
         app_window.set_export_resolutions(export_res_names, export_res_index)
-        app_window.set_export_profiles(export_profile_names, export_profile_index)
+        app_window.set_export_profiles(export_profiles_model, export_profile_index)
         app_window.set_export_samples(export_samples_names, export_samples_index)
         app_window.set_script(script)
         app_window.set_framerates(framerate_names, framerate_index)
@@ -282,9 +289,8 @@ class _Viewer:
         res_id = self._config.CHOICES["export_res"][index]
         self._config.set_export_res(res_id)
 
-    @Slot(int)
-    def _select_export_profile(self, index: int):
-        profile_id = self._config.CHOICES["export_profile"][index]
+    @Slot(str)
+    def _select_export_profile(self, profile_id: str):
         self._config.set_export_profile(profile_id)
         self._test_settings()
 
@@ -297,8 +303,8 @@ class _Viewer:
     def _cancel_export(self):
         self._cancel_export_request = True
 
-    @Slot(str, int, int, int)
-    def _export_video(self, filename: str, res_index: int, profile_index: int, samples: int):
+    @Slot(str, int, str, int)
+    def _export_video(self, filename: str, res_index: int, profile_id: str, samples: int):
         scene_data = self._current_scene_data
         if scene_data is None:
             return
@@ -306,7 +312,6 @@ class _Viewer:
         filename = self._uri_to_path(filename)
 
         res_id = self._config.CHOICES["export_res"][res_index]
-        profile_id = self._config.CHOICES["export_profile"][profile_index]
 
         extra_args = self._get_scene_building_extra_args()
 