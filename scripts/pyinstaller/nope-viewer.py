39b7a83e71ac7f444bbe502bbfac1e91435e4365
Matthieu Bouron <matthieu.bouron@gmail.com>
2023-09-28 21:37:22 +0200

    scripts/pyinstaller: allow the entry point to execute ffmpeg_win32.run()
    subprocess.call([sys.executable, "-m", "pynopegl_utils.viewer.ffmpeg_win32", ...])
    currently fails in the pyinstaller environment because:
    - the pyinstaller environment does not bundle a standalone Python interpreter
    - sys.executable refers to the standalone application, not the Python
      interpreter
    
    To work around these limitations, we simply allow the main entry point
    to call ffmpeg_win32.run().

--- scripts/pyinstaller/nope-viewer.py
+++ scripts/pyinstaller/nope-viewer.py
@@ -1,5 +1,6 @@
 import os
 import os.path as op
+import platform
 import sys
 
 from pynopegl_utils.viewer import run
@@ -7,4 +8,14 @@ from pynopegl_utils.viewer import run
 os.environ["PATH"] = os.pathsep.join([op.join(sys._MEIPASS, "bin"), os.environ["PATH"]])
 
 if __name__ == "__main__":
-    run()
+    if (
+        platform.system() == "Windows"
+        and len(sys.argv) > 3
+        and sys.argv[1] == "-m"
+        and sys.argv[2] == "pynopegl_utils.viewer.ffmpeg_win32"
+    ):
+        from pynopegl_utils.viewer.ffmpeg_win32 import run as run_ffmpeg
+
+        run_ffmpeg(sys.argv[3:])
+    else:
+        run()
