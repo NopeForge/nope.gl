NopeForge/nope.gl

commit fc42c8afd453b05f01adc02fe6a6b25288cfcb4e

hwconv: use ngli_pipeline_compat_update_texture_info()
The hwconv module is the only place that do not use
ngli_pipeline_compat_update_texture_info(). It was left as-is in
4e69f5c5611a8ac8660df964e7b8de04e03943c7 to avoid unnecessary graphics
calls. Since the overhead added by
ngli_pipeline_compat_update_texture_info() is negligible, the benefit in
terms of maintenance of this simplification outweigh the performance
drawback.

libnopegl/src/hwconv.c
@@ -178,42 +178,7 @@ int ngli_hwconv_convert_image(struct hwconv *hwconv, const struct image *image)
     const struct pgcraft_texture_info *info = ngli_darray_data(texture_infos_array);
     ngli_assert(ngli_darray_count(texture_infos_array) == 1);
 
-    const struct pgcraft_texture_info_field *fields = info->fields;
-
-    if (image->params.layout) {
-        const float dimensions[] = {(float)image->params.width, (float)image->params.height, (float)image->params.depth};
-        ngli_pipeline_compat_update_uniform(pipeline, fields[NGLI_INFO_FIELD_DIMENSIONS].index, dimensions);
-    }
-
-    int ret = -1;
-    switch (image->params.layout) {
-    case NGLI_IMAGE_LAYOUT_DEFAULT:
-        ret = ngli_pipeline_compat_update_texture(pipeline, fields[NGLI_INFO_FIELD_SAMPLER_0].index, image->planes[0]);
-        break;
-    case NGLI_IMAGE_LAYOUT_NV12:
-        ret = ngli_pipeline_compat_update_texture(pipeline, fields[NGLI_INFO_FIELD_SAMPLER_0].index, image->planes[0]);
-        ret &= ngli_pipeline_compat_update_texture(pipeline, fields[NGLI_INFO_FIELD_SAMPLER_1].index, image->planes[1]);
-        break;
-    case NGLI_IMAGE_LAYOUT_NV12_RECTANGLE:
-        ret = ngli_pipeline_compat_update_texture(pipeline, fields[NGLI_INFO_FIELD_SAMPLER_RECT_0].index, image->planes[0]);
-        ret &= ngli_pipeline_compat_update_texture(pipeline, fields[NGLI_INFO_FIELD_SAMPLER_RECT_1].index, image->planes[1]);
-        break;
-    case NGLI_IMAGE_LAYOUT_MEDIACODEC:
-        ret = ngli_pipeline_compat_update_texture(pipeline, fields[NGLI_INFO_FIELD_SAMPLER_OES].index, image->planes[0]);
-        break;
-    case NGLI_IMAGE_LAYOUT_YUV:
-        ret = ngli_pipeline_compat_update_texture(pipeline, fields[NGLI_INFO_FIELD_SAMPLER_0].index, image->planes[0]);
-        ret &= ngli_pipeline_compat_update_texture(pipeline, fields[NGLI_INFO_FIELD_SAMPLER_1].index, image->planes[1]);
-        ret &= ngli_pipeline_compat_update_texture(pipeline, fields[NGLI_INFO_FIELD_SAMPLER_2].index, image->planes[2]);
-        break;
-    default:
-        ngli_assert(0);
-    }
-    ngli_assert(ret == 0);
-    ngli_pipeline_compat_update_uniform(pipeline, fields[NGLI_INFO_FIELD_SAMPLING_MODE].index, &image->params.layout);
-    ngli_pipeline_compat_update_uniform(pipeline, fields[NGLI_INFO_FIELD_COORDINATE_MATRIX].index, image->coordinates_matrix);
-    ngli_pipeline_compat_update_uniform(pipeline, fields[NGLI_INFO_FIELD_COLOR_MATRIX].index, image->color_matrix);
-
+    ngli_pipeline_compat_update_texture_info(pipeline, info);
     ngli_pipeline_compat_draw(pipeline, 3, 1);
 
     ngli_gpu_ctx_end_render_pass(gpu_ctx);

commit 63ce7902942912c7b0958a2c77caabe9c445ab2d

hwconv: fix a crash in ngli_hwconv_convert_image()
The crash can be reproduced with `ngl-player -d 0 <media>` when a
hwaccel is in use.

Fixes a regression introduced by fc42c8afd453b05f01adc02fe6a6b25288cfcb4e.

libnopegl/src/hwconv.c
@@ -171,8 +171,10 @@ int ngli_hwconv_convert_image(struct hwconv *hwconv, const struct image *image)
     struct pipeline_compat *pipeline = hwconv->pipeline_compat;
 
     const struct darray *texture_infos_array = ngli_pgcraft_get_texture_infos(hwconv->crafter);
-    const struct pgcraft_texture_info *info = ngli_darray_data(texture_infos_array);
     ngli_assert(ngli_darray_count(texture_infos_array) == 1);
+
+    struct pgcraft_texture_info *info = ngli_darray_data(texture_infos_array);
+    info->image = image;
 
     ngli_pipeline_compat_update_texture_info(pipeline, info);
     ngli_pipeline_compat_draw(pipeline, 3, 1);