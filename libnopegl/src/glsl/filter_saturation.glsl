d74bb7a29dd941c34681d09792e97aa4c10ad3fe
Clément Bœsch <u@pkh.me>
2024-01-16 12:52:32 +0100

    glsl/saturation: extract gray in an intermediate variable

--- libnopegl/src/glsl/filter_saturation.glsl
+++ libnopegl/src/glsl/filter_saturation.glsl
@@ -22,6 +22,7 @@
 
 vec4 filter_saturation(vec4 color, vec2 coords, float saturation)
 {
-    color.rgb = ngli_sat(mix(vec3(dot(ngli_luma_weights, color.rgb)), color.rgb, saturation));
+    vec3 gray = vec3(dot(ngli_luma_weights, color.rgb));
+    color.rgb = ngli_sat(mix(gray, color.rgb, saturation));
     return color;
 }
