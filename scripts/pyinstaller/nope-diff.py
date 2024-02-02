6a41bfce07c24ce58b5a7baf445e818694abf0f2
Matthieu Bouron <matthieu.bouron@gmail.com>
2024-02-02 10:51:30 +0100

    release_{macos,win}: distribute NopeDiff along with NopeViewer

---
+++ scripts/pyinstaller/nope-diff.py
@@ -0,0 +1,10 @@
+import os
+import os.path as op
+import sys
+
+from pynopegl_utils.diff import run
+
+os.environ["PATH"] = os.pathsep.join([op.join(sys._MEIPASS, "bin"), os.environ["PATH"]])
+
+if __name__ == "__main__":
+    run()
