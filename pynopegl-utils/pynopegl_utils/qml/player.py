b53df6abf29282f7c720f93388bb6b8e6b2e2e3c
Clément Bœsch <u@pkh.me>
2024-02-23 15:11:21 +0100

    utils/qml: add a NGLPlayer to scope the nope widget and the QML player

---
+++ pynopegl-utils/pynopegl_utils/qml/player.py
@@ -0,0 +1,58 @@
+#
+# Copyright 2024 Clément Bœsch <u pkh.me>
+# Copyright 2024 Nope Forge
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+from pynopegl_utils import qml
+from PySide6.QtCore import QObject
+
+import pynopegl as ngl
+
+
+class NGLPlayer:
+    def __init__(self, app_window, qml_engine):
+        self._ngl_widget = qml.create_ngl_widget(qml_engine)
+        self._qml_player = app_window.findChild(QObject, "player")
+        self._qml_player.timeChanged.connect(self._ngl_widget.set_time)
+
+    def set_scene(self, scene: ngl.Scene):
+        self._ngl_widget.set_scene(scene)
+        self._qml_player.setProperty("duration", scene.duration)
+        self._qml_player.setProperty("framerate", list(scene.framerate))
+        self._qml_player.setProperty("aspect", list(scene.aspect_ratio))
+
+    def set_callbacks(self, livectls_changed=None, mouse_down=None, zoom=None, pan=None):
+        if livectls_changed:
+            self._ngl_widget.livectls_changed.connect(livectls_changed)
+        if mouse_down:
+            self._qml_player.mouseDown.connect(mouse_down)
+        if zoom:
+            self._qml_player.zoom.connect(zoom)
+        if pan:
+            self._qml_player.pan.connect(pan)
+
+    def change_live_data(self, key: str, value):
+        self._ngl_widget.livectls_changes[key] = value
+
+    def update(self):
+        self._ngl_widget.update()
+
+    def stop(self):
+        self._ngl_widget.stop()
