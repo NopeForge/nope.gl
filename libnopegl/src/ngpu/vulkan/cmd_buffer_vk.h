abcd21040772bcbd0bf840864efcd22525390a60
Matthieu Bouron <matthieu.bouron@gmail.com>
2025-03-22 10:24:12 +0100

    ngpu/cmd_buffer_vk: use ngpu prefix

--- libnopegl/src/ngpu/vulkan/cmd_buffer_vk.h
+++ libnopegl/src/ngpu/vulkan/cmd_buffer_vk.h
@@ -31,7 +31,7 @@
 
 struct ngpu_buffer;
 
-struct cmd_buffer_vk {
+struct ngpu_cmd_buffer_vk {
     struct ngli_rc rc;
     struct ngpu_ctx *gpu_ctx;
     int type;
@@ -45,21 +45,21 @@ struct cmd_buffer_vk {
     struct darray buffer_refs; // array of ngpu_buffer pointers
 };
 
-struct cmd_buffer_vk *ngli_cmd_buffer_vk_create(struct ngpu_ctx *gpu_ctx);
-void ngli_cmd_buffer_vk_freep(struct cmd_buffer_vk **sp);
-VkResult ngli_cmd_buffer_vk_init(struct cmd_buffer_vk *s, int type);
-VkResult ngli_cmd_buffer_vk_add_wait_sem(struct cmd_buffer_vk *s, VkSemaphore *sem, VkPipelineStageFlags stage);
-VkResult ngli_cmd_buffer_vk_add_signal_sem(struct cmd_buffer_vk *s, VkSemaphore *sem);
+struct ngpu_cmd_buffer_vk *ngpu_cmd_buffer_vk_create(struct ngpu_ctx *gpu_ctx);
+void ngpu_cmd_buffer_vk_freep(struct ngpu_cmd_buffer_vk **sp);
+VkResult ngpu_cmd_buffer_vk_init(struct ngpu_cmd_buffer_vk *s, int type);
+VkResult ngpu_cmd_buffer_vk_add_wait_sem(struct ngpu_cmd_buffer_vk *s, VkSemaphore *sem, VkPipelineStageFlags stage);
+VkResult ngpu_cmd_buffer_vk_add_signal_sem(struct ngpu_cmd_buffer_vk *s, VkSemaphore *sem);
 
-#define NGLI_CMD_BUFFER_VK_REF(cmd, rc) ngli_cmd_buffer_vk_ref((cmd), (struct ngli_rc *)(rc))
-VkResult ngli_cmd_buffer_vk_ref(struct cmd_buffer_vk *s, struct ngli_rc *rc);
-VkResult ngli_cmd_buffer_vk_ref_buffer(struct cmd_buffer_vk *s, struct ngpu_buffer *buffer);
+#define NGPU_CMD_BUFFER_VK_REF(cmd, rc) ngpu_cmd_buffer_vk_ref((cmd), (struct ngli_rc *)(rc))
+VkResult ngpu_cmd_buffer_vk_ref(struct ngpu_cmd_buffer_vk *s, struct ngli_rc *rc);
+VkResult ngpu_cmd_buffer_vk_ref_buffer(struct ngpu_cmd_buffer_vk *s, struct ngpu_buffer *buffer);
 
-VkResult ngli_cmd_buffer_vk_begin(struct cmd_buffer_vk *s);
-VkResult ngli_cmd_buffer_vk_submit(struct cmd_buffer_vk *s);
-VkResult ngli_cmd_buffer_vk_wait(struct cmd_buffer_vk *s);
+VkResult ngpu_cmd_buffer_vk_begin(struct ngpu_cmd_buffer_vk *s);
+VkResult ngpu_cmd_buffer_vk_submit(struct ngpu_cmd_buffer_vk *s);
+VkResult ngpu_cmd_buffer_vk_wait(struct ngpu_cmd_buffer_vk *s);
 
-VkResult ngli_cmd_buffer_vk_begin_transient(struct ngpu_ctx *gpu_ctx, int type, struct cmd_buffer_vk **sp);
-VkResult ngli_cmd_buffer_vk_execute_transient(struct cmd_buffer_vk **sp);
+VkResult ngpu_cmd_buffer_vk_begin_transient(struct ngpu_ctx *gpu_ctx, int type, struct ngpu_cmd_buffer_vk **sp);
+VkResult ngpu_cmd_buffer_vk_execute_transient(struct ngpu_cmd_buffer_vk **sp);
 
 #endif
