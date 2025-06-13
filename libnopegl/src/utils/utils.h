c8229a05f11a82db8772d63790ac1756942f8194
Matthieu Bouron <matthieu.bouron@gmail.com>
2025-06-13 12:13:26 +0200

    utils: add NGLI_ALIGNMENT() macro
    Useful to get the alignment of a variable.

--- libnopegl/src/utils/utils.h
+++ libnopegl/src/utils/utils.h
@@ -92,6 +92,8 @@
 
 #define NGLI_IS_ALIGNED(v, a) (((v) & ((__typeof__(v))(a) - 1)) == 0)
 
+#define NGLI_ALIGNMENT(v) ((v) & ~((v) - 1))
+
 #define NGLI_ATTR_ALIGNED _Alignas(NGLI_ALIGN_VAL)
 
 #define NGLI_ALIGNED_VEC(vname) float NGLI_ATTR_ALIGNED vname[4]
