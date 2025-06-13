b5818d8826976bf5dbee68af7cd5fc286d131f38
Matthieu Bouron <matthieu.bouron@gmail.com>
2025-06-13 11:14:30 +0200

    ngpu/vkcontext: use an uint32_t  to store the memory type index
    VkMemoryAllocateInfo.memoryTypeIndex type is uint32_t.
    
    Fixes -Wsign-conversion warnings.

--- libnopegl/src/ngpu/vulkan/texture_vk.c
+++ libnopegl/src/ngpu/vulkan/texture_vk.c
@@ -396,17 +396,17 @@ static VkResult texture_vk_init(struct ngpu_texture *s, const struct ngpu_textur
     VkMemoryRequirements mem_reqs;
     vkGetImageMemoryRequirements(vk->device, s_priv->image, &mem_reqs);
 
-    int mem_type_index = NGL_ERROR_NOT_FOUND;
+    uint32_t mem_type_index = UINT32_MAX;
     if (s->params.usage & NGPU_TEXTURE_USAGE_TRANSIENT_ATTACHMENT_BIT) {
         const VkMemoryPropertyFlags mem_props = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT |
                                                 VK_MEMORY_PROPERTY_LAZILY_ALLOCATED_BIT;
         mem_type_index = ngli_vkcontext_find_memory_type(vk, mem_reqs.memoryTypeBits, mem_props);
     }
 
-    if (mem_type_index < 0) {
+    if (mem_type_index == UINT32_MAX) {
         const VkMemoryPropertyFlags mem_pros = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
         mem_type_index = ngli_vkcontext_find_memory_type(vk, mem_reqs.memoryTypeBits, mem_pros);
-        if (mem_type_index < 0)
+        if (mem_type_index == UINT32_MAX)
             return VK_ERROR_FORMAT_NOT_SUPPORTED;
     }
 
